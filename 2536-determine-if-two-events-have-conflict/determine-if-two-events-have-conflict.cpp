b43c84feef83c993254292f004c28195aaf9f516
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-18 01:28:49 +0530

    Time: 3 ms (46.19%) | Memory: 13.1 MB (49.48%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
 
-    int giveTime(string& s)
-    {
-        int time=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(i==':')
-            continue;
-            time=10*time+(s[i]-'0');
-        }
-        return time;
-    }
-
     bool haveConflict(vector<string>& event1, vector<string>& event2) 
     {
-        int event2StartTime=giveTime(event2[0]);
-        int event2EndTime=giveTime(event2[1]);
-        int event1StartTime=giveTime(event1[0]);
-        int event1EndTime=giveTime(event1[1]);
-        
-        return event2StartTime<=event1EndTime && event1StartTime<=event2EndTime;
+        return event2[0]<=event1[1] && event1[0]<=event2[1];
     }
 };
