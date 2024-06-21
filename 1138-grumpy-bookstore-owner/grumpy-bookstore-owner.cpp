a0edc91952475ee6a0a12476d4315a1cd63352dc
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-21 16:43:23 +0530

    Time: 29 ms (29.42%) | Memory: 34.3 MB (47.97%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) 
+    {
+        int maxExtraSatisfaction=0,extraSatisfaction=0,satisfaction=0;
+        for(int j=0;j<grumpy.size();j++)
+        {
+            if(grumpy[j]!=1)
+            satisfaction+=customers[j];
+        }
+
+        int i=0;
+        for(int j=0;j<grumpy.size();j++)
+        {
+            if(grumpy[j]==1)
+            extraSatisfaction+=customers[j];
+            if(j-i+1==minutes)
+            {
+                maxExtraSatisfaction=max(maxExtraSatisfaction,extraSatisfaction);
+                if(grumpy[i]==1)
+                extraSatisfaction-=customers[i];
+                i++;
+            }
+        }
+        return satisfaction+maxExtraSatisfaction;
+    }
+};
