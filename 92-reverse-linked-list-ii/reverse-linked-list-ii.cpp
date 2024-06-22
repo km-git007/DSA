cb5d39e9ab8a814664d03220d300d49852b01fd9
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-22 18:10:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (89.46%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    pair<ListNode*,ListNode*> reverseList(ListNode* head,int count)
+    {
+        ListNode *curr=head,*prev=NULL;
+        while(curr && count)
+        {
+           ListNode *Next=curr->next;
+           curr->next=prev;
+           prev=curr;
+           curr=Next; 
+           count--;
+        }
+
+        // making connection with the remaining list in the right side.
+        head->next=curr;
+        return {prev,head};
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) 
+    {
+        if(left==right)
+        return head;
+
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode *leftEnd=dummy;
+
+        for(int steps=0;steps<left-1;steps++)
+        leftEnd=leftEnd->next;
+
+        pair<ListNode*,ListNode*> nodePair=reverseList(leftEnd->next,right-left+1);
+        ListNode* reversedHead=nodePair.first;
+        leftEnd->next=reversedHead;
+
+        return dummy->next;  
+    }
+};
