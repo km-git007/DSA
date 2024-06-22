116e561602cc2b40775c87c98f4b45823aded013
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-23 04:00:44 +0530

    Time: 10 ms (36.02%) | Memory: 11.4 MB (75.11%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) 
+    {
+        vector<int> common(26,0);
+        for(char c : words[0])
+        common[c-'a']++;
+
+        for(int i=1;i<words.size();i++)
+        {
+            vector<int> freq(26,0);
+            for(char c : words[i])
+            freq[c-'a']++;
+
+            for(int j=0;j<26;j++)
+            common[j]=min(common[j],freq[j]);
+        }
+
+        vector<string> ans;
+        for(int i=0;i<26;i++)
+        {
+            if(common[i])
+            {
+                char c=char(i+'a');
+                for(int j=0;j<common[i];j++)
+                ans.push_back(string(1,c));
+            }
+        }
+        return ans;
+    }
+};
