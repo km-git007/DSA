85323fa40d717711601840ba6d68868ed5c8ac35
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-22 17:07:38 +0530

    Time: 7 ms (46.48%) | Memory: 9.3 MB (40.91%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,20 +4,20 @@ public:
     {
         unordered_map<char,char> map;
         unordered_set<char> set;
-
         for(int i=0;i<s.length();i++)
         {
-            if(map.count(s[i])==0)
+            if(map.count(s[i])==0)          // given char is not present in map
             {
-                if(set.count(t[i])==0)
+                if(set.count(t[i])==0)    // t[i] shouldn't be in set already means it should be unmapped. 
                 {
                     map[s[i]]=t[i];
                     set.insert(t[i]);
                 }
                 else
-                return false;
+                return false;               // t[i] in set already so returning false.
             }
-            else
+
+            else                            // given char is already in map   
             {
                 if(map[s[i]]!=t[i])
                 return false;
