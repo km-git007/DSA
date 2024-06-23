3b0d20855cf058c517964c36a98abe6c092e7b8d
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-23 20:13:00 +0530

    Time: 162 ms (9.54%) | Memory: 98 MB (5.23%) - LeetSync

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& a, int limit) 
+    {
+        list<int> minList,maxList;
+        int maxLen=0,i=0;
+        for(int j=0;j<a.size();j++)
+        {
+            while(!minList.empty() && a[j]<minList.back())
+            minList.pop_back();
+            minList.push_back(a[j]);
+
+            while(!maxList.empty() && a[j]>maxList.back())
+            maxList.pop_back();
+            maxList.push_back(a[j]);
+
+            while(maxList.front()-minList.front()>limit)
+            {
+                if(a[i]==maxList.front())
+                maxList.pop_front();
+                if(a[i]==minList.front())
+                minList.pop_front();
+                i++;
+            }
+
+            maxLen=max(maxLen,j-i+1);
+        }
+        return maxLen;
+    }
+};
