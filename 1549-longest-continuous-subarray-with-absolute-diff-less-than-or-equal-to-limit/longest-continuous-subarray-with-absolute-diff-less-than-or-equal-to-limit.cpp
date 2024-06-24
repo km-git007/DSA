6d8ad93b22485b1a07d359c07c584e65f9d10274
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-06-24 14:10:50 +0530

    Time: 119 ms (46.42%) | Memory: 62 MB (54.00%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     int longestSubarray(vector<int>& a, int limit) 
     {
-        deque<int> minList,maxList;
+        priority_queue<pair<int,int>> maxHeap;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minHeap;
         int maxLen=0,i=0;
         for(int j=0;j<a.size();j++)
         {
-            while(!minList.empty() && a[j]<minList.back())
-            minList.pop_back();
-            minList.push_back(a[j]);
+            maxHeap.push({a[j],j});
+            minHeap.push({a[j],j});
 
-            while(!maxList.empty() && a[j]>maxList.back())
-            maxList.pop_back();
-            maxList.push_back(a[j]);
-
-            while(maxList.front()-minList.front()>limit)
+            while(maxHeap.top().first-minHeap.top().first>limit)
             {
-                if(a[i]==maxList.front())
-                maxList.pop_front();
-                if(a[i]==minList.front())
-                minList.pop_front();
-                i++;
+                i=min(maxHeap.top().second, minHeap.top().second)+1;
+                // Remove elements from the heaps that are outside the current window
+                while(maxHeap.top().second<i)
+                maxHeap.pop();
+                while(minHeap.top().second<i)
+                minHeap.pop();
             }
-
             maxLen=max(maxLen,j-i+1);
         }
         return maxLen;
