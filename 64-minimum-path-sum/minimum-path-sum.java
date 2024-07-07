84a7057ae8844a942945997aba45415518167fda
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-07-07 23:48:04 +0530

    Time: 6 ms (5.97%) | Memory: 45.6 MB (18.19%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private int[][] dp;
+    private int n;
+    private int m;
+
+    public int solve(int row,int col,int[][] grid,int[][] dp)
+    {
+        if(row==n || col==m)
+        return Integer.MAX_VALUE;
+
+        if(row==n-1 && col==m-1)
+        return grid[row][col];
+
+        if(dp[row][col]!=-1)
+        return dp[row][col];
+
+        return dp[row][col]=grid[row][col]+Math.min(solve(row+1,col,grid,dp),solve(row,col+1,grid,dp));
+    }
+
+    public int minPathSum(int[][] grid) 
+    {
+        dp = new int[201][201];
+        for (int[] row : dp)
+        Arrays.fill(row,-1);
+
+        m=grid[0].length;
+        n=grid.length;
+
+       return solve(0,0,grid,dp);
+    }
+}
