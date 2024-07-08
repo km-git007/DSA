6a06937ba8227e959bda817af267bd6ef635576e
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-07-09 00:09:17 +0530

    Time: 16 ms (61.03%) | Memory: 17.3 MB (29.95%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -16,24 +16,11 @@ public:
         if(lists.size()==0)
         return NULL;
 
-        // priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>> minHeap;
-        // for(int i=0;i<lists.size();i++)
-        // minHeap.push({lists[i][0].val,lists[i][0]});
-
-         // Min-heap based on pair<int, ListNode*>
-        auto compare = [](const pair<int, ListNode*>& a, const pair<int, ListNode*>& b) 
-        {
-            return a.first > b.first;
-        };
-
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, decltype(compare)> minHeap(compare);
-
-        // Initialize minHeap with the first node of each list
+        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>> minHeap;
+        
         for(ListNode* list : lists) 
-        {
-            if(list)
-            minHeap.push({list->val, list});
-        }
+        if(list)
+        minHeap.push({list->val,list});
 
         ListNode* dummy=new ListNode(-1);
         ListNode* tail=dummy;
