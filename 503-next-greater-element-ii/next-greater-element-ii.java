cb289bcf999dd47348dce7d36967f724bf409b12
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-07-17 02:31:02 +0530

    Time: 17 ms (47.92%) | Memory: 46.2 MB (40.93%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) 
+    {
+        Stack<Integer> stack = new Stack<>();
+        int []res = new int[nums.length];
+        for(int i=2*nums.length-1;i>=0;i--)
+        {
+            int index=i%nums.length;
+            while(!stack.isEmpty() && nums[index]>=stack.peek())
+            stack.pop();
+
+            if(stack.isEmpty())
+            res[index]=-1;
+            else
+            res[index]=stack.peek();
+
+            stack.push(nums[index]);
+        }
+        return res;
+    }
+}
