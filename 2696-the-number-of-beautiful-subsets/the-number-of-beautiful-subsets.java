9d5790395e274e3a79e4b7b28e83d1dcf8f1ca84
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-07-26 14:50:55 +0530

    Time: 797 ms (38.60%) | Memory: 45.4 MB (11.65%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    HashMap<Integer,Integer> map=new HashMap<>();
+    private int solve(int index,int[] nums,int k)
+    {
+        if(index==nums.length)
+        return 1;
+
+        int subsets=solve(index+1,nums,k);
+        if(!map.containsKey(nums[index]+k) && !map.containsKey(nums[index]-k))
+        {
+            map.put(nums[index],map.getOrDefault(nums[index],0)+1);
+            subsets+=solve(index+1,nums,k);
+            map.put(nums[index],map.get(nums[index])-1);
+
+            if(map.get(nums[index])==0) 
+            map.remove(nums[index]);
+        }
+        return subsets;
+    }
+
+    public int beautifulSubsets(int[] nums, int k) 
+    {
+        Arrays.sort(nums);
+        return solve(0,nums,k)-1;
+    }
+}
