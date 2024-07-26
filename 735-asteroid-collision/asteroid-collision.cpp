d1bb7979789fecbc886c0cc63650a96aa3cf0cdf
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-07-26 13:45:57 +0530

    Time: 12 ms (39.26%) | Memory: 20 MB (92.37%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -9,30 +9,18 @@ public:
             if(asteroids[i]>0)                    
             ans.push_back(asteroids[i]);
             
-            // negative element encountered
-            else                            
+            else
             {
-                // to keep track of blast. For example asteroids=[10,-10]
-                bool blast=false;           
+                while(!ans.empty() && ans.back()>0 && ans.back()<abs(asteroids[i]))
+                ans.pop_back();
 
-                // keep popping till you either empty the ans or you keep encountering a positive element at the   
-                // back of the the ans having value lesss then the abs value of the current negative element.
-                while(!ans.empty() && ans.back()>0 && ans.back()<=abs(asteroids[i]))
+                if(!ans.empty() && ans.back()==abs(asteroids[i]))
                 {
-                    if(ans.back()==abs(asteroids[i]))
-                    {
-                        blast=true;
-                        ans.pop_back();
-                        break;
-                    }
                     ans.pop_back();
+                    continue;
                 }
-                
-                // add the current negative element to the ans if the ans is empty or you've encountered a 
-                // negative element at the back of the the ans and no blast has occured.
-                // again the same example --> asteroids=[10,-10]
-                // for above example even after the ans being empty '-10' won't be added to the ans.
-                if((ans.empty() || ans.back()<0) && !blast)
+
+                if(ans.empty() || ans.back()<0)
                 ans.push_back(asteroids[i]);
             }
         }
