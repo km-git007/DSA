4b0c0dfe73d6a363f95deb4b5da8616379717c26
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-08 21:50:34 +0530

    Time: 3 ms (98.99%) | Memory: 13.7 MB (48.36%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<pair<int,int>> directions={{0,1},{1,0},{0,-1},{-1,0}};   // R D L U
+    vector<vector<int>> spiralMatrixIII(int n, int m, int rStart, int cStart) 
+    {
+        vector<vector<int>> ans;
+        int steps=0;
+        int dir=0;
+        ans.push_back({rStart,cStart});
+        while(ans.size()<m*n)
+        {
+            if(dir==0 || dir==2)
+            steps++;
+
+            for(int count=0;count<steps;count++)
+            {
+                rStart+=directions[dir].first;
+                cStart+=directions[dir].second;
+
+                if(rStart>=0 && rStart<n && cStart>=0 && cStart<m)
+                ans.push_back({rStart,cStart});
+
+            }
+            dir=(dir+1)%4;
+        }
+        return ans;
+    }
+};
