986fa80b3d4e807ea0a27bd4fd63035faf079c36
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-11 04:47:27 +0530

    Time: 331 ms (78.54%) | Memory: 133.8 MB (44.16%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -5,12 +5,12 @@ public:
         public:
 
         int index;
-        int pTime;
+        int processingTime;
         int startTime;
-        Task(int startTime,int pTime,int index)
+        Task(int startTime,int processingTime,int index)
         {
             this->index=index;
-            this->pTime=pTime;
+            this->processingTime=processingTime;
             this->startTime=startTime;
         }
     };
@@ -21,9 +21,9 @@ public:
         bool operator()(Task a,Task b)
         {
             // Min-heap based on processing time
-            if(a.pTime!=b.pTime)
-            return a.pTime>b.pTime;
-            // If pTime is the same, sort by index
+            if(a.processingTime!=b.processingTime)
+            return a.processingTime>b.processingTime;
+            // If processingTime is the same, sort by index
             return a.index>b.index;
 
             // priority_queue mein ULTA LIKHNA PADTA HAI.
@@ -47,20 +47,29 @@ public:
         long time=0;
         while(i<n || !pq.empty())
         {
+            // If CPU is sitting idle then currentTime becomes equal to the startTime of the next available task.
             if(pq.empty() && time<tasksList[i].startTime)
             time=tasksList[i].startTime;
 
+            //  keep pushing tasks into queue while the enqueueing time of next task is lower than the current time!!
             while(i<n && time>=tasksList[i].startTime)
             {
                 pq.push(tasksList[i]);
                 i++;
             }
-
+            // Why are we popping one by one?
+            // Because we have pushed those Tasks whose starting Time was less than or equal to the current time.
+            // So if you pop using a while loop then after finishing one Task which isnt in the queue yet 
+            // but has become eligible to be in the queue due to its less processing Time and should be processed
+            // next by the CPU will not get processed for not being in the queue.
+            // tasks=[[1,2],[3,2],[3,5],[3,6],[4,1]]
+            // here last task will be processed at the last if we use while loop.
+            // rather its should be the third task to be processed.  
             Task t=pq.top();
             pq.pop();
 
             order.push_back(t.index);
-            time+=t.pTime;
+            time+=t.processingTime;
         }
         return order;
     }
