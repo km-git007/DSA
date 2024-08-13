188026ec7d5c6a2bc6cf2aa36cbea8cd8fca2c3c
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-14 01:46:42 +0530

    Time: 74 ms (73.09%) | Memory: 13.6 MB (91.59%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) 
     {
+        // Longest common Substring
         int n=nums1.size();
         int m=nums2.size();
         int maxLen=0;
-        int dp[n+1][m+1];
-        for(int i=0;i<=n;i++)
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+        for(int i=1;i<=n;i++)
         {
-            for(int j=0;j<=m;j++)
+            for(int j=1;j<=m;j++)
             {
-                if(i==0 || j==0)
-                dp[i][j]=0;
-
-                else if(nums1[i-1]==nums2[j-1])
-                dp[i][j]=1+dp[i-1][j-1];
-
+                if(nums1[i-1]==nums2[j-1])
+                curr[j]=1+prev[j-1];
+                
                 else
-                dp[i][j]=0;
+                curr[j]=0;
 
-                maxLen=max(dp[i][j],maxLen);
+                maxLen=max(curr[j],maxLen);
             }
+            prev=curr;
         }
         return maxLen;
     }
