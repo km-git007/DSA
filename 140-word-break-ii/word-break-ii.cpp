b12afa0dea3355820797c7acb6758f84f7a044c8
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-16 01:54:54 +0530

    Time: 2 ms (42.48%) | Memory: 9.4 MB (37.25%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    vector<string> ans;
+    unordered_set<string> set;
+    void solve(string s,string temp,int start)
+    {
+        if(start==s.length())
+        {
+            temp.pop_back();
+            ans.push_back(temp);
+            return;
+        }
+
+        string curr="";
+        for(int i=start;i<s.length();i++)
+        {
+            curr.push_back(s[i]);
+            if(set.count(curr))
+            {
+                solve(s,temp+curr+" ",i+1);
+            }
+        }
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) 
+    {
+        for(string word : wordDict)
+        set.insert(word);
+
+        string temp="";
+        solve(s,temp,0);
+        return ans;
+    }
+};
