ddbba60629ba51864fb51804cb02217642b617d0
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-16 20:30:45 +0530

    Time: 5 ms (93.63%) | Memory: 45.3 MB (70.44%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.java
@@ -1,34 +1,33 @@
 class CustomStack {
 
-    private int capacity;
-    private List<Integer> stack=new ArrayList<>();
+    private int capacity,index;
+    private int[] stack;
     public CustomStack(int maxSize) 
     {
+        stack=new int[maxSize];
         capacity=maxSize;
+        index=0;
     }
     
     public void push(int x) 
     {
-        if(stack.size()<capacity)
-        stack.add(x);
+        if(index<capacity)
+        stack[index++]=x;
     }
     
     public int pop() 
     {
         int topElement=-1;
-        if(stack.size()>0)
-        {
-            topElement=stack.get(stack.size()-1);
-            stack.remove(stack.size()-1);
-        }
+        if(index>0)
+        topElement=stack[--index];
         return topElement;
     }
     
     public void increment(int k, int val) 
     {
-        int size=Math.min(k,stack.size());
+        int size=Math.min(k,index);
         for(int i=0;i<size;i++)
-        stack.set(i,stack.get(i)+val);
+        stack[i]+=val;
     }
 }
 
