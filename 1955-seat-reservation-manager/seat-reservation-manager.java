ce534f099f550efa109df2557b7cfebb2b7668ba
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-17 13:52:54 +0530

    Time: 88 ms (56.22%) | Memory: 88.9 MB (44.20%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.java
+++ 1955-seat-reservation-manager/seat-reservation-manager.java
@@ -1,37 +1,21 @@
 class SeatManager {
 
-    private class Seat{
-
-        int seatNumber;
-        boolean isReserved;
-        Seat(int seatNumber,boolean isReserved)
-        {
-            this.seatNumber=seatNumber;
-            this.isReserved=isReserved;
-        } 
-    }
-    
-    PriorityQueue<Seat> queue=new PriorityQueue<>((a,b)->a.seatNumber-b.seatNumber);
+    PriorityQueue<Integer> queue=new PriorityQueue<>();
     public SeatManager(int n) 
     {
         for(int i=1;i<=n;i++)
-        {
-            Seat seat=new Seat(i,false);
-            queue.offer(seat);
-        }
+        queue.offer(i);
     }
     
     public int reserve() 
     {
-        Seat seat=queue.poll(); 
-        seat.isReserved=true;
-        return seat.seatNumber;
+        int seatNumber=queue.poll(); 
+        return seatNumber;
     }
     
     public void unreserve(int seatNumber) 
     {
-        Seat seat=new Seat(seatNumber,false);
-        queue.offer(seat);
+        queue.offer(seatNumber);
     }
 }
 
