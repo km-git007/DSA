8e11efda61994202eb3ac35211439b57eaa507a8
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-18 20:32:22 +0530

    Time: 49 ms (68.14%) | Memory: 58 MB (38.34%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    public boolean isValid(int[] time,int totalTrips,long allotedTime)
+    {
+        long trips=0;
+        for(int i=0;i<time.length;i++)
+        {
+            trips+=allotedTime/time[i];
+            if(trips>=totalTrips)
+            return true;
+        }
+        return false;
+    }
+
+    public long minimumTime(int[] time, int totalTrips) 
+    {
+        long timeSum=Long.MAX_VALUE;
+        long end=Math.max(timeSum,(long)totalTrips);
+        long start=Arrays.stream(time).min().orElse(0);  // Find the minimum element in time
+
+        long minimumTimeRequired=end;
+        while(start<=end)
+        {
+            long mid=start+(end-start)/2;
+            if(isValid(time,totalTrips,mid))
+            {
+                minimumTimeRequired=mid;
+                end=mid-1;
+            }
+            else
+                start=mid+1;
+        }  
+        return minimumTimeRequired;
+    }
+}
