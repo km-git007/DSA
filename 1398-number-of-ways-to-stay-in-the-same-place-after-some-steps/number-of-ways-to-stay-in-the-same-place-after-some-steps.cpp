e843264f378e3194fe247c36886dc2d5ce249fba
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-21 21:35:05 +0530

    Time: 16 ms (36.42%) | Memory: 8.4 MB (87.19%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int mod=1e9+7;
+    int MOD=1e9+7;
     int dp[501][501];
-    int solve(int steps,int pos,int end)
+    int solve(int steps,int position,int end)
     {
-        if(pos==1 && steps==0)
+        if(position==1 && steps==0)
         return 1;
 
-        if(steps==0 || pos<1)
+        if(steps==0 || position<1)
         return 0;
 
-        if(dp[steps][pos]!=-1)
-        return dp[steps][pos];
+        if(dp[steps][position]!=-1)
+        return dp[steps][position];
 
-        int stay=solve(steps-1,pos,end);
+        int stay=solve(steps-1,position,end);
 
         int left=0,right=0;
-        if(pos>1)
-        left=solve(steps-1,pos-1,end);
+        if(position>1)
+        left=solve(steps-1,position-1,end);
 
-        if(pos<end)
-        right=solve(steps-1,pos+1,end);
+        if(position<end)
+        right=solve(steps-1,position+1,end);
 
-        int totalWays=((stay%mod + left%mod)%mod + right%mod)%mod;
-        return dp[steps][pos]=totalWays;
+        int totalWays=((stay%MOD + left%MOD)%MOD + right%MOD)%MOD;
+        return dp[steps][position]=totalWays;
     }
 
     int numWays(int steps, int arrLen) 
