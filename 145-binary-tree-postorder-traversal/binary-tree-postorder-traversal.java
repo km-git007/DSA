85888eaafd568c0c39ff72c29508ace06b8bba0e
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-26 13:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (65.00%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -18,36 +18,25 @@ class Solution {
     public List<Integer> postorderTraversal(TreeNode root) 
     {
         List<Integer> ans=new ArrayList<>();
-        // adds element in order(Root-Right-Left)
-        // we will have to reverse the ans at last.
-        while(root!=null)
+        if(root==null)
+        return ans;
+
+        // Left->Right->Root
+        // we will add in order Root->Left->Right and then reverse the list
+        ArrayDeque<TreeNode> stack=new ArrayDeque<>();
+        stack.push(root);
+        while(!stack.isEmpty())
         {
-            if(root.right==null)
-            {
-                ans.add(root.val);
-                root=root.left;
-            }
-            else
-            {
-                TreeNode curr=root.right;
-                while(curr.left!=null && curr.left!=root)
-                curr=curr.left;
+            TreeNode node=stack.pop();
+            ans.add(node.val);
 
-                if(curr.left==null)
-                {
-                    ans.add(root.val);
-                    curr.left=root;
-                    root=root.right;
-                }
-                else if(curr.left==root)
-                {
-                    curr.left=null;
-                    root=root.left;
-                }
-            }
+            if(node.left!=null)
+            stack.push(node.left);
+
+            if(node.right!=null)
+            stack.push(node.right);
         }
-
         Collections.reverse(ans);
         return ans;
-    } 
+    }
 }
