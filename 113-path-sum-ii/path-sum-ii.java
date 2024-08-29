f99e0e67597530ada28b31577945c2fa3d18fc8a
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-08-30 00:11:39 +0530

    Time: 1 ms (99.78%) | Memory: 44.5 MB (45.68%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private List<List<Integer>> ans=new ArrayList<>();
+    private List<Integer> list=new ArrayList<>();
+    private void solve(TreeNode root, int target)
+    {
+        if(root==null)
+        return;
+
+        target-=root.val;
+        list.add(root.val);
+
+        if(root.left==null && root.right==null && target==0)
+        ans.add(new ArrayList(list));
+
+        solve(root.left,target);
+        solve(root.right,target);
+
+        list.remove(list.size()-1);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) 
+    {
+        solve(root,targetSum);
+        return ans;
+    }
+}
