0f426922985392c115dc6acbd4e7febf3360b2f0
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-09-02 01:42:51 +0530

    Time: 1123 ms (22.26%) | Memory: 73.6 MB (65.72%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.java
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.java
@@ -17,112 +17,65 @@ public class FoodRatings {
     // stores <food-name,Food> as key value pair.
     private HashMap<String,Food> foodMap;
 
-    // stores <cuisine,List<Food>> as key value pair.
-    // stores the list of Foods belonging to the same cuisine.
-    private HashMap<String,List<Food>> cuisineMap;
 
-    // lets us know the maxRatedFood of the cuisine in O(1) time.
-    private HashMap<String,Food> maxRatedFoodOfCuisineMap;
+    // HashMap that maps a cuisine to a priority queue of Food objects
+    private HashMap<String, PriorityQueue<Food>> cuisineMap;
 
 
-    private boolean compareRating(Food food1,Food food2)
-    {
-        // if this condition is satisfied then food1 becomes the max rated food of the cuisine
-        if(food1.rating>food2.rating || (food1.rating==food2.rating && food1.name.compareTo(food2.name)<0))
-            return true;
-        return false;
-    }
-
-
+    // Global static comparator for Food objects
+    private static final Comparator<Food> FOOD_COMPARATOR = (food1, food2) -> {
+        // First, compare by rating in descending order (max rating at the top)
+        if (food1.rating != food2.rating) {
+            return food2.rating - food1.rating;
+        }
+        // If ratings are the same, compare by name in lexicographical order (min name at the top)
+        return food1.name.compareTo(food2.name);
+    };
+    
+    
+    
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings)
     {
         foodMap=new HashMap<>();
         cuisineMap=new HashMap<>();
-        maxRatedFoodOfCuisineMap=new HashMap<>();
 
         for(int i=0;i<foods.length;i++)
         {
             Food foodItem=new Food(foods[i],cuisines[i],ratings[i]);
             foodMap.put(foods[i],foodItem);
 
-            // if the cuisine map contains the List of Foods belonging to the same cuisine
-            // just add the current foodItem in the cuisineMap.
-            if(cuisineMap.containsKey(cuisines[i]))
-            {
-                cuisineMap.get(cuisines[i]).add(foodItem);
-            }
+            // Get the priority queue for the cuisine, or create one if it doesn't exist
+            cuisineMap.putIfAbsent(cuisines[i], new PriorityQueue<>(FOOD_COMPARATOR));
 
-            // if the cuisine map doesn't contain the List of Foods belonging to the same cuisine
-            // Create a new list of Foods and add the foodItem in the list and map it with the cuisine.
-            else
-            {
-                List<Food> foodList=new ArrayList<>();
-                foodList.add(foodItem);
-                cuisineMap.put(cuisines[i],foodList);
-            }
-
-
-            // Now populating the Hashmap maxRatedFoodOfCuisine which lets us know
-            // the max-rated-food-of-the-cuisine in O(1) time.
-            if(maxRatedFoodOfCuisineMap.containsKey(cuisines[i]))
-            {
-                Food food= maxRatedFoodOfCuisineMap.get(cuisines[i]);
-                if(compareRating(foodItem,food))
-                    maxRatedFoodOfCuisineMap.put(cuisines[i],foodItem);
-            }
-            else
-                maxRatedFoodOfCuisineMap.put(cuisines[i],foodItem);
+            // Add the food item to the corresponding cuisine's priority queue
+            cuisineMap.get(cuisines[i]).add(foodItem);
         }
     }
 
 
     public void changeRating(String food, int newRating)
     {
-        Food foodItem=foodMap.get(food);
-        int presentRating=foodItem.rating;
-        
-        
-        // find the maxRatedFood of the current food item's cuisine
-        Food maxRatedFood= maxRatedFoodOfCuisineMap.get(foodItem.cuisine);
+        Food foodItem = foodMap.get(food);
+    
+        // Get the priority queue for the cuisine
+        PriorityQueue<Food> foodQueue = cuisineMap.get(foodItem.cuisine);
+
+        // Remove the food item from the priority queue
+        foodQueue.remove(foodItem);
 
         // Update the rating of the food
-        foodItem.rating=newRating;
+        foodItem.rating = newRating;
 
-        
-        // If the rating is getting increased then we check whether we can make this foodItem as
-        // the maxRatedFood of its cuisine.
-        if(presentRating<newRating)
-        {
-            if(compareRating(foodItem,maxRatedFood))
-                maxRatedFoodOfCuisineMap.put(foodItem.cuisine,foodItem);
-        }
-        
-        // If the rating is going to get down then we check whether the current FoodItem is
-        // the maxRatedFood of its cuisine or not.If it is the maxRatedFood its cuisine then
-        // we iterate over the list of foodItems belonging to the cuisine
-        // and update the maxRatedFood of the current food item's cuisine.
-        else if(presentRating>newRating && maxRatedFood==foodItem)
-        {
-            List<Food> foodList = cuisineMap.get(foodItem.cuisine);
-            Food newMaxRatedFood = null;
-            for (Food myFood : foodList)
-            {
-                if (newMaxRatedFood == null || compareRating(myFood, newMaxRatedFood))
-                {
-                    newMaxRatedFood = myFood;
-                }
-            }
-            // Update the max-rated food in the map
-            maxRatedFoodOfCuisineMap.put(foodItem.cuisine, newMaxRatedFood);
-        }
+        // Reinsert the food item with the updated rating
+        foodQueue.add(foodItem);
     }
-
-
+    
     public String highestRated(String cuisine)
     {
-        return maxRatedFoodOfCuisineMap.get(cuisine).name;
+        return cuisineMap.get(cuisine).peek().name;
     }
 }
+
 
 /**
  * Your FoodRatings object will be instantiated and called as such:
