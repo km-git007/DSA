3ff829f9a3e88ec8c3948678a38a98516d311dec
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-09-06 22:36:24 +0530

    Time: 1 ms (85.85%) | Memory: 44.9 MB (32.33%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private HashSet<Integer> set=new HashSet<>();
+    private List<TreeNode> list=new ArrayList<>();
+    private TreeNode solve(TreeNode root)
+    {
+        if(root==null)
+        return root;
+
+        root.left=solve(root.left);
+        root.right=solve(root.right);
+
+        if(set.contains(root.val))
+        {
+            if(root.left!=null)
+            list.add(root.left);
+
+            if(root.right!=null)
+            list.add(root.right);
+
+            root=null;
+        }
+        return root;
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) 
+    {
+        for(int num : to_delete)
+        set.add(num);
+
+        if(!set.contains(root.val))
+        list.add(root);
+
+        solve(root);
+        return list;
+    }
+}
