97f3f15e078fcdfe92570e4b35fdccff430e6e21
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-09-10 21:04:31 +0530

    Time: 8 ms (64.98%) | Memory: 44.9 MB (96.21%) - LeetSync

--- 1708-design-parking-system/design-parking-system.java
+++ 1708-design-parking-system/design-parking-system.java
@@ -1,45 +1,30 @@
 class ParkingSystem {
 
-    HashMap<Integer,Integer> map;
     int big,medium,small;
     public ParkingSystem(int big, int medium, int small) 
     {
-        map=new HashMap<>();
         this.big=big;
         this.medium=medium;
         this.small=small;
-        map.put(1,0);
-        map.put(2,0);
-        map.put(3,0);
     }
     
     public boolean addCar(int carType) 
     {
         if(carType==1)
         {
-            if(map.get(1)<big)
-            {
-                map.put(1,map.get(1)+1);
-                return true;
-            }
+            big--;
+            return big>=0;
         }
         else if(carType==2)
         {
-            if(map.get(2)<medium)
-            {
-                map.put(2,map.get(2)+1);
-                return true;
-            }
+            medium--;
+            return medium>=0;
         }
         else
         {
-            if(map.get(3)<small)
-            {
-                map.put(3,map.get(3)+1);
-                return true;
-            }
+            small--;
+            return small>=0;
         }
-        return false;
     }
 }
 
