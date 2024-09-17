9e1139b7d2f0441ffbdfc2412722c4be5082faee
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-09-17 14:44:27 +0530

    Time: 2 ms (51.80%) | Memory: 8.9 MB (72.35%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -2,53 +2,40 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) 
     {
-        unordered_map<string,int> map1;
-        string curr1="";
+        unordered_map<string,int> map;
+        string word="";
         for(int i=0;i<s1.length();i++)
         {
             if(s1[i]==' ')
             {
-                map1[curr1]++;
-                curr1="";
+                map[word]++;
+                word="";
                 continue;
             }
-            curr1.push_back(s1[i]);
-            if(i==s1.length()-1)
-            map1[curr1]++;
+            word.push_back(s1[i]);
         }
 
-        unordered_map<string,int> map2;
-        string curr2="";
+        map[word]++;
+
+        word="";
         for(int i=0;i<s2.length();i++)
         {
             if(s2[i]==' ')
             {
-                map2[curr2]++;
-                curr2="";
+                map[word]++;
+                word="";
                 continue;
             }
-            curr2.push_back(s2[i]);
-            if(i==s2.length()-1)
-            map2[curr2]++;
+            word.push_back(s2[i]);
         }
+
+        map[word]++;
 
         vector<string> ans;
-        for(auto x : map1)
+        for(auto x : map)
         {
-            if(!map2.count(x.first))
-            {
-                if(x.second==1)
-                ans.push_back(x.first);
-            }
-        }
-
-        for(auto x : map2)
-        {
-            if(!map1.count(x.first))
-            {
-                if(x.second==1)
-                ans.push_back(x.first);
-            }
+            if(x.second==1)
+            ans.push_back(x.first);
         }
         return ans;
     }
