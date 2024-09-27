b6bc7ac49ea78e553a7d0dbaa7e91eff235ac6c6
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-09-28 04:45:26 +0530

    Time: 120 ms (33.15%) | Memory: 142.7 MB (59.06%) - LeetSync

--- 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] resultsArray(int[][] queries, int k) 
     {
+        int index=0;
         int[] ans=new int[queries.length];
-        for(int i=0;i<Math.min(k-1,queries.length);i++)
-        ans[i]=-1;
-
-        int index=k-1;
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         for(int[] query : queries)
         {
@@ -13,8 +10,7 @@ class Solution {
             if(maxHeap.size()>k)
             maxHeap.poll();
 
-            if(maxHeap.size()==k)
-            ans[index++]=maxHeap.peek();
+            ans[index++]=(maxHeap.size()==k)?maxHeap.peek():-1;
         }
         return ans;
     }
