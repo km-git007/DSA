061d2fe106808664ee4b10cd420cce1e5954ec1b
km-git007 <160534780+km-git007@users.noreply.github.com>
2024-10-14 21:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (71.87%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.java
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -14,34 +14,40 @@
  * }
  */
 class Solution {
-    private int cameras;
-    private int solve(TreeNode root)
+    private int cameras = 0;
+    private int installCameras(TreeNode root)
     {
-        //  '-1' --> covered   '1' --> has a camera  '0' --> needs a camera
-        if(root==null)
-        return -1;
-
-        int LeftState=solve(root.left);
-        int RightState=solve(root.right);
-
-        if(LeftState==0 || RightState==0)
-        {
-            cameras++;
-            return 1;
-        }
-
-        if(LeftState==-1 && RightState==-1)
+        // 0 --> covered already   1 --> needs a camera   -1 --> has a camera
+        if(root == null)    
         return 0;
 
-        // if(LeftState==1 || RightState==1)
-        return -1;
+        int LeftResponse = installCameras(root.left);
+        int RightResponse = installCameras(root.right);
+
+        // if any of the node's child needs a camera then we will have to
+        // install the camera at this node
+        if(LeftResponse == 1 || RightResponse==1)
+        {
+            cameras++;
+            return -1;
+        }
+        // if both the node's child are already covered then node needs a camera and will ask
+        // its parent to fulfill its need.
+        if(LeftResponse == 0 && RightResponse == 0)
+        return 1;
+
+        // if any of the node's child has a camera then the node is covered by its child.
+        // if(LeftResponse == -1 || RightResponse == -1)
+        return 0;
     }
 
-    public int minCameraCover(TreeNode root) 
+    public int minCameraCover(TreeNode root)
     {
-        cameras=0;
-        if(solve(root)==0) //root=[0]
-        cameras++;   
+        // if root says that it needs a camera then we have to install the camera at the 
+        // root since it has no parent to fulfill its need.
+        if(installCameras(root) == 1)   
+        return cameras + 1;
+
         return cameras;
     }
 }
