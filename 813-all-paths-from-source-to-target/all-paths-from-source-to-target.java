bc89da415e6dd05fea996844c14287e35b2bafd8
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2024-10-26 17:39:59 +0530

    Time: 2 ms (78.34%) | Memory: 46.3 MB (52.82%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,34 +1,23 @@
 class Solution {
-    private int target;
     List<Integer> path = new ArrayList<>();
     List<List<Integer>> pathList = new ArrayList<>();
-    private void findPaths(int node, int[] vis,int[][] graph)
-    {        
-        if(node == target){
-            path.add(node);
-            pathList.add(new ArrayList<>(path));
-            path.removeLast();
-            return;
-        }
-        
+    private void findPaths(int node, int target,int[][] graph)
+    {
         path.add(node);
-        vis[node] = 1;
+
+        if(node == target)
+        pathList.add(new ArrayList<>(path));        
         
-        for(int adjNode : graph[node]){
-            if(vis[adjNode] == 0){
-                findPaths(adjNode, vis, graph);
-            }
-        }
+        for(int adjNode : graph[node])
+        findPaths(adjNode, target, graph);
         
         path.removeLast();
-        vis[node] = 0;
     }
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) 
     {
-        target = graph.length -1;
-        int[] vis = new int[graph.length];
-        findPaths(0, vis, graph);
+        int target = graph.length -1;
+        findPaths(0, target, graph);
         return pathList;
     }
 }
