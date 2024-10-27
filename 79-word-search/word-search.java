9c7bffdf0ce7d8fee9484935b2618dcc72d33238
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2024-10-27 15:05:30 +0530

    Time: 180 ms (35.99%) | Memory: 41.7 MB (40.18%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,35 +1,43 @@
 class Solution {
-    int n,m;
-    private boolean dfs(int row,int col,int index,char[][] board, String word)
+    
+    private int n, m;
+    private String targetWord;
+    private int directions[][]=new int[][]{{-1,0},{0,-1},{0,1},{1,0}};
+    private boolean dfs(int row,int col,int index,char[][] board)
     {
-        if(index==word.length())
+        if(index == targetWord.length())
         return true;
 
-        if(row<0 || row>=n || col<0 || col>=m || board[row][col]=='#' || word.charAt(index)!=board[row][col])
+        if(row < 0 || row >= n || col < 0 || col >=m || board[row][col] != targetWord.charAt(index))
         return false;
 
-        char originalCharacter=board[row][col];
-        board[row][col]='#';
+        char c = board[row][col];
+        board[row][col] = '#';
 
-        boolean up=dfs(row-1,col,index+1,board,word);
-        boolean down=dfs(row+1,col,index+1,board,word);
-        boolean right=dfs(row,col+1,index+1,board,word);
-        boolean left=dfs(row,col-1,index+1,board,word);
-
-        board[row][col]=originalCharacter;
-        return up || down || right || left;
+        for( int[] dir : directions)
+        {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if(dfs(newRow, newCol, index + 1, board))
+            return true;
+        }
+        
+        // backtrack
+        board[row][col] = c;
+        return false;
     }
 
     public boolean exist(char[][] board, String word) 
     {
         n=board.length;
         m=board[0].length;
+        targetWord = word;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(board[i][j]==word.charAt(0))
-                if(dfs(i,j,0,board,word))
+                if(dfs(i,j,0,board))
                 return true;
             }
         }
