5db38a8e16067623a730db3a4f74562f1dde9334
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2024-11-10 17:27:19 +0530

    Time: 53 ms (50.47%) | Memory: 49.3 MB (68.76%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,10 +3,8 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-
-    if(Array.isArray(obj))
-    return obj.length === 0;
-
-    return Object.entries(obj).length === 0;
-
+    for (const key in obj){
+        return false;
+    }
+    return true;
 };
