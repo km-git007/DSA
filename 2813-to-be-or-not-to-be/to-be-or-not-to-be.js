06eeee79fe1ec8622db35abb0c1d2537428cc878
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2024-11-10 17:14:25 +0530

    Time: 59 ms (18.88%) | Memory: 49.1 MB (9.31%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -4,17 +4,15 @@
  */
 var expect = function(val) {
     return {
+        
         toBe(value){
-            if(value === val)
-            return true;
-            else
-            throw new Error("Not Equal");
+            if(value === val)  return true;
+            else  throw new Error("Not Equal");
         },
+
         notToBe(value){
-            if(value !== val)
-            return true;
-            else
-            throw new Error("Equal");
+            if(value !== val)  return true;
+            else   throw new Error("Equal");
         }
     }
 };
