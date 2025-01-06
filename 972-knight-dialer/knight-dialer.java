34dc33b822999b45a779a0007ac07d6392d88d5b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-07 01:25:53 +0530

    Time: 37 ms (52.66%) | Memory: 45.2 MB (21.81%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,61 +1,46 @@
 class Solution {
-
-    private int n;
-    private final int mod = (int) 1e9 + 7;
-    int[][] directions = {
-        {-2, -1}, // Up 2, Left 1
-        {-1, -2}, // Up 1, Left 2
-        {1, -2},  // Down 1, Left 2
-        {2, -1},  // Down 2, Left 1
-        {2, 1},   // Down 2, Right 1
-        {1, 2},   // Down 1, Right 2
-        {-1, 2},  // Up 1, Right 2
-        {-2, 1}   // Up 2, Right 1
+    int[][] memo;
+    int n;
+    int MOD = (int) 1e9 + 7;
+    int[][] jumps = {
+        {4, 6},
+        {6, 8},
+        {7, 9},
+        {4, 8},
+        {3, 9, 0},
+        {},
+        {1, 7, 0},
+        {2, 6},
+        {1, 3},
+        {2, 4}
     };
-    private int[][][] dp;
-    private int solve(int row, int col, int jumps, int[][] board)
-    {
-        if(row < 0 || row >= 4 || col < 0 || col >=3 || board[row][col] == 1)
-        return 0;
-
-        if(jumps == n - 1)
-        return 1;
-
-        if(dp[row][col][jumps] != -1)
-        return dp[row][col][jumps];
-
-        int ways = 0;
-        for(int[] dir : directions)
-        {
-            int r = row + dir[0];
-            int c = col + dir[1];
-            ways = (ways + solve(r, c, jumps + 1, board) % mod) % mod;
+    
+    public int dp(int remain, int square) {
+        if (remain == 0) {
+            return 1;
         }
-        return dp[row][col][jumps] = ways;
+        
+        if (memo[remain][square] != 0) {
+            return memo[remain][square];
+        }
+        
+        int ans = 0;
+        for (int nextSquare : jumps[square]) {
+            ans = (ans + dp(remain - 1, nextSquare)) % MOD;
+        }
+        
+        memo[remain][square] = ans;
+        return ans;
     }
-
-    public int knightDialer(int N) 
-    {
-        int[][] board = new int[4][3];
-        // * and # are obstacles
-        board[3][0] = 1; 
-        board[3][2] = 1; 
-
-        n = N;
-        dp = new int [4][3][N];
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            Arrays.fill(dp[i][j], -1);
+    
+    public int knightDialer(int n) {
+        this.n = n;
+        memo = new int[n + 1][10];
+        int ans = 0;
+        for (int square = 0; square < 10; square++) {
+            ans = (ans + dp(n - 1, square)) % MOD;
         }
-
-        int possiblePhoneNumbers = 0;
-        for(int i = 0; i < 4; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            if(board[i][j] == 0)
-            possiblePhoneNumbers = (possiblePhoneNumbers + solve(i, j, 0, board) % mod) % mod;
-        }
-        return possiblePhoneNumbers;
+        
+        return ans;
     }
 }
