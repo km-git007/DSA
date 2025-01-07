5823e0950d1d62962b2280ce8fbbb75740b5d91b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-08 03:29:04 +0530

    Time: 1 ms (81.26%) | Memory: 41.4 MB (92.64%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,36 +1,23 @@
 class Solution {
-    private int[] dp;
-    private int solve(String s,int index)
-    {
-        if(index == s.length())
-        return 1;
-
-        if(s.charAt(index) == '0')
-        return 0;
-
-        if(dp[index] != -1)
-        return dp[index];
-
-        int ways = solve(s, index + 1);
-
-        if(index + 1 < s.length() && Integer.parseInt(s.substring(index, index + 2)) <= 26)
-        ways += solve(s, index + 2);
-
-        return dp[index] = ways;
-    }
-
     public int numDecodings(String s) 
     {
         int n = s.length();
-        dp = new int[n];
-        Arrays.fill(dp, -1);
+        int[] dp = new int[n + 1];
 
-        // if(s.charAt(0) != '0')
-        // dp[0] = 1;
+        // empty string can be decoded in one way
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(s.charAt(i) == '0')
+            dp[i] = 0;
+            else
+            {
+                dp[i] = dp[i + 1];
 
-        // if(n > 1 && s.charAt(1) != '0')
-
-
-        return solve(s, 0);
+                if(i + 1 < n && Integer.parseInt(s.substring(i, i + 2)) <= 26)
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
     }
 }
