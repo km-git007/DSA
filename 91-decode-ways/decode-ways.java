bd633712e29c2bb15d50ab2085ce551d6e05b5ed
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-08 03:33:07 +0530

    Time: 1 ms (81.26%) | Memory: 42 MB (41.32%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,22 +2,23 @@ class Solution {
     public int numDecodings(String s) 
     {
         int n = s.length();
-        int[] dp = new int[n + 1];
-
         // empty string can be decoded in one way
-        dp[n] = 1;
+        int plusOne = 1;
+        int plusTwo = -1, curr;
         for(int i = n - 1; i >= 0; i--)
         {
             if(s.charAt(i) == '0')
-            dp[i] = 0;
+            curr = 0;
             else
             {
-                dp[i] = dp[i + 1];
+                curr = plusOne;
 
                 if(i + 1 < n && Integer.parseInt(s.substring(i, i + 2)) <= 26)
-                dp[i] += dp[i + 2];
+                curr += plusTwo;
             }
+            plusTwo = plusOne;
+            plusOne = curr;
         }
-        return dp[0];
+        return plusOne;
     }
 }
