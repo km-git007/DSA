3374b17efc1c5243d544eed0b24576aadda50348
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-10 17:48:39 +0530

    Time: 15 ms (84.47%) | Memory: 61.8 MB (84.48%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+         int maxCount[26] = {0}; // To store the maximum frequency of each character needed
 
-    unordered_map<int,int> buildMap(string &word)
-    {
-        unordered_map<int,int> map;
-        for(char c : word)
-        map[c - 'a']++;
-        return map;
-    }
-
-    vector<int> buildMaxFreqMap(vector<string>& words)
-    {
-        vector<int> maxFreq(26, 0);
-        for(string word : words)
-        {
-            auto map = buildMap(word);
-            for(auto x : map)
-            if(maxFreq[x.first] < x.second)
-            maxFreq[x.first] = x.second;
-        }
-        return maxFreq;
-    }
-
-    bool check(vector<int> &maxFreq, string &word)
-    {
-        auto map = buildMap(word);
-        for(int i = 0; i < 26; i++)
-        {
-            if(maxFreq[i])
-            {
-                if(!map.count(i) || map[i] < maxFreq[i])
-                return false;
+        // Calculate the maximum frequency of each character needed from words2
+        for (const auto& word : words2) {
+            int count[26] = {0}; // Frequency count for the current word in words2
+            for (char ch : word) {
+                count[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; ++i) {
+                maxCount[i] = std::max(maxCount[i], count[i]);
             }
         }
-        return true;
-    }
 
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) 
-    {
-        // build a maxFreq map from words2
-        auto maxFreq = buildMaxFreqMap(words2);
+        std::vector<std::string> result; // To store the universal words
 
-        vector<string> res;
-        for(string word : words1)
-        {
-            if(check(maxFreq, word))
-            res.push_back(word);
+        // Check each word in words1
+        for (const auto& word : words1) {
+            int count[26] = {0}; // Frequency count for the current word in words1
+            for (char ch : word) {
+                count[ch - 'a']++;
+            }
+
+            // Check if this word can cover all character requirements from words2
+            bool isUniversal = true;
+            for (int i = 0; i < 26; ++i) {
+                if (count[i] < maxCount[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+
+            if (isUniversal) {
+                result.push_back(word);
+            }
         }
-        return res;
+
+        return result;
     }
 };
