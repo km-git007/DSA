e9d32dd3d6848b2902d5ce6ba0dde87378a0e202
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-16 04:26:54 +0530

    Time: 102 ms (80.30%) | Memory: 126.5 MB (68.33%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dfs(int id, vector<int>& informTime, vector<vector<int>> &adj)
+    {
+        int minTime = 0;
+        for(int subOr : adj[id])
+        {
+            minTime = max(minTime, informTime[id] + dfs(subOr, informTime, adj));
+        }
+        return minTime;
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) 
+    {
+        vector<vector<int>> adj(n);
+        for(int i = 0; i < manager.size(); i++)
+        {
+            if(i != headID)
+            adj[manager[i]].push_back(i);
+        }
+
+        return dfs(headID, informTime, adj);
+    }
+};
