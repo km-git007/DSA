b946689ea5d1a9d75ed36545e064472aacc1e97b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-18 04:21:09 +0530

    Time: 44 ms (51.86%) | Memory: 46.6 MB (63.93%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,32 @@
 class Solution {
-    private int[][] dp;
-    private int solve(int[] nums, int index, int target)
-    {
-        if(target == 0)
-        return 1;
-
-        if(target < 0 || index < 0)
-        return 0;
-
-        if(dp[index][target] != -1)
-        return dp[index][target];
-
-        int take = solve(nums, index - 1, target - nums[index]);
-        int notTake = solve(nums, index - 1, target);
-
-        if(take > 0 || notTake > 0)
-        return dp[index][target] = 1;
-
-        return dp[index][target] = 0;
-    }
-
     public boolean canPartition(int[] nums) 
     {
-        int sum = Arrays.stream(nums).sum();
-        if(sum % 2 != 0)
+        int Sum = Arrays.stream(nums).sum();
+        // return false if Sum not divisible by '2'
+        if(Sum % 2 != 0)
         return false;
 
-        dp = new int[nums.length + 1][sum / 2 + 1];
-        for(int i = 0; i < nums.length + 1; i++)
-        Arrays.fill(dp[i], -1);
+        int m = Sum / 2;
+        int n = nums.length;
 
-        return solve(nums, nums.length - 1, sum / 2) == 1;
+        // create dp array
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        // fill the first column
+        for(int i = 0; i < n + 1; i++)
+        dp[i][0] = true;
+
+        for(int i = 1; i < n + 1; i++)
+        {
+            for(int j = 1; j < m + 1; j++)
+            {
+                if(j - nums[i - 1] >= 0)
+                dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+
+                else
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 }
