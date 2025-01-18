8e25f41df9219ddd218ab174262d9eeff201f5bb
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-19 04:39:24 +0530

    Time: 1544 ms (5.03%) | Memory: 244.5 MB (5.71%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -6,12 +6,9 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        // create time array
-        // vector<vector<int>> time(n, vector<int>(m, 1e9));
-        // time to reach source is '0'
-        // time[0][0] = 0;
-
+        // creating a vis array
         vector<vector<int>> vis(n, vector<int>(m, 0));
+
         // Min-Heap of vectors (lexicographical order) {time, row, col}
         priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
         pq.push({0, 0, 0});
@@ -21,12 +18,12 @@ public:
             auto v = pq.top();
             pq.pop();
 
-            int currTime = v[0];
+            int time = v[0];
             int row = v[1];
             int col = v[2];
 
             if(row == n - 1 && col == m - 1)
-            return currTime;
+            return time;
 
             if(vis[row][col])
             continue;
@@ -39,12 +36,12 @@ public:
                 if(r < 0 || r >= n || c < 0 || c >= m)
                 continue;
 
-                if(currTime + 1 >= grid[r][c])
-                pq.push({currTime + 1, r, c});
+                if(time + 1 >= grid[r][c])
+                pq.push({time + 1, r, c});
 
                 else
                 {
-                    int diff = grid[r][c] - currTime;
+                    int diff = grid[r][c] - time;
 
                     if(diff % 2 == 0)
                     pq.push({grid[r][c] + 1, r, c});
@@ -53,7 +50,7 @@ public:
                     pq.push({grid[r][c], r, c});
                 }
             }
-
+            // mark the cell as visited after exploring all the neighbours
             vis[row][col] = 1;
         }
         return - 1;
