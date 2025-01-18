769ddaf5c230ff35def34b9a76602d4a61cab23e
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-19 03:21:45 +0530

    Time: 1884 ms (19.48%) | Memory: 201.7 MB (54.46%) - LeetSync

---
+++ 3386-find-edges-in-shortest-paths/find-edges-in-shortest-paths.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    void buildGraph(vector<vector<pair<int,int>>> &adj, vector<vector<int>>& edges)
+    {
+        for(auto edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+    }
+
+    vector<int> dijkstrasAlgo(int n, int source, vector<vector<pair<int,int>>> &adj)
+    {
+        // create a distance array
+        vector<int> dist(n, 1e9);
+        dist[source] = 0;
+
+        // Min-heap of pair {wt, node}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, source});
+
+        while(!pq.empty())
+        {
+            auto p = pq.top();
+            pq.pop();
+
+            int node = p.second;
+            int currDist = p.first;
+
+            for(auto adjNode : adj[node])
+            {
+                int neighbour = adjNode.first;
+                int wt = adjNode.second;
+                if(dist[neighbour] > currDist + wt)
+                {
+                    dist[neighbour] = currDist + wt;
+                    pq.push({dist[neighbour], neighbour});
+                }
+            }
+        }
+        return dist;
+    }
+
+    vector<bool> findAnswer(int n, vector<vector<int>>& edges) 
+    {
+        vector<vector<pair<int,int>>> adj(n);
+        // build adj list
+        buildGraph(adj, edges);
+
+        vector<int> distFromSource = dijkstrasAlgo(n, 0, adj);
+        vector<int> distFromDest = dijkstrasAlgo(n, n - 1, adj);
+
+        int m = edges.size();
+        vector<bool> res(m, false);
+        for(int i = 0; i < m; i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+
+            if(distFromSource[u] + wt + distFromDest[v] == distFromSource[n - 1])
+            res[i] = true;
+
+            if(distFromSource[v] + wt + distFromDest[u] == distFromSource[n - 1])
+            res[i] = true;
+        }
+
+        return res;
+    }
+};
