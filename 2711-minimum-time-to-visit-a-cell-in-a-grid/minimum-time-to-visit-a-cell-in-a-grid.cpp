2ae3d53815d240fbf040101deca7b3f501396343
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-19 04:43:13 +0530

    Time: 146 ms (69.96%) | Memory: 53.8 MB (77.38%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,66 +1,53 @@
+using info = tuple<int, short, short>; // (time, i, j)
+const static int d[5] = {0, 1, 0, -1, 0};
 class Solution {
 public:
-    vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    int solve(vector<vector<int>>& grid)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        // creating a vis array
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-
-        // Min-Heap of vectors (lexicographical order) {time, row, col}
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        pq.push({0, 0, 0});
-
-        while(!pq.empty())
-        {
-            auto v = pq.top();
-            pq.pop();
-
-            int time = v[0];
-            int row = v[1];
-            int col = v[2];
-
-            if(row == n - 1 && col == m - 1)
-            return time;
-
-            if(vis[row][col])
-            continue;
-
-            for(int i = 0; i < directions.size(); i++)
-            {
-                int r = row + directions[i].first;
-                int c = col + directions[i].second;
-
-                if(r < 0 || r >= n || c < 0 || c >= m)
-                continue;
-
-                if(time + 1 >= grid[r][c])
-                pq.push({time + 1, r, c});
-
-                else
-                {
-                    int diff = grid[r][c] - time;
-
-                    if(diff % 2 == 0)
-                    pq.push({grid[r][c] + 1, r, c});
-
-                    else
-                    pq.push({grid[r][c], r, c});
-                }
-            }
-            // mark the cell as visited after exploring all the neighbours
-            vis[row][col] = 1;
-        }
-        return - 1;
+    inline static bool isOutside(short i, short j, short n, short m) {
+        return i < 0 || i >= n || j < 0 || j >= m;
     }
 
-    int minimumTime(vector<vector<int>>& grid) 
-    {
-        if(grid[0][1] > 1 && grid[1][0] > 1)
-        return -1;
+    int minimumTime(vector<vector<int>>& grid) {
+        if (grid[1][0] > 1 && grid[0][1] > 1)
+            return -1; // edge case
 
-        return solve(grid);
+        short n = grid.size(), m = grid[0].size();
+        vector<vector<int>> time(n, vector<int>(m, INT_MAX));
+        priority_queue<info, vector<info>, greater<info>> pq;
+
+        // Start at (0, 0) with time=0
+        pq.emplace(0, 0, 0);
+        time[0][0] = 0;
+        while (!pq.empty()) {
+            auto [t, i, j] = pq.top();
+            pq.pop();
+            // reach the destination
+            if (i == n - 1 && j == m - 1)
+                return t;
+
+            // Traverse all four directions
+            for (int a = 0; a < 4; a++) {
+                int r = i + d[a], s = j + d[a + 1];
+                if (isOutside(r, s, n, m))
+                    continue;
+
+                // minimum time to reach (r, s)
+                int wait_time = grid[r][s];
+                int next_time = t + 1;
+
+                // Wait until we can step on this cell
+                if (next_time < wait_time) {
+                    int diff = wait_time - next_time;
+                    next_time += (diff % 2 == 0) ? diff : diff + 1;
+                }
+
+                // update if this path having quicker time
+                if (next_time < time[r][s]) {
+                    time[r][s] = next_time;
+                    pq.emplace(next_time, r, s);
+                }
+            }
+        }
+
+        return -1; // never reach
     }
 };
