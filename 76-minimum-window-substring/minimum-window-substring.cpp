4e8f9bdde4742771f1261de1e80410da0419c032
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-20 21:07:19 +0530

    Time: 14 ms (44.06%) | Memory: 11.4 MB (74.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     string minWindow(string s, string t) 
     {
-        if(t.length() > s.length())
+        if(s.length() < t.length())
         return "";
 
         unordered_map<char,int> map;
         for(char c : t)
         map[c]++;
 
-        int i = 0, count = map.size(), startIndex = -1, minLength = s.length();
+        int count = map.size(), i = 0;
+        int startIndex = -1, minLength = s.length();
         for(int j = 0; j < s.length(); j++)
         {
-            if(map.count(s[j]) > 0)
+            if(map.count(s[j]))
             {
                 map[s[j]]--;
                 if(map[s[j]] == 0)
@@ -27,15 +28,16 @@ public:
                     minLength = j - i + 1;
                 }
 
-                if(map.count(s[i]) > 0)
+                if(map.count(s[i]))
                 {
                     map[s[i]]++;
-                    if(map[s[i]] > 0)
+                    if(map[s[i]] == 1)
                     count++;
                 }
+
                 i++;
             }
         }
-        return (startIndex == -1) ? "" : s.substr(startIndex, minLength);
+        return startIndex == -1 ? "" : s.substr(startIndex, minLength);
     }
 };
