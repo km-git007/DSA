94922a0fda0033b3049baf47a26b3012a843b300
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-23 20:54:47 +0530

    Time: 7 ms (58.95%) | Memory: 10.3 MB (95.05%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,25 +4,32 @@ public:
     {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-         // Fill the first row and column (base cases)
-        for (int i = 0; i <= n; i++) dp[i][0] = i; // Deleting characters from `s`
-        for (int j = 0; j <= m; j++) dp[0][j] = j; // Inserting characters into `s`
+        vector<int> curr(m + 1, 0);
+        vector<int> prev(m + 1, 0);
+
+        // Fill the first row
+        // Inserting characters into `s`
+        for (int j = 0; j <= m; j++) curr[j] = j; 
+        prev = curr;
 
         for(int i = 1; i < n + 1; i++)
         {
-            for(int j = 1; j < m + 1; j++)
+            for(int j = 0; j < m + 1; j++)
             {
+                if(j == 0)
+                curr[j] = i;
+
                 // characters match
-                if(s[i - 1] == t[j - 1])
-                dp[i][j] = dp[i - 1][j - 1];
+                else if(s[i - 1] == t[j - 1])
+                curr[j] = prev[j - 1];
 
                 // characters don't match
                 else
-                dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+                curr[j] = 1 + min({prev[j - 1], curr[j - 1], prev[j]});
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
