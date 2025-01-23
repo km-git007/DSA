f770c74e2288789666ce67c050220a9673b4c914
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-23 18:40:42 +0530

    Time: 2 ms (60.10%) | Memory: 25.9 MB (96.85%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -5,28 +5,54 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<int> rows(n, 0), cols(m, 0);
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i < grid.size(); i++) 
         {
-            for(int j = 0; j < m; j++)
+            // Initialize variables for rows
+            int x = -1, y = -1; 
+            for(int j = 0; j < grid[0].size(); j++) 
             {
-                if(grid[i][j])
-                {
-                    cols[j]++;
-                    rows[i]++;
+                if(grid[i][j] != 0) 
+                {   // Check if there's a server or marked server
+                    if(x == -1 && y == -1) 
+                    {
+                        // Store position of the first server in this row
+                        x = i; y = j; 
+                    } else 
+                    {   // Mark both as communicating
+                        grid[x][y] = 2; 
+                        grid[i][j] = 2;
+                    }
+                }
+            }
+        }
+
+        for(int j = 0; j < grid[0].size(); j++) 
+        {
+            // Initialize variables for columns
+            int x = -1, y = -1; 
+            for(int i = 0; i < grid.size(); i++) 
+            {
+                if(grid[i][j] != 0) 
+                {   // Check if there's a server or marked server
+                    if(x == -1 && y == -1) 
+                    {
+                        // Store position of the first server in this column
+                        x = i; y = j; 
+                    } else 
+                    {   // Mark both as communicating
+                        grid[x][y] = 2; 
+                        grid[i][j] = 2;
+                    }
                 }
             }
         }
 
         int servers = 0;
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(grid[i][j] && (rows[i] > 1 || cols[j] > 1))
-                servers++;
-            }
-        }
+        for(int i = 0; i < grid.size(); i++)
+        for(int j = 0; j < grid[i].size(); j++)
+        // Increment count for each communicating server
+        if(grid[i][j] == 2)
+        servers++; 
 
         return servers;
     }
