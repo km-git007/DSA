f07895320cc77be13f7e5666842f2cfcca866285
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-27 19:54:19 +0530

    Time: 7 ms (53.65%) | Memory: 19.8 MB (53.04%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
+    vector<int> vis, pathVis;
+    bool hasCycle(int node, vector<int> adj[])
+    {
+        if(vis[node] && pathVis[node])
+        return true;
 
-    bool canFinish(int v, vector<vector<int>>& prerequisites) 
+        if(vis[node])
+        return false;
+
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for(int adjNode : adj[node])
+        {
+            if(hasCycle(adjNode, adj))
+            return true;
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+    bool canFinish(int V, vector<vector<int>>& prerequisites) 
     {
         // creating adj list.
-        vector<int> adj[v];
-
-	    // creating indegree vector.
-	    vector<int> ind(v,0);
+        vector<int> adj[V];
 
         for(auto x : prerequisites)
-        {
-            adj[x[1]].push_back(x[0]);
-            ind[x[0]]++;
-        }
+        adj[x[1]].push_back(x[0]);
         
-        queue<int> q;
-        // pushing nodes having zero indegree.
-	    for(int i=0;i<v;i++)        
-	    {
-	        if(ind[i]==0)
-	        q.push(i);
-	    }
+        // vis and pathVis vectors
+        vis = pathVis = vector<int>(V, 0);
 
-        int count=0;
-	    while(!q.empty())
-	    {
-	        int node=q.front();
-	        q.pop();
-            count++;
-	        for(int x: adj[node])
-	        {
-	            ind[x]--;           // decreasing indegree by 1.
-	            if(ind[x]==0)       // if indegree becomes zero. push the node in the queue.
-	            q.push(x);
-	        }
-	    }
-	    return count == v;
+        for(int i = 0; i < V; i++) 
+        {
+            if(!vis[i]) 
+            {
+                if(hasCycle(i, adj))
+                return false; // Cycle detected so can't finish the courses
+            }
+        }
+
+        return true;
     }
 };
