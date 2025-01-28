49f69bff2c4b9085edf1e81249eb72334682b443
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-28 09:12:34 +0530

    Time: 43 ms (11.38%) | Memory: 103.7 MB (27.85%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<int>> vis;
     vector<vector<int>> directions = {{-1,0},{0,-1},{0,1},{1,0}};
     int dfs(int row, int col, vector<vector<int>>& grid)
     {
-        if(row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0 || vis[row][col])
+        if(row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0)
         return 0;
 
-        vis[row][col] = 1;
-
+        // collect fish
         int fish = grid[row][col];
+
+        // mark as vis
+        grid[row][col] =0;
+
         for(auto dir : directions)
         {
             int r = row + dir[0];
@@ -25,14 +27,13 @@ public:
     {
         n = grid.size();
         m = grid[0].size();
-        vis = vector<vector<int>> (n, vector<int>(m, 0));
 
         int maxFish=0;
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
-                if(grid[i][j] && !vis[i][j])
+                if(grid[i][j])
                 maxFish = max(maxFish, dfs(i, j, grid));
             }
         }
