e7949a10d4b52a4bc03170f2cf97a1baf03acf82
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-31 02:42:33 +0530

    Time: 3 ms (60.89%) | Memory: 41.5 MB (87.52%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String smallestSubsequence(String s) 
+    {
+        boolean[] taken = new boolean[26];
+        int[] lastIndex = new int[26];
+
+        for(int i = 0; i < s.length(); i++)
+        lastIndex[s.charAt(i) - 'a'] = i;
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) 
+        {
+            // Get the current character
+            char ch = s.charAt(i);
+
+            if(taken[ch - 'a'])
+            continue;
+            
+            // Check if the stack is not empty and if the currChar is less than top 
+            // and topChar is present afterwards
+            while(!stack.isEmpty() && ch - 'a' < stack.peek() && lastIndex[stack.peek()] > i) 
+            taken[stack.pop()] = false;
+            
+            // Push curr char
+            stack.push(ch - 'a'); 
+            taken[ch - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        // Poll from the end for correct order
+        while(!stack.isEmpty())
+        sb.append((char)(stack.pollLast() + 'a'));
+        
+        return sb.toString();
+    }
+}
