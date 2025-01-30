fe93fd93e77e165ae273650d412cfcc5054dde0e
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-01-31 02:40:09 +0530

    Time: 5 ms (97.41%) | Memory: 45.9 MB (87.82%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) 
     {
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         int []res = new int[nums.length];
-        for(int i=2*nums.length-1;i>=0;i--)
+        for(int i = 2 * nums.length - 1; i >= 0; i--)
         {
-            int index=i%nums.length;
-            while(!stack.isEmpty() && nums[index]>=stack.peek())
+            int index = i % nums.length;
+            while(!stack.isEmpty() && nums[index] >= stack.peek())
             stack.pop();
 
             if(stack.isEmpty())
-            res[index]=-1;
+            res[index] = -1;
+
             else
-            res[index]=stack.peek();
+            res[index] = stack.peek();
 
             stack.push(nums[index]);
         }
