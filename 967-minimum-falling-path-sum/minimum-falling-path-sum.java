ef66371909bb17ff175507b36b875e755939b72b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-02 04:56:18 +0530

    Time: 16 ms (5.60%) | Memory: 45.7 MB (28.39%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -4,29 +4,33 @@ class Solution {
     {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] dp = new int[n][m];
+        
+        List<Integer> prev = new ArrayList<>();
 
         // initialize the first column
         for(int j = 0; j < m; j++)
-        dp[0][j] = grid[0][j];
+        prev.add(grid[0][j]);
 
         int INF = Integer.MAX_VALUE/2;
-        for(int i = 1; i < m; i++)
+        for(int i = 1; i < n; i++)
         {
+            // create a new list for the current row dp values
+            List<Integer> curr = new ArrayList<>(Collections.nCopies(m, 0));
             for(int j = 0; j < m; j++)
             {
-                int left = j - 1 >= 0 ? dp[i - 1][j - 1] : INF;
-                int right = j + 1 < m ? dp[i - 1][j + 1] : INF;
-                int up = dp[i - 1][j];
+                int left = j - 1 >= 0 ? prev.get(j - 1) : INF;
+                int right = j + 1 < m ? prev.get(j + 1) : INF;
+                int up = prev.get(j);
 
-                dp[i][j] = grid[i][j] + Math.min(up, Math.min(left, right));
+                curr.set(j, grid[i][j] + Math.min(up, Math.min(left, right)));
             }
+            prev = curr;
         }
 
         // find the minimum from the last row
-        int res = dp[n - 1][0];
+        int res = prev.get(0);
         for(int j = 1; j < m; j++)
-        res = Math.min(res, dp[n - 1][j]);
+        res = Math.min(res, prev.get(j));
 
         return res;
     }
