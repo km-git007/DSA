07d04c4e92b1b6b5dd625ddc6813ff67ec894ab3
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-01 16:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (23.02%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -3,7 +3,7 @@ class Solution {
     {
         for(int i = 1; i < nums.length; i++)
         {
-            if(Math.abs(nums[i] % 2 - nums[i - 1] % 2) == 0)
+            if(nums[i] % 2 == nums[i - 1] % 2)
             return false;
         }
 
