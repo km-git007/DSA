b7a04996de2fc0de5ed17bbc4673b9079e22ec09
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-02 15:37:30 +0530

    Time: 84 ms (6.11%) | Memory: 87.4 MB (5.34%) - LeetSync

--- 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.java
+++ 3525-maximum-energy-boost-from-two-drinks/maximum-energy-boost-from-two-drinks.java
@@ -1,47 +1,31 @@
-
 class Solution {
-    int n;
-    long[][] dp;
-    long solve(int index, int drinkA, int[] energyDrinkA, int[] energyDrinkB) 
+    private Long[][] dp;
+    private long solve(int index, int canDrinkB, int[] energyDrinkA, int[] energyDrinkB)
     {
-        if (index >= n)
+        if(index >= energyDrinkA.length)
         return 0;
 
-        if (dp[index][drinkA] != -1) 
-        return dp[index][drinkA];
+        if(dp[index][canDrinkB] != null)
+        return dp[index][canDrinkB];
 
-        long maxEnergy = 0;
-        if(drinkA==1) 
+        long switchDrink = -1, notSwitch = -1;
+        if(canDrinkB == 0)
         {
-            // drink A and not switch
-            long A1 = energyDrinkA[index] + solve(index + 1, 1, energyDrinkA, energyDrinkB);
-            // drink A and switch
-            long A2 = energyDrinkA[index] + solve(index + 2, 0, energyDrinkA, energyDrinkB);
-            maxEnergy = Math.max(A1, A2);
+            notSwitch = energyDrinkA[index] + solve(index + 1, 0, energyDrinkA, energyDrinkB);
+            switchDrink = energyDrinkA[index] + solve(index + 2, 1, energyDrinkA, energyDrinkB);
         }
-        else 
+        else
         {
-            // drink B and not switch
-            long B1 = energyDrinkB[index] + solve(index + 1, 0, energyDrinkA, energyDrinkB);
-            // drink B and switch
-            long B2 = energyDrinkB[index] + solve(index + 2, 1, energyDrinkA, energyDrinkB);
-            maxEnergy = Math.max(B1, B2);
+            notSwitch = energyDrinkB[index] + solve(index + 1, 1, energyDrinkA, energyDrinkB);
+            switchDrink = energyDrinkB[index] + solve(index + 2, 0, energyDrinkA, energyDrinkB);
         }
-        return dp[index][drinkA] =maxEnergy;
+        
+        return dp[index][canDrinkB] = Math.max(notSwitch, switchDrink);
     }
 
     public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) 
     {
-        n = energyDrinkA.length;
-        dp = new long[n][2];
-        for (long[] row : dp)
-        Arrays.fill(row, -1);
-
-        // maxEnergyBoost gained when you start with A
-        long startWithA = solve(0, 1, energyDrinkA, energyDrinkB);
-        // maxEnergyBoost gained when you start with B
-        long startWithB = solve(0, 0, energyDrinkA, energyDrinkB);
-        // take max of the two values
-        return Math.max(startWithA, startWithB);
+        dp = new Long[energyDrinkA.length][2];
+        return Math.max(solve(0, 0, energyDrinkA, energyDrinkB), solve(0, 1, energyDrinkA, energyDrinkB));
     }
 }
