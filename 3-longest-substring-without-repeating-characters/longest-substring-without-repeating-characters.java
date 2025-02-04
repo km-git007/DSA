d1f999e743d93ba3ca565afde944e1325a7d2d39
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-04 18:11:52 +0530

    Time: 8 ms (21.61%) | Memory: 44.4 MB (79.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,7 +6,7 @@ class Solution {
         for(int j = 0; j < s.length(); j++)
         {
             map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-            while(map.size() < j - i + 1)
+            if(map.size() < j - i + 1)
             {
                 // remove 1 occurance the ith number from the map
                 map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
@@ -18,6 +18,7 @@ class Solution {
                 i++;
             }
 
+            if(map.size() == j - i + 1)
             maxLength = Math.max(maxLength, j - i + 1);
         }
         return maxLength;
