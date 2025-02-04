a8085b12093e104c25659250fe0a9b8316346d79
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-04 20:30:09 +0530

    Time: 3 ms (72.81%) | Memory: 11.8 MB (70.85%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     int dp[301];
     unordered_set<string> set;
-    int solve(string &s,int start)
+    int solve(string &s, int start)
     {
-        if(start==s.length())
-        return 1;
+        if(start == s.length())
+        return true;
 
-        if(dp[start]!=-1)
+        if(dp[start] != -1)
         return dp[start];
 
         string curr="";
-        for(int i=start;i<s.length();i++)
+        for(int i = start; i < s.length(); i++)
         {
             curr.push_back(s[i]);
-            if(set.count(curr))
-            {
-                if(solve(s,i+1))
-                return dp[start]=1;
-            }
+            if(set.count(curr) && solve(s,i+1))
+            return 1;
         }
+        // return false
         return dp[start]=0;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) 
     {
         memset(dp,-1,sizeof(dp));
+
         for(string word : wordDict)
         set.insert(word);
         
