3c46ea9fc9b263fceb6a6c19579ffbb2ef60bf0a
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-04 17:37:29 +0530

    Time: 55 ms (20.04%) | Memory: 54.7 MB (95.81%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int totalFruit(int[] fruits) 
+    {
+        int maxi = 0, i = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int j = 0; j < fruits.length; j++)
+        {
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            while(map.size() > 2)
+            {
+                // remove 1 quantity the ith fruit from the basket
+                map.put(fruits[i], map.get(fruits[i]) - 1);
+
+                // if no more quantity of the its fruit is left than remove it from the map
+                if(map.get(fruits[i]) == 0)
+                map.remove(fruits[i]);
+
+                i++;
+            }
+            maxi = Math.max(maxi, j - i + 1);
+        }
+        return maxi;
+    }
+}
