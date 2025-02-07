96dc26b4365db4c87f882d17a4ad1dde9b492488
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-07 20:21:20 +0530

    Time: 50 ms (57.79%) | Memory: 57.6 MB (88.92%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,33 +1,36 @@
 class Solution {
     
-    public boolean isValid(int[] time,int totalTrips,long allotedTime)
+    public boolean isValid(int[] time, int totalTrips, long allotedTime)
     {
-        long trips=0;
-        for(int i=0;i<time.length;i++)
-        {
-            trips+=allotedTime/time[i];
-            if(trips>=totalTrips)
+       long tripsDone = 0;
+       for(int i = 0; i < time.length; i++)
+       {
+            tripsDone += (allotedTime / time[i]);
+            if(tripsDone >= totalTrips)
             return true;
-        }
-        return false;
+       }
+       return false;
     }
 
     public long minimumTime(int[] time, int totalTrips) 
     {
-        long end=Long.MAX_VALUE;
-        long start=Arrays.stream(time).min().orElse(0);  // Find the minimum element in time
+        long end = Long.MAX_VALUE;
+        long start = Arrays.stream(time).min().orElse(0);  // Find the minimum element in time
 
-        long minimumTimeRequired=end;
-        while(start<=end)
+        long minimumTimeRequired = end;
+        // checking if this amount of time is fine
+        while(start <= end)
         {
-            long mid=start+(end-start)/2;
-            if(isValid(time,totalTrips,mid))
+            long mid = start + (end-start)/2;
+            // checking if trips can be done in time lesser than what we took right now
+            if(isValid(time, totalTrips,mid))
             {
-                minimumTimeRequired=mid;
-                end=mid-1;
+                minimumTimeRequired = mid;
+                end = mid-1;
             }
+            // means we need to increase the amont of time
             else
-                start=mid+1;
+            start = mid + 1;
         }  
         return minimumTimeRequired;
     }
