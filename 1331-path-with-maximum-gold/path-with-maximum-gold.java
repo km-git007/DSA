50f0ea6837d7a5542fd395fafbdfeb64839e0068
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-12 20:44:40 +0530

    Time: 85 ms (33.90%) | Memory: 41.4 MB (41.48%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,32 +1,32 @@
 class Solution {
-    int n,m; 
-    int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
+    int n, m; 
+    int[][] directions ={{0,1},{0,-1},{1,0},{-1,0}};
     private int solve(int row,int col,int[][] grid)
     {
-        if(row<0 || row>=n || col<0 || col>=m || grid[row][col]==0)
+        if(row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0)
         return 0;
 
-        int gold=grid[row][col];
-        grid[row][col]=0;   // marking the cell as visited.
+        int gold = grid[row][col];
+        grid[row][col] = 0;   // marking the cell as visited.
 
-        int maxGold=0; 
-        for(int i=0;i<4;i++)
-        maxGold = Math.max(maxGold,solve(row + dir[i][0], col + dir[i][1], grid));
+        int maxGold = 0; 
+        for(int[] dir : directions)
+        maxGold = Math.max(maxGold,solve(row + dir[0], col + dir[1], grid));
 
-        grid[row][col]=gold; // unmarking the cell.
+        grid[row][col] = gold; // unmarking the cell.
         return maxGold+gold;
     }
 
     public int getMaximumGold(int[][] grid) 
     {
-        n=grid.length;
-        m=grid[0].length;
-        int maximumGold=0;
+        n = grid.length;
+        m = grid[0].length;
+        int maximumGold = 0;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            if(grid[i][j]>0)
-            maximumGold=Math.max(maximumGold,solve(i,j,grid));
+            for(int j = 0; j < m; j++)
+            if(grid[i][j] > 0)
+            maximumGold = Math.max(maximumGold, solve(i,j,grid));
         }
         return maximumGold;
     }
