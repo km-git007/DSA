9c3b65e297f22a59b9939a0f67655ce7c344e375
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-14 03:01:52 +0530

    Time: 30 ms (28.66%) | Memory: 43.4 MB (35.21%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkInclusion(String p, String s) 
+    {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < p.length(); i++)
+        map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
+
+        int i = 0, count = map.size();
+        List<Integer> res = new ArrayList<>();
+        for(int j = 0; j < s.length(); j++)
+        {
+            char ch = s.charAt(j);
+            if(map.containsKey(ch))
+            {
+                map.put(ch, map.getOrDefault(ch, 0) - 1);
+                if(map.get(ch) == 0)
+                count--;
+            }
+
+            if(j - i + 1 == p.length())
+            {
+                if(count == 0)
+                return true;
+
+                if(map.containsKey(s.charAt(i)))
+                {
+                    map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+                    if(map.get(s.charAt(i)) == 1)
+                    count++;
+                }
+                i++;
+            }
+        }
+        return false;
+    }
+}
