7a5dcd29af1c3cdec3935314662a244e3549869d
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-14 21:40:37 +0530

    Time: 13 ms (66.48%) | Memory: 45.1 MB (58.97%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    private boolean validSpeed(int[] piles, int allotedTime, int speed)
+    {
+        int time = 0;
+        for(int pile : piles)
+        {
+            time += pile / speed + (pile % speed == 0 ? 0 : 1);
+            if(time > allotedTime)
+            return false;
+        }
+        return true;
+    }
+    
+    public int minEatingSpeed(int[] piles, int h) 
+    {
+        int start = 1;
+        int end = Arrays.stream(piles).max().getAsInt();
+        int minEatingSpeed = end;
+        
+        while (start <= end) 
+        {
+            int mid = start + (end - start) / 2;
+            if(validSpeed(piles, h, mid)) 
+            {
+                minEatingSpeed = mid;
+                end = mid - 1;
+            } 
+            else 
+            start = mid + 1;
+        }
+        // return the minimumSpeed
+        return minEatingSpeed;
+    }
+}
