88d831a1e7bdda6e1fd4b76028a5239f8847b2d8
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-24 05:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.07%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -17,37 +17,23 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) 
     {
         List<Integer> ans=new ArrayList<>();
-        if(root==null)
+
+        if(root == null)
         return ans;
 
-        while(root!=null)
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while(!stack.isEmpty())
         {
-            // Left-Part doesn't exist.
-            if(root.left==null)
-            {
-                ans.add(root.val);
-                root=root.right;
-            }
-            // Left-Part does exist.
-            else
-            {
-                TreeNode curr=root.left;
-                while(curr.right!=null && curr.right!=root)
-                curr=curr.right;
-                // Left-Part not yet traversed.
-                if(curr.right==null)
-                {
-                    ans.add(root.val);
-                    curr.right=root;
-                    root=root.left;
-                }
-                // Left-Part already traversed.
-                else
-                {
-                    curr.right=null;
-                    root=root.right;
-                }
-            }
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+
+            if(node.right != null)
+            stack.push(node.right);
+
+            if(node.left != null)
+            stack.push(node.left);
         }
         return ans;
     }
