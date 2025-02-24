b0e2368226b30091670a2b667ab441b5348ea9e1
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-02-24 21:30:39 +0530

    Time: 4 ms (8.99%) | Memory: 44.9 MB (44.27%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -21,26 +21,27 @@ class Solution {
 
     public List<Integer> postorder(Node root) 
     {
-        List<Integer> ans=new ArrayList<>();
-        if(root==null)
+        List<Integer> ans = new ArrayList<>();
+        if(root == null)
         return ans;
 
         // Left-to-Right->Root
         // we will add in order Root->Right-to-Left and then reverse the list
-        Deque<Node> stack=new ArrayDeque<>();
+        Deque<Node> stack = new ArrayDeque<>();
         stack.push(root);
         while(!stack.isEmpty())
         {
-            Node node=stack.pop();
+            Node node = stack.pop();
+            List<Node> nodeChildren = node.children;
+
             ans.add(node.val);
 
-            List<Node> offsprings=node.children;
-            //to add in the order Root->Right-to-Left in the list we must
+            // to add in the order Root->Right-to-Left in the list we must
             // add from left to right in the stack
-            // so that the processing order in the next iteration is right to left
-            for(Node offspring : offsprings) 
-            stack.push(offspring);
+            for(Node child : nodeChildren)
+            stack.push(child);
         }
+        // reverse the list and return
         Collections.reverse(ans);
         return ans;
     }
