6fafc7312d031441875a866ef81895a88d7ad47b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-05 21:43:51 +0530

    Time: 42 ms (61.98%) | Memory: 45 MB (88.58%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,39 @@
+class Solution {
+    private static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int minimumEffortPath(int[][] grid) 
+    {
+        int rows = grid.length, cols = grid[0].length;
+
+        int[][] effort = new int[rows][cols];
+        for(int i = 0; i < rows; i++)
+        Arrays.fill(effort[i], Integer.MAX_VALUE / 2);
+        effort[0][0] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{0, 0, 0});
+
+        while (!pq.isEmpty()) 
+        {
+            int[] node = pq.poll();
+            int r = node[1], c = node[2], currEffort = node[0];
+
+            if(r == rows - 1 && c == cols - 1)
+            return currEffort;
+
+            for (int[] dir : directions) 
+            {
+                int newRow = r + dir[0], newCol = c + dir[1];
+                if(newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) 
+                {
+                    int newEffort = Math.max(currEffort, Math.abs(grid[newRow][newCol] - grid[r][c]));
+                    if(newEffort < effort[newRow][newCol])
+                    {
+                        pq.offer(new int[]{newEffort, newRow, newCol});
+                        effort[newRow][newCol] = newEffort;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
