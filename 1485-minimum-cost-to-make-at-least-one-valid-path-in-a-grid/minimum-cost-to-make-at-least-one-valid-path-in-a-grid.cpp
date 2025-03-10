966e809ce985f9cb9f9e03bbcfeab22af0fbd438
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-11 00:21:26 +0530

    Time: 107 ms (10.85%) | Memory: 50.6 MB (5.22%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -6,6 +6,7 @@ public:
         {1, 0},  // Down 
         {-1, 0} // Up 
     };
+
     int solve(vector<vector<int>>& grid)
     {
         int n = grid.size();
@@ -14,14 +15,16 @@ public:
         // create vis array
         vector<vector<bool>> vis(n, vector<bool>(m, false));
 
-        // Min-Heap of vectors (lexicographical order) {cost, row, col}
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        pq.push({0, 0, 0});
+        // deque {cost, row, col}
+        deque<vector<int>> dq;
+        dq.push_front({0, 0, 0});
+        // vis[0][0] = true;
 
-        while(!pq.empty())
+        // '0 - 1' BFS
+        while(!dq.empty())
         {
-            auto v = pq.top();
-            pq.pop();
+            auto v = dq.front();
+            dq.pop_front();
 
             int cost = v[0];
             int row = v[1];
@@ -30,10 +33,10 @@ public:
             if(row == n - 1 && col == m - 1)
             return cost;
 
-            if(vis[row][col])
+            if(vis[row][col])  // ✅ Skip already visited nodes
             continue;
 
-            vis[row][col] = true;
+            vis[row][col] = true;  // ✅ Mark as visited after dequeuing
 
             for(int i = 0; i < directions.size(); i++)
             {
@@ -42,7 +45,14 @@ public:
                 if(r >= 0 && r < n && c >= 0 && c < m && !vis[r][c])
                 {
                     int nextCellTravelCost = ((grid[row][col] - 1) == i ? 0 : 1);
-                    pq.push({nextCellTravelCost + cost, r, c});
+
+                    if(nextCellTravelCost == 0)
+                    dq.push_front({cost, r, c});
+
+                    else
+                    dq.push_back({cost + 1, r, c});
+
+                    // vis[row][col] = true;
                 }
             }
         }
