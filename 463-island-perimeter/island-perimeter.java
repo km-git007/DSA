1d3c9db027b4e0f4861752da5e5105731bf4976d
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-13 19:29:28 +0530

    Time: 9 ms (16.81%) | Memory: 46.4 MB (5.61%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,45 +1,53 @@
 class Solution {
+    
+    // Directions for moving up, down, left, and right
     private static int[][] directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-    int n, m, perimeter;
-    private int dfs(int row, int col, int[][] grid) 
+    private int n,m;
+    private int bfs(int startRow, int startCol, int[][] grid) 
     { 
-        if(row < 0 || col < 0 || row >= n || col >= m || grid[row][col] == 0)
-        return 1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{startRow, startCol}); 
+        // marking the cell as vis
+        grid[startRow][startCol] = -1;
 
-        // already visited cell
-        if(grid[row][col] == -1)
-        return 0;
-
-        // mark as vis
-        grid[row][col] = -1;
-
-        int cellPeri = 0;
-        for(var dir : directions)
+        int perimeter = 0;       
+        while (!q.isEmpty()) 
         {
-            int x = row + dir[0];
-            int y = col + dir[1];
-            cellPeri += dfs(x, y, grid);
+            int[] currCell = q.poll();
+            int currRow = currCell[0];
+            int currCol = currCell[1];
+                        
+            // Explore neighbors
+            for (int[] dir : directions) 
+            {
+                int row = currRow + dir[0];
+                int col = currCol + dir[1];
+                // Check if the new position is within bounds and not visited.
+                if(row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == 1) 
+                {
+                    q.add(new int[]{row,col});
+                    grid[row][col] = -1;
+                }
+                else if(row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0)
+                perimeter++;
+            }
         }
-
-        perimeter += cellPeri;
-        return 0;
+        return perimeter;
     }
 
     public int islandPerimeter(int[][] grid) 
     {
         n = grid.length;
         m = grid[0].length;
-        perimeter = 0;
-
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
                 if(grid[i][j] == 1)
-                dfs(i,j,grid);
+                return bfs(i,j,grid);
             }
         }
-
-        return perimeter;
+        // no isLand exists hence return -1.
+        return -1;
     }
 }
