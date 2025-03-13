de219237a8216091a08dbe886c7ca2904ee06c60
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-13 19:37:08 +0530

    Time: 7 ms (29.51%) | Memory: 46 MB (32.58%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -3,51 +3,36 @@ class Solution {
     // Directions for moving up, down, left, and right
     private static int[][] directions = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
     private int n,m;
-    private int bfs(int startRow, int startCol, int[][] grid) 
+    private boolean check(int row, int col, int[][] grid) 
     { 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startRow, startCol}); 
-        // marking the cell as vis
-        grid[startRow][startCol] = -1;
-
-        int perimeter = 0;       
-        while (!q.isEmpty()) 
-        {
-            int[] currCell = q.poll();
-            int currRow = currCell[0];
-            int currCol = currCell[1];
-                        
-            // Explore neighbors
-            for (int[] dir : directions) 
-            {
-                int row = currRow + dir[0];
-                int col = currCol + dir[1];
-                // Check if the new position is within bounds and not visited.
-                if(row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == 1) 
-                {
-                    q.add(new int[]{row,col});
-                    grid[row][col] = -1;
-                }
-                else if(row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0)
-                perimeter++;
-            }
-        }
-        return perimeter;
+        return row >= 0 && row < n && col >= 0 && col < m && grid[row][col] == 1;
     }
 
     public int islandPerimeter(int[][] grid) 
     {
         n = grid.length;
         m = grid[0].length;
+        int perimeter = 0;
         for(int i = 0; i < n; i++)
         {
             for(int j = 0; j < m; j++)
             {
+                int cellPeri = 0;
                 if(grid[i][j] == 1)
-                return bfs(i,j,grid);
+                {
+                    // Explore neighbors
+                    for (int[] dir : directions) 
+                    {
+                        int row = i + dir[0];
+                        int col = j + dir[1];
+                        cellPeri += (check(row, col, grid) ? 0 : 1);
+                    }
+                    // add cell perimeter to total perimeter
+                    perimeter += cellPeri;
+                }
             }
         }
-        // no isLand exists hence return -1.
-        return -1;
+        // return perimeter
+        return perimeter ;
     }
 }
