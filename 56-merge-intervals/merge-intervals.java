8a865448830666b877c900bd7e57a708ef83da42
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-25 06:46:00 +0530

    Time: 8 ms (86.85%) | Memory: 46.9 MB (15.91%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,22 +1,28 @@
 class Solution {
     public int[][] merge(int[][] intervals) 
     {
-        // Sort the intervals based on the first element of each sub-array
+        // Sort the intervals based on the start time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int[] prev=new int[]{intervals[0][0],intervals[0][1]};
-        List<int[]> ans=new ArrayList<>();
-        ans.add(prev);
-        for(int i=1;i<intervals.length;i++)
+
+        List<int[]> mergedIntervals = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        mergedIntervals.add(currentInterval);
+
+        for (int i = 1; i < intervals.length; i++) 
         {
-            if(intervals[i][0]<=prev[1])
-            prev[1]=Math.max(prev[1],intervals[i][1]);
-            else
-            {
-                prev=new int[]{intervals[i][0], intervals[i][1]};
-                ans.add(prev);
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            // Merge intervals by updating the end time
+            if(start <= currentInterval[1])
+            currentInterval[1] = Math.max(currentInterval[1], end);
+
+            else 
+            {    // Add new interval and update reference
+                currentInterval = new int[]{start, end};
+                mergedIntervals.add(currentInterval);
             }
         }
-        return ans.toArray(new int[ans.size()][]);
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 }
