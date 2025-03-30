e6e272c8f34d4f4b3bd9370590f202875cff79cc
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-31 01:46:32 +0530

    Time: 26 ms (37.19%) | Memory: 74.6 MB (48.77%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int M, N;
-    // The three possible directions for the next column.
-    const int direction[3] = {-1, 0, 1};
-    vector<vector<int>> dp;
-    int DFS(int row, int col, vector<vector<int>>& grid) 
-    {
-        if(dp[row][col] != -1)
-        return dp[row][col];
-
-        int maxMoves = 0;
-        for (int dir : direction) 
-        {
-            // Next cell after the move.
-            int newRow = row + dir, newCol = col + 1;
-
-            // If the next cell is valid and greater than the current cell
-            // value, perform recursion to that cell with updated value of moves.
-            if(newRow >= 0 && newCol >= 0 && newRow < M && newCol < N && grid[row][col] < grid[newRow][newCol])
-            maxMoves = max(maxMoves, 1 + DFS(newRow, newCol, grid));
-        }
-
-        return dp[row][col] = maxMoves;
-    }
-
     int maxMoves(vector<vector<int>>& grid) 
     {
-        M = grid.size();
-        N = grid[0].size();
-        dp = vector<vector<int>>(M, vector<int>(N, -1));
+        int N = grid.size();
+        int M = grid[0].size();
+        vector<vector<int>> dp(N, vector<int>(M, 0));
 
         int maxMoves = 0;
-        for(int i = 0; i < M; i++) 
+        for(int j = 1; j < M; j++)
         {
-            int movesRequired = DFS(i, 0, grid);
-            maxMoves = max(maxMoves, movesRequired);
+            for(int i = 0; i < N; i++) 
+            {
+                int p1 = 0, p2 = 0, p3 = 0;
+                if(i > 0 && grid[i - 1][j - 1] < grid[i][j])
+                p1 = 1 + dp[i - 1][j - 1];
+
+                if(i < N - 1 && grid[i + 1][j - 1] < grid[i][j])
+                p2 = 1 + dp[i + 1][j - 1];
+                
+                if(grid[i][j - 1] < grid[i][j])
+                p3 = 1 + dp[i][j - 1];
+
+                dp[i][j] = max({p1, p2, p3});
+
+                if(dp[i][j] == 0)
+                dp[i][j] = -1;
+                
+                else
+                maxMoves = max(maxMoves, dp[i][j]);
+            }
         }
+
         return maxMoves;
     }
 };
