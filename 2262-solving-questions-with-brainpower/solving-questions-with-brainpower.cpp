d8927a62fb579e0a6c1ca1a432895aef557ad3cd
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-01 11:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 116.7 MB (99.04%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    long long dp[100000];
-    long long solve(int index, vector<vector<int>>& questions) 
-    {
-        if(index >= questions.size())
-        return 0;
-
-        if(dp[index] != -1)
-        return dp[index];
-
-        long long pick = questions[index][0] + solve(index + questions[index][1] + 1, questions);
-        long long skip = solve(index + 1, questions);
-
-        return dp[index] = max(pick, skip);
-    }
-
     long long mostPoints(vector<vector<int>>& questions) 
     {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, questions);
+        int n = questions.size();
+
+        if(n == 1)
+        return questions[0][0];
+
+        long long dp[100000];
+        dp[n - 1] = questions[n - 1][0];
+        dp[n - 2] = max(questions[n - 1][0], questions[n - 2][0]);
+
+        for(int i = n - 3; i >= 0; i--)
+        {
+            long long take = questions[i][0]; 
+
+            if(i + questions[i][1] + 1 < n)
+            take += dp[i + questions[i][1] + 1];
+
+            long long skip = dp[i + 1];
+
+            dp[i] = max(take, skip);
+        }
+
+        return dp[0];
     }
 };
