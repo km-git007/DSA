bf065f3b31d6bdec1ac083bb32b2184f065eb96d
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-04 11:53:07 +0530

    Time: 1 ms (98.68%) | Memory: 44.5 MB (77.28%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private List<TreeNode> solve(int start, int end)
+    {
+        List<TreeNode> res = new ArrayList<>();
+        if(start > end)
+        {
+            res.add(null);
+            return res;
+        } 
+
+        else if(start == end)
+        {
+            res.add(new TreeNode(start));
+            return res;
+        } 
+
+        for(int root = start; root <= end; root++)
+        {
+            var leftBSTs = solve(start, root - 1);
+            var rightBSTs = solve(root + 1, end);
+            for(TreeNode rightRoot : rightBSTs)
+            {
+                for(TreeNode leftRoot : leftBSTs)
+                {
+                    TreeNode node = new TreeNode(root);
+                    node.left = leftRoot;
+                    node.right = rightRoot;
+                    res.add(node);
+                }       
+            }
+        }
+        return res;
+    }
+
+    public List<TreeNode> generateTrees(int n) 
+    {
+        return solve(1, n);
+    }
+}
