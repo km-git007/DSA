d9a4d7e9c8f9c65b62044b83c3c8e113c69c9f8e
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-04 11:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (61.75%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int dp[20];
-    int solve(int n)
-    {
-        if(n == 0 || n == 1)
-        return 1;
-
-        if(dp[n] != -1)
-        return dp[n];
-
-        int res = 0;
-        for(int i = 0; i < n; i++)
-        res += solve(i) * solve(n - i - 1);
-
-        return dp[n] = res;
-    }
-
     int numTrees(int n) 
     {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1; 
+
+        // Bottom-up DP computation
+        for(int node = 2; node <= n; node++) 
+        {
+            for(int root = 0; root < node; root++) 
+            dp[node] += dp[root] * dp[node - 1 - root];
+        }
+
+        return dp[n]; 
     }
 };
