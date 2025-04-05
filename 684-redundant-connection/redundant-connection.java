5f8b9113b085dbc3664c4fd386c624d73ef2e6d9
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-05 12:25:14 +0530

    Time: 1 ms (73.97%) | Memory: 43.3 MB (60.82%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,66 +1,59 @@
-class Solution {
-    
-    private class DSU {
-        private int[] parent, rank;
-        // Constructor
-        public DSU(int n) 
-        {
-            parent = new int[n];
-            rank = new int[n];
-            for (int i = 0; i < n; i++) 
-            {
-                parent[i] = i;
-                rank[i] = 0;
-            }
-        }
+class DSU {
 
-        // Find function with path compression
-        public int find(int x) 
-        {
-            if (x != parent[x]) {
-                parent[x] = find(parent[x]); // Path compression
-            }
+    int[] parent, rank;
 
-            return parent[x];
-        }
-
-        // Union by rank
-        public void unionSet(int x, int y) 
-        {
-            int parentX = find(x);
-            int parentY = find(y);
-
-            // Already in the same set
-            if (parentX == parentY) return; 
-
-            if (rank[parentX] > rank[parentY]) {
-                parent[parentY] = parentX;
-            } else if (rank[parentX] < rank[parentY]) {
-                parent[parentX] = parentY;
-            } else {
-                parent[parentX] = parentY;
-                rank[parentY]++;
-            }
+    public DSU(int n) {
+        this.parent = new int[n];
+        this.rank = new int[n];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
         }
     }
 
+    public int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public int getParent(int x) {
+        return parent[x];
+    }
+
+    public void  union(int x, int y)
+    {
+        int parentX = find(x);
+        int parentY = find(y);
+        if(parentX == parentY) return;
+
+        if(rank[parentX] == rank[parentY]) {
+            parent[parentX] = parentY;
+            rank[parentY]++;
+        }
+        else if(rank[parentX] < rank[parentY]) {
+            parent[parentX] = parentY;
+        }
+        else {
+            parent[parentY] = parentX;
+        }
+    }
+}
+
+class Solution {
     public int[] findRedundantConnection(int[][] edges) 
     {
-        int N = edges.length;
-        DSU dsu = new DSU(N);
+        int n = edges.length;
+        // graph is 1 based
+        DSU dsu = new DSU(n + 1);
 
-        for (int[] edge : edges) 
+        for(int[] edge : edges)
         {
-            // If input is 1-based
-            int x = edge[0] - 1; 
-            int y = edge[1] - 1;
+            int x = edge[0];
+            int y = edge[1];
 
-            // Redundant edge found
-            if(dsu.find(x) == dsu.find(y)) 
-            return edge; 
-            
-            // do union
-            dsu.unionSet(x, y);
+            if(dsu.find(x) == dsu.find(y))
+            return edge;
+
+            dsu.union(x, y);
         }
 
         // No redundant edge found
