5ff1af2c65dbe52b3c65088b6b8b2ccd1f8195a1
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-07 13:39:56 +0530

    Time: 42 ms (54.31%) | Memory: 46.9 MB (52.63%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,19 +1,4 @@
 class Solution {
-    private Boolean[][] dp;
-    private boolean check(int index, int target, int[] nums)
-    {
-        if(target == 0)
-        return true;
-
-        if(index >= nums.length || target < 0)
-        return false;
-
-        if(dp[index][target] != null)
-        return dp[index][target];
-
-        return dp[index][target] = check(index + 1, target - nums[index], nums) || check(index + 1, target, nums);
-    }
-
     public boolean canPartition(int[] nums) 
     {
         int Sum = Arrays.stream(nums).sum();
@@ -21,50 +6,27 @@ class Solution {
         if(Sum % 2 != 0)
         return false;
 
-        int m = Sum / 2;
-        dp = new Boolean[nums.length][m + 1];
-        return check(0, m, nums);
+        int sum = Sum / 2;
+        int n = nums.length;
 
+        // create dp array
+        boolean[][] dp = new boolean[n + 1][sum + 1];
 
+        // fill the first column
+        for(int i = 0; i < n + 1; i++)
+        dp[i][0] = true;
 
+        for(int i = 1; i < n + 1; i++)
+        {
+            for(int j = 1; j < sum + 1; j++)
+            {
+                if(j - nums[i - 1] >= 0)
+                dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // int n = nums.length;
-
-        // // create dp array
-        // boolean[][] dp = new boolean[n + 1][m + 1];
-
-        // // fill the first column
-        // for(int i = 0; i < n + 1; i++)
-        // dp[i][0] = true;
-
-        // for(int i = 1; i < n + 1; i++)
-        // {
-        //     for(int j = 1; j < m + 1; j++)
-        //     {
-        //         if(j - nums[i - 1] >= 0)
-        //         dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-
-        //         else
-        //         dp[i][j] = dp[i - 1][j];
-        //     }
-        // }
-        // return dp[n][m];
+                else
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
     }
 }
