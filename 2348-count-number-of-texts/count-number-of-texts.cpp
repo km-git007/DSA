ec3f9e7738fb61f7cf66f3ea50d729cb08e2169e
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-10 00:25:49 +0530

    Time: 27 ms (42.37%) | Memory: 18.5 MB (67.46%) - LeetSync

--- 2348-count-number-of-texts/count-number-of-texts.cpp
+++ 2348-count-number-of-texts/count-number-of-texts.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    int MOD;
-    vector<int> dp;
-    unordered_set<char> set;
-    int solve(int index, string &pressedKeys)
-    {
-        if(index >= pressedKeys.length())
-        return 1;
-
-        if(dp[index] != -1)
-        return dp[index];
-
-        int times = set.count(pressedKeys[index]) ? 4 : 3;
-
-        int totalWays = 0;
-        for(int i = index; i < index + times; i++)
-        {
-            if(pressedKeys[i] != pressedKeys[index])
-            break;
-
-            totalWays = (totalWays + solve(i + 1, pressedKeys) % MOD) % MOD;
-        }
-
-        return dp[index] = totalWays;
-    }
-
     int countTexts(string pressedKeys) 
     {
-        MOD = 1e9 + 7;
-        set = {'7','9'};
-        dp = vector<int>(pressedKeys.length(), -1);
-        return solve(0, pressedKeys);
+        int n = pressedKeys.length(), MOD = 1e9 + 7;
+        unordered_set<char> set = {'7','9'};
+
+        vector<int> dp(n + 1);
+        dp[n] = 1;
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            int times = set.count(pressedKeys[i]) ? 4 : 3;
+            int totalWays = 0;
+            for(int j = i; j < i + times && j < n; j++)
+            {
+                if(pressedKeys[j] != pressedKeys[i])
+                break;
+
+                totalWays = (totalWays + dp[j + 1] % MOD) % MOD;
+            }
+            dp[i] = totalWays;
+        }
+        return dp[0];
     }
 };
