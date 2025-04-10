6e87f336c7d152a04e9f1f09ce6ced7580d70fad
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-10 23:54:17 +0530

    Time: 9 ms (63.64%) | Memory: 48.7 MB (62.74%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -4,19 +4,15 @@ class Solution {
     {
         int n = coins.length;
         int[][]dp = new int[n + 1][amount + 1];
-        for(int i = 0; i <= n; i++)
+
+        // base case: amount 0 can always be formed with 1 way (pick nothing)
+        for (int i = 0; i <= n; i++)
+        dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++)
         {
-            for(int j = 0; j <= amount; j++)
-            {
-                if(j == 0)
-                dp[i][j] = 1;
-
-                else if(i == 0)
-                dp[i][j] = 0;
-
-                else
-                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
-            }
+            for(int j = 1; j <= amount; j++)
+            dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
         }
         return dp[n][amount];
     }
