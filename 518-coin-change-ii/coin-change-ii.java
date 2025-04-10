5458855f7d886bdb6399df1603a862db3eb543d6
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-10 23:52:50 +0530

    Time: 13 ms (25.03%) | Memory: 48.6 MB (62.74%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,33 +1,23 @@
 class Solution {
 
-    Integer[][] dp;
-    private int solve(int[] coins, int amount, int index)
-    {
-        if(amount == 0)
-        return 1;
-
-        if(index >= coins.length)
-        return 0;
-
-        if(dp[amount][index] != null)
-        return dp[amount][index];
-
-        int ways = 0;
-        for(int i = index; i < coins.length; i++)
-        {
-            if(amount < coins[i])
-            break;
-
-            ways += solve(coins, amount - coins[i], i);
-        }
-
-        return dp[amount][index] = ways;
-    }
-
     public int change(int amount, int[] coins) 
     {
-        dp = new Integer[amount + 1][coins.length + 1];
-        Arrays.sort(coins);
-        return solve(coins, amount, 0);
+        int n = coins.length;
+        int[][]dp = new int[n + 1][amount + 1];
+        for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= amount; j++)
+            {
+                if(j == 0)
+                dp[i][j] = 1;
+
+                else if(i == 0)
+                dp[i][j] = 0;
+
+                else
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[n][amount];
     }
 }
