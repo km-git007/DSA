ca389d9d9cdbab78d26795ecc661c3dd61d3d03c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-10 19:27:28 +0530

    Time: 11 ms (75.49%) | Memory: 44.4 MB (45.71%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,18 @@
 class Solution {
-    private Integer[][] dp;
-    private int solve(int curr, int next, int[] nums)
-    {
-        // Base case: if we have exhausted the list
-        if(curr < 0)
-        return 0;
-
-        if (dp[curr][next] != null)
-        return dp[curr][next]; 
-
-        // Option 1: Skip the current element
-        int exclude = solve(curr - 1, next, nums);
-
-        // Option 2: Include the current element (if possible)
-        int include = 0;
-        if (next == nums.length || nums[curr] < nums[next])
-        include = 1 + solve(curr - 1, curr, nums); 
-
-        // Memoize the result and return 
-        return dp[curr][next] = Math.max(include, exclude);
-    }
-
     public int lengthOfLIS(int[] nums) 
     {
-        dp = new Integer[nums.length + 1][nums.length + 1];
-        return solve(nums.length - 1, nums.length, nums);
+        int n = nums.length;
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for(int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            Integer ceil = treeSet.ceiling(num);
+
+            if(ceil != null)
+            treeSet.remove(ceil);
+
+            treeSet.add(num);
+        }
+        return treeSet.size();
     }
 }
