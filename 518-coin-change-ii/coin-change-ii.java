621f922c8bac14f3a52a509068a77a476c53c92e
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-10 21:30:50 +0530

    Time: 14 ms (22.95%) | Memory: 54.4 MB (5.06%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -6,17 +6,17 @@ class Solution {
         if(amount == 0)
         return 1;
 
-        if(index >= coins.length)
+        if(index == 0)
         return 0;
 
         if(dp[amount][index] != null)
         return dp[amount][index];
 
-        int notTake = solve(coins, amount, index + 1);
+        int notTake = solve(coins, amount, index - 1);
 
         int take = 0;
-        if(amount >= coins[index])
-        take = solve(coins, amount - coins[index], index);
+        if(amount >= coins[index - 1])
+        take = solve(coins, amount - coins[index - 1], index);
 
         return dp[amount][index] = take + notTake;
     }
@@ -24,6 +24,6 @@ class Solution {
     public int change(int amount, int[] coins) 
     {
         dp = new Integer[amount + 1][coins.length + 1];
-        return solve(coins, amount, 0);
+        return solve(coins, amount, coins.length);
     }
 }
