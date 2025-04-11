7f8e6eebe8219dc5046b26263b3281ea3ffed872
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-11 16:33:33 +0530

    Time: 112 ms (19.69%) | Memory: 56.5 MB (5.30%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,38 +1,38 @@
 class Solution {
-    private int dp[][]=new int[1001][1001];
-    private int isPallindrome(String s,int start,int end)
+    private int dp[][] = new int[1001][1001];
+    private int isPalindrome(String s, int start, int end)
     {
-        if(start>=end)
+        if(start >= end)
         return 1;
 
-        if(dp[start][end]!=-1)
+        if(dp[start][end] != -1)
         return dp[start][end];
 
-        if(s.charAt(start)!=s.charAt(end))
+        if(s.charAt(start) != s.charAt(end))
         return dp[start][end]=0;
 
-        return dp[start][end]=isPallindrome(s,start+1,end-1);
+        return dp[start][end] = isPalindrome(s, start + 1, end - 1);
     }
 
-    private int solve(String s,int start)
+    private int solve(String s)
     {
-        int pStrings=0;
-        for(int i=0;i<s.length();i++)
+        int count = 0;
+        for(int i = 0; i < s.length(); i++)
         {
-            for(int j=i;j<s.length();j++)
+            for(int j = i; j < s.length(); j++)
             {
-                if(isPallindrome(s,i,j)==1)
-                pStrings++;
+                if(isPalindrome(s,i,j) == 1)
+                count++;
             }
         }
-        return pStrings;
+        return count;
     }
 
     public int countSubstrings(String s) 
     {
-        for(int i=0;i<dp.length;i++)
-        Arrays.fill(dp[i],-1);
+        for(int i = 0; i < dp.length; i++)
+        Arrays.fill(dp[i], -1);
 
-        return solve(s,0);
+        return solve(s);
     }
 }
