a144ad655942b6db2283d2fc28ecbcbee6ae65f2
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-16 21:41:41 +0530

    Time: 24 ms (13.95%) | Memory: 55 MB (10.30%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,27 @@
+class Solution {
+    private Integer[][] dp;
+    private int solve(int n, int m, String s, String t)
+    {
+        // all the chars in String 't' found
+        if(m == 0)
+        return 1;
+
+        // can't match further
+        if(n == 0)
+        return 0;
+
+        if(dp[n][m] != null)
+        return dp[n][m];
+
+        if(s.charAt(n - 1) == t.charAt(m - 1))
+        return dp[n][m] = solve(n - 1, m - 1, s, t) + solve(n - 1, m, s, t);
+
+        return dp[n][m] = solve(n - 1, m, s, t);
+    }
+
+    public int numDistinct(String s, String t) 
+    {
+        dp = new Integer[s.length() + 1][t.length() + 1];
+        return solve(s.length(), t.length(), s, t);
+    }
+}
