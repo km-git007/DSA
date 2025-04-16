709dec45e3c0736df185c89bbd8c313500368674
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-17 01:21:24 +0530

    Time: 17 ms (27.07%) | Memory: 11.4 MB (81.53%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -3,23 +3,27 @@ public:
     int numSplits(string s) 
     {
         int n = s.length();
-        unordered_map<char, int> map;
-
-        vector<int> distinct(n, 0);
-        for(int i = n - 1; i >= 0; i--)
-        {
-            distinct[i] = map.size();
-            map[s[i]]++;
-        }
-
-        // clear the map
-        map.clear();
-
+        unordered_map<char, int> leftMap, rightMap;
+        
+        // Initialize rightMap with frequency of all characters in the string
+        for(char ch : s) 
+        rightMap[ch]++;
+        
         int goodWays = 0;
-        for(int i = 0; i < n; i++)
-        {
-            map[s[i]]++;
-            if(map.size() == distinct[i])
+        // Traverse the string and compute the number of distinct letters for left and right
+        // Only go till n-1 to ensure non-empty sright
+        for (int i = 0; i < n - 1; i++) 
+        {  
+            // Update leftMap with current character and update rightMap
+            leftMap[s[i]]++;
+            rightMap[s[i]]--;
+            
+            // Remove the character from rightMap if its count becomes 0
+            if(rightMap[s[i]] == 0)
+            rightMap.erase(s[i]);
+
+            // Compare the distinct counts of the left and right part
+            if(leftMap.size() == rightMap.size()) 
             goodWays++;
         }
         return goodWays;
