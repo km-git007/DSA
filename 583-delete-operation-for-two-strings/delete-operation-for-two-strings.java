8a8155b977fb005d159229b2f35f6a955f09770a
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-16 20:57:47 +0530

    Time: 6 ms (93.93%) | Memory: 44.8 MB (72.47%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,26 +1,20 @@
 class Solution {
-    private int[][] dp;
-    private int LCS(int n, int m, String s, String t)
-    {
-        if(n == 0 || m == 0)
-        return 0;
-
-        if(dp[n][m] != -1)
-        return dp[n][m];
-
-        if(s.charAt(n - 1) == t.charAt(m - 1))
-        return 1 + LCS(n - 1, m - 1, s, t);
-
-        return dp[n][m] = Math.max(LCS(n - 1, m, s, t), LCS(n, m - 1, s, t));
-    }
-
     public int minDistance(String word1, String word2) 
     {
-        dp = new int[word1.length() + 1][word2.length() + 1];
-        for(int i = 0; i <= word1.length(); i++)
-        Arrays.fill(dp[i], -1);
+        int n = word1.length(), m = word2.length(); 
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1.charAt(i - 1) == word2.charAt(j - 1))
+                dp[i][j] = 1 + dp[i - 1][j - 1];
 
-        int lcs = LCS(word1.length(), word2.length(), word1, word2);
-        return word1.length() + word2.length() - 2 * lcs;
+                else
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        int lcs = dp[n][m];
+        return n + m - 2 * lcs;
     }
 }
