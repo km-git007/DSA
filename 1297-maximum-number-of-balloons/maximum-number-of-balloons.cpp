5ada3fec8ebc3e458f5853f18375a2e9a763e31f
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-17 00:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (81.88%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) 
     {
-        unordered_set<char> set = {'b', 'a', 'l', 'o', 'n'};
-        unordered_map<char, int> map;
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
 
         for(char ch : text)
         {
-            if(set.count(ch))
-            map[ch]++;
+            switch(ch)
+            {
+                case 'b': b++; break;
+                case 'a': a++; break;
+                case 'l': l++; break;
+                case 'o': o++; break;
+                case 'n': n++; break;
+            }
         }
 
-        if(map.size() < 5)
-        return 0;
+        // Since 'l' and 'o' appear twice in "balloon"
+        l /= 2;
+        o /= 2;
 
-        int instances = INT_MAX;
-        for (auto& entry : map) 
-        {
-            char key = entry.first;
-            int value = (key == 'l' || key == 'o') ? entry.second / 2 : entry.second;
-
-            instances = min(instances, value);
-        }
-        return instances;
+        return min({b, a, l, o, n});
     }
 };
