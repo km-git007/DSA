409c13ff4b47732cc010479fb0a623ffdcb0c43a
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-18 14:38:36 +0530

    Time: 1 ms (63.53%) | Memory: 46.7 MB (20.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,36 +1,25 @@
 class Solution {
     public int trap(int[] height) 
     {
-        // finding the tower with maximum height;
-        // calculating water trapped between 0th -> max height tower 
-        // then calculating water trapped between max height tower -> nth tower
-
-        int maxHeight=height[0];
-        int index=0;
-        for(int i=1;i<height.length;i++)
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+        while(left < right)
         {
-            if(height[i]>maxHeight)
+            if(height[left] > height[right])
             {
-                maxHeight=height[i];
-                index=i;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+            else
+            {
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
             }
         }
-
-        int leftMax=height[0];
-        int water=0;
-        for(int i=1;i<index;i++)
-        {
-            leftMax=Math.max(leftMax,height[i]);
-            water+=leftMax-height[i];
-        }
-
-        int rightMax=height[height.length-1];
-        for(int i=height.length-2;i>index;i--)
-        {
-            rightMax=Math.max(rightMax,height[i]);
-            water+=rightMax-height[i];
-        }
-
         return water;
     }
 }
