6be72bdd55a12ebda6eb5e98989a8f4ea674ad09
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-04-26 01:39:12 +0530

    Time: 2 ms (83.04%) | Memory: 44.4 MB (96.81%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -9,26 +9,22 @@ class Solution {
             return;
         }
 
-        if(index >= candidates.length)
-        return;
+        for(int i = index; i < candidates.length; i++)
+        {
+            if(candidates[i] > target)
+            break;
 
-        // skip
-        solve(candidates, target, index + 1);
-
-        if(target < candidates[index])
-        return;
-
-        // pick
-        temp.add(candidates[index]);
-        solve(candidates, target - candidates[index], index);
-        temp.remove(temp.size() - 1);
+            temp.add(candidates[i]);
+            solve(candidates, target - candidates[i], i);
+            temp.remove(temp.size() - 1);
+        }
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) 
     {
         res = new ArrayList<>();
         temp = new ArrayList<>();
-
+        
         Arrays.sort(candidates);
         solve(candidates, target, 0);
         return res;
