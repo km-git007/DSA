ac46c9b3f954a5773cd903a4058ab57e184d08fb
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-05-02 21:33:21 +0530

    Time: 25 ms (41.89%) | Memory: 45.6 MB (66.79%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String pushDominoes(String dom) 
+    {
+        int N = dom.length();
+        int [] forces = new int[N];
+        
+        int force = 0;
+        for(int i = 0; i < N; i++)
+        {
+            char ch = dom.charAt(i);
+            if(ch == 'R')
+            force = N;
+
+            else if(ch == '.')
+            force = Math.max(0, force - 1);
+
+            else
+            force = 0;
+
+            forces[i] = force;
+        }
+
+        force = 0;
+        for(int i = N - 1; i >= 0; i--)
+        {
+            char ch = dom.charAt(i);
+            if(ch == 'L')
+            force = N;
+
+            else if(ch == '.')
+            force = Math.max(0, force - 1);
+
+            else
+            force = 0;
+
+            forces[i] -= force;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++)
+        {
+            char ch = forces[i] > 0 ? 'R' : (forces[i] < 0 ? 'L' : '.');
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
