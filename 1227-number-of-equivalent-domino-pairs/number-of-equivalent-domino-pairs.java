98dbc1255d22f0576b3be803c909c54a72522a6f
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-05-04 20:17:32 +0530

    Time: 5 ms (65.14%) | Memory: 53 MB (58.27%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.java
@@ -1,38 +1,16 @@
-class Pair {
-    int x, y;
-
-    public Pair(int x, int y) {
-        this.x = Math.min(x, y);
-        this.y = Math.max(x, y);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Pair)) return false;
-        Pair other = (Pair) o;
-        return x == other.x && y == other.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-}
-
 class Solution 
 {
     public int numEquivDominoPairs(int[][] dominoes) 
     {
-        int count = 0;
-        Map<Pair, Integer> map = new HashMap<>();
+        int[][] count = new int[10][10];  // count[i][j] stores frequency of (i, j)
+        int res = 0;
         for (int[] dom : dominoes) 
         {
-            Pair p = new Pair(dom[0], dom[1]);
-            int freq = map.getOrDefault(p, 0);
-            count += freq;
-            map.put(p, freq + 1);
+            int a = Math.min(dom[0], dom[1]);
+            int b = Math.max(dom[0], dom[1]);
+            res += count[a][b];
+            count[a][b]++;
         }
-        return count;
+        return res;
     }
 }
