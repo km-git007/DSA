08e72467cd0516d24de7bf9ab57f13140d0fe0ce
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-06-03 00:52:57 +0530

    Time: 257 ms (56.96%) | Memory: 151.1 MB (53.66%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,114 +1,86 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,abm,bmi2")
-
-static const auto io_sync_off = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return nullptr;
-}();
-
-// class Solution {
-//   public:
-//     int largestIsland(std::vector<std::vector<int>> &grid) {
-//         int groupId = -1, n = grid.size();
-//         int groupSize[n * n + 1];
-//         std::memset(groupSize, 0, sizeof(groupSize));
-
-//         auto dfs = [&](this auto &&dfs, int i, int j) -> int {
-//             if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] < 1) {
-//                 return 0;
-//             }
-//             grid[i][j] = groupId;
-//             return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1);
-//         };
-
-//         for (int i = 0; i < n; ++i) {
-//             for (int j = 0; j < n; ++j) {
-//                 if (grid[i][j] > 0) {
-//                     groupSize[~(groupId - 1)] = dfs(i, j);
-//                     --groupId;
-//                 }
-//             }
-//         }
-
-//         int res = 0;
-//         std::unordered_set<int> check;
-//         for (int i = 0; i < n; ++i) {
-//             for (int j = 0; j < n; ++j) {
-//                 if (grid[i][j] == 0) {
-//                     check.clear();
-//                     int cur = 1;
-//                     if (i - 1 >= 0 && check.insert(grid[i - 1][j]).second) {
-//                         cur += groupSize[~(grid[i - 1][j] - 1)];
-//                     }
-//                     if (i + 1 < n && check.insert(grid[i + 1][j]).second) {
-//                         cur += groupSize[~(grid[i + 1][j] - 1)];
-//                     }
-//                     if (j - 1 >= 0 && check.insert(grid[i][j - 1]).second) {
-//                         cur += groupSize[~(grid[i][j - 1] - 1)];
-//                     }
-//                     if (j + 1 < n && check.insert(grid[i][j + 1]).second) {
-//                         cur += groupSize[~(grid[i][j + 1] - 1)];
-//                     }
-//                     res = std::max(res, cur);
-//                 }
-//             }
-//         }
-
-//         return res == 0 ? n * n : res;
-//     }
-// };
-
 class Solution {
-  public:
-    int largestIsland(std::vector<std::vector<int>> &grid) {
-        int groupId = -1, n = grid.size();
-        int groupSize[n * n + 1];
-        std::memset(groupSize, 0, sizeof(groupSize));
+public:
+    int n;
+    vector<pair<int, int>> directions = {
+        {-1, 0}, // Up
+        {0, 1},  // Right
+        {1, 0},  // Down
+        {0, -1}  // Left
+    };
 
-        auto dfs = [&](this auto &&dfs, int i, int j) -> int {
-            if (i < 0 || i >= n || j < 0 || j >= n || grid[i][j] < 1) {
-                return 0;
-            }
-            grid[i][j] = groupId;
-            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1);
-        };
+    int dfs(int row, int col, vector<vector<int>>& grid, int &id)
+    {
+        if(row < 0 || row >= n || col < 0 || col >= n || !grid[row][col] || grid[row][col] == id)
+        return 0;
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] > 0) {
-                    groupSize[~(groupId - 1)] = dfs(i, j);
-                    --groupId;
+        // mark the curr cell as visited
+        grid[row][col] = id;
+
+        int size = 1;
+        for (auto [dx, dy] : directions) 
+        {
+            int r = row + dx;
+            int c = col + dy;
+
+            size += dfs(r, c, grid, id);
+        }
+        return size;
+    }
+
+    int largestIsland(vector<vector<int>>& grid) 
+    {
+        n = grid.size();
+        unordered_map<int, int> map;
+
+        int id = 2;
+        // Mark each island with a unique ID and calculate its size
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    int islandSize = dfs(i, j, grid, id);
+                    map[id] = islandSize;
+                    id++;
                 }
             }
         }
 
-        int res = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 0) {
-                    int cur = 1;
-                    int a = (i > 0) ? grid[i - 1][j] : 0;
-                    int b = (i + 1 < n) ? grid[i + 1][j] : 0;
-                    int c = (j > 0) ? grid[i][j - 1] : 0;
-                    int d = (j + 1 < n) ? grid[i][j + 1] : 0;
-                    if (a != 0) {
-                        cur += groupSize[~(grid[i - 1][j] - 1)];
+        // there's no land, only water exists 
+        // flip any zero and get island of 1 unit
+        if (map.empty()) 
+        return 1;
+
+        int maxSize = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    int largeIslandSize = 1;
+                    unordered_set<int> set;
+                    for (auto [r, c] : directions) 
+                    {
+                        int row = i + r;
+                        int col = j + c;
+
+                        if(row < 0 || row >= n || col < 0 || col >= n || grid[row][col] < 2)
+                        continue;
+
+                        int neighborId = grid[row][col];
+                        if(!set.count(neighborId)) 
+                        {
+                            largeIslandSize += map[neighborId];
+                            set.insert(neighborId);
+                        }
+
                     }
-                    if (b != 0 && b != a) {
-                        cur += groupSize[~(grid[i + 1][j] - 1)];
-                    }
-                    if (c != 0 && c != a && c != b) {
-                        cur += groupSize[~(grid[i][j - 1] - 1)];
-                    }
-                    if (d != 0 && d != a && d != b && d != c) {
-                        cur += groupSize[~(grid[i][j + 1] - 1)];
-                    }
-                    res = std::max(res, cur);
+                    maxSize = max(maxSize, largeIslandSize);
                 }
             }
         }
-
-        return res == 0 ? n * n : res;
+        return maxSize == 0 ? n * n : maxSize;
     }
 };
