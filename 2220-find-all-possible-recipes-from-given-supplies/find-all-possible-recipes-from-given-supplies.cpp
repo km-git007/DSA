d929022bf1680f298e76bc9a70054f85406de7e7
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-06-16 00:39:49 +0530

    Time: 174 ms (32.35%) | Memory: 165 MB (28.90%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,55 +1,48 @@
 class Solution {
 public:
-
-    unordered_map<string, int> inDegree;
-    unordered_map<string, unordered_set<string>> buildGraph(vector<string>& recipes, vector<vector<string>>& ingredients) 
+    void buildGraph(vector<string>& recipes, vector<vector<string>>& ingredients, 
+    unordered_map<string, int> &ind, unordered_map<string, vector<string>> &adj)
     {
-        unordered_map<string, unordered_set<string>> adj;  // Adjacency list
-        for (int i = 0; i < recipes.size(); i++) 
+        for(int i = 0; i < recipes.size(); i++)
         {
             string recipe = recipes[i];
-            // Number of ingredients needed
-            inDegree[recipe] = ingredients[i].size();  
+            for(string ingredient : ingredients[i])
+            {
+                if(!adj.count(ingredient))
+                adj[ingredient] = vector<string>();
 
-            // Edge: ingredient -> recipe
-            for(const string& ingredient : ingredients[i])
-            adj[ingredient].insert(recipe);  
+                adj[ingredient].push_back(recipe);
+                ind[recipe]++;
+            }
         }
-
-        return adj;  // Return the graph
     }
 
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) 
     {
-        vector<string> res;
+        unordered_map<string, vector<string>> adj;
+        unordered_map<string, int> ind;
+        buildGraph(recipes, ingredients, ind, adj);
 
-        // any of the supply item doesnt need any other item to be made. It can exist independently.
-        for(string supply : supplies)
-        inDegree[supply] = 0;
-
-        // build adj List
-        auto adj = buildGraph(recipes, ingredients);
-
-        // contains all the recipes yet to be made
         queue<string> q;
-        for(auto [item, ind] : inDegree)
-        {
-            if(ind == 0)
-            q.push(item);
-        }
+        for(string supply : supplies)
+        q.push(supply);
 
+        vector<string> res;
         while(!q.empty())
         {
-            auto item = q.front();
+            string node = q.front();
             q.pop();
 
-            for(auto adjItem : adj[item])
+            if(!adj.count(node))
+            continue;
+
+            for(string adjNode : adj[node])
             {
-                inDegree[adjItem]--;
-                if(inDegree[adjItem] == 0)
+                ind[adjNode]--;
+                if(ind[adjNode] == 0)
                 {
-                    q.push(adjItem);
-                    res.push_back(adjItem);
+                    q.push(adjNode);
+                    res.push_back(adjNode);
                 }
             }
         }
