0791face4596f8da8dd40d21065f7cbfa3491afc
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-07-24 23:44:54 +0530

    Time: 27 ms (95.75%) | Memory: 58.5 MB (77.76%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,28 +1,22 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) 
+    public int[] maxSlidingWindow(int[] nums, int k)
     {
-        int n = nums.length;
-        int[] res = new int[n-k+1];
+        int [] result = new int[nums.length - k + 1];
         Deque<Integer> deque = new ArrayDeque<>();
-		int i = 0, index=0;
-		for(int j = 0; j < n; j++)
-		{
-			while(!deque.isEmpty() && nums[j] > deque.peekLast())
-            deque.pollLast();
-
-            deque.addLast(nums[j]);
-
-            if(j - i + 1 == k)
-            {
-                res[index++] = deque.peekFirst();
-
-                if(nums[i] == deque.peekFirst())
-                deque.pollFirst();
-
+        int i = 0;
+        for(int j = 0; j < nums.length; j++) {
+            while(!deque.isEmpty() && deque.peekLast() < nums[j]) {
+                deque.pollLast();
+            }
+            deque.offerLast(nums[j]);
+            if(j - i + 1 == k) {
+                result[i] = deque.peekFirst();
+                if(deque.peekFirst() == nums[i]) {
+                    deque.pollFirst();
+                }
                 i++;
             }
-		}
-
-        return res;
+        }
+        return result;
     }
 }
