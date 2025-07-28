5a537a1bb7d3b1b74b8b06b73ee21e8a89578255
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-07-28 06:12:07 +0530

    Time: 5 ms (84.50%) | Memory: 45.7 MB (10.79%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,46 +1,32 @@
 class Solution {
-    public int[] asteroidCollision(int[] asteroids) 
-    {
-        List<Integer> list=new ArrayList<>();
-        for (int i=0;i<asteroids.length;i++) 
+        public int[] asteroidCollision(int[] asteroids)
         {
-            // Positive element encountered then simply add it to the list.
-            if(asteroids[i]>0)
-            list.add(asteroids[i]);
-
-            // Negative element encountered
-            else 
-            {
-                // To keep track of blast. For example asteroids = [10, -10]
-                boolean blast=false;
-
-                // Keep popping till you either empty the list or you keep encountering a positive element
-                // at the back of the list having value less than the absolute value of the current negative element.
-                while(!list.isEmpty() && list.get(list.size()-1)>0 && list.get(list.size()-1)<=Math.abs(asteroids[i])) 
-                {
-                    if(list.get(list.size()-1)==Math.abs(asteroids[i])) 
-                    {
-                        blast=true;
-                        list.remove(list.size()-1);
-                        break;
-                    }
-                    list.remove(list.size()-1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < asteroids.length; i++)
+        {
+            if(asteroids[i] < 0){
+                while(!stack.isEmpty() && stack.peek() > 0 && Math.abs(stack.peek()) < Math.abs(asteroids[i])){
+                    stack.pop();
                 }
-
-                // Add the current negative element to the list if the list is empty or you've encountered a 
-                // negative element at the back of the list and no blast has occurred.
-                // Again the same example --> asteroids = [10, -10]
-                // For the above example, even after the list being empty, '-10' won't be added to the list.
-                if((list.isEmpty() || list.get(list.size()-1)<0) && !blast)
-                list.add(asteroids[i]);
+                
+                if(stack.isEmpty() || stack.peek() < 0){
+                    stack.push(asteroids[i]);
+                    continue;
+                }
+                // if two of them are of the same magnitude
+                if(Math.abs(stack.peek()) == Math.abs(asteroids[i])){
+                    stack.pop();
+                }
+            }
+            else{
+                stack.push(asteroids[i]);
             }
         }
-
-        //Convert list to array
-        int[] result=new int[list.size()];
-        for(int i=0;i<list.size();i++)
-        result[i]=list.get(i);
-
+        
+        int[] result = new int[stack.size()];
+        for(int i = result.length - 1; i >= 0; i--){
+            result[i] = stack.pop();
+        }
         return result;
     }
 }
