a450c9b0691e45185b592db0bb68c35c9f0a2218
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-07-29 00:35:55 +0530

    Time: 24 ms (95.94%) | Memory: 55.4 MB (53.43%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,22 +1,32 @@
+class Node{
+    int day;
+    int price;
+
+    public Node(int day, int price) {
+        this.day = day;
+        this.price = price;
+    }
+}
 class StockSpanner {
 
-    private ArrayDeque<int[]> stack = new ArrayDeque<>();
-    int index = 0, span = 1;
-    public StockSpanner() {}
+    private Deque<Node> stack;
+    int index = 1;
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
+    }
 
-    public int next(int price) 
-    {
-        while(!stack.isEmpty() && stack.peek()[0] <= price)
-        stack.pop();
-
-        if(stack.isEmpty())
-        span = index + 1;
-        else
-        span = index - stack.peek()[1];
-
-        stack.push(new int[]{price,index});
+    public int next(int price) {
+        while(!stack.isEmpty() && stack.peek().price <= price) {
+            stack.pop();
+        }
         
+        int span;
+        if(stack.isEmpty()) span = index;
+        else span = index - stack.peek().day;
+        
+        stack.push(new Node(index, price));
         index++;
+        
         return span;
     }
 }
