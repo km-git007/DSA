5b92f55797ca0276c5a35b2dc31f7f8edd5a74e1
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-07-28 06:35:14 +0530

    Time: 2 ms (96.65%) | Memory: 42 MB (31.38%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,27 +1,23 @@
 class Solution {
-    private boolean isValid(Deque<Character> stack, char ch) {
-        if(ch == ')' && stack.peek() != '('){
-            return false;
-        }
-        else if(ch == ']' && stack.peek() != '['){
-            return false;
-        }
-        else if(ch == '}' && stack.peek() != '{'){
-            return false;
-        }
-        
-        stack.pop();
-        return true;
-    }
-
-    public boolean isValid(String s) {
+    public boolean isValid(String s) 
+    {
         Deque<Character> stack = new ArrayDeque<>();
-        for(char ch : s.toCharArray()) {
-            if(ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            }
-            else if(stack.isEmpty() || !isValid(stack, ch)){
+        Map<Character, Character> matchingBrackets = Map.of(
+                ')', '(',
+                ']', '[',
+                '}', '{'
+        );
+        for(char ch : s.toCharArray()) 
+        {
+            if(ch == '(' || ch == '[' || ch == '{')
+            stack.push(ch);
+            
+            else 
+            {
+                if(stack.isEmpty() || stack.peek() != matchingBrackets.get(ch))
                 return false;
+
+                stack.pop();
             }
         }
         return stack.isEmpty();
