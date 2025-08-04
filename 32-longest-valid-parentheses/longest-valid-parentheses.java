32192b76111ab335950f948d4ce00349e8dd65c3
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-04 12:12:54 +0530

    Time: 3 ms (79.00%) | Memory: 44.8 MB (5.02%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int longestValidParentheses(String s) 
-    {
+    public int longestValidParentheses(String s) {
         int maxLength = 0;
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(-1);
-        for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(ch == '('){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
                 stack.push(i);
             }
             else{
                 stack.pop();
                 if(stack.isEmpty()){
                     stack.push(i);
+                    continue;
                 }
-                else{
-                    maxLength = Math.max(maxLength, i - stack.peek());
-                }
+                maxLength = Math.max(maxLength, i - stack.peek());
             }
         }
         return maxLength;
