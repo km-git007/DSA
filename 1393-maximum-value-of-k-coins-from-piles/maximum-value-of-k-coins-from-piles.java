9a2835017198be6595bf0195dd26778a90e913cc
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-04 13:30:05 +0530

    Time: 320 ms (22.42%) | Memory: 53.8 MB (29.15%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.java
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.java
@@ -1,31 +1,18 @@
 class Solution {
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-
-        int[] dp = new int[k+1];
-        int count = 0;
-
-        for(List<Integer> pile : piles){
-
-            int n = Math.min(pile.size(), k);
-            int[] preSum = new int[n+1];
-            int sum = 0;
-            for(int i = 0 ; i < n; ++i){
-               sum += pile.get(i);
-               preSum[i + 1] = sum;
-            }
-
-            count += n;
-
-            for(int i = Math.min(k, count); i > 0; --i){
-              int cur = dp[i];
-              for(int j = Math.min(n, i); j > 0; --j)
-                  cur = Math.max(cur, dp[i - j] + preSum[j]);
-              
-               dp[i] = cur;              
-            }
-
-        }
         
-        return dp[k];
+        int[][] dp = new int[piles.size() + 1][k + 1];
+        for(int index = 1; index <= piles.size(); index++){
+            for(int coins = 1; coins <= k; coins++){
+                int notTake = dp[index - 1][coins];
+                int take = 0, total  = 0;
+                for(int i = 0; i < piles.get(index - 1).size() && i < coins; i++){
+                    total += piles.get(index - 1).get(i);
+                    take = Math.max(take, total + dp[index - 1][coins - i - 1]);
+                }
+                dp[index][coins] = Math.max(notTake, take);
+            }
+        }
+        return dp[piles.size()][k];
     }
 }
