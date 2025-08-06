adc7ccd180b0412a09dad1df2da3e8c048cd62cd
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-07 04:48:18 +0530

    Time: 20 ms (27.11%) | Memory: 49.5 MB (8.30%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,69 +1,51 @@
 class Solution {
-
-    private List<List<int[]>> buildGraph(int[][] times, int n) 
-    {
-        List<List<int[]>> adj = new ArrayList<>();
-        for(int i = 0; i <= n; i++) 
-        adj.add(new ArrayList<>());
-
-        for(int[] time : times) 
-        {
-            int u = time[0];
-            int v = time[1];
-            int t = time[2];
-            adj.get(u).add(new int[]{t, v});
+    private List<List<Integer>>[] buildGraph(int n, int[][] times){
+        List<List<Integer>>[] adj = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
+
+        for(int[] time : times) {
+            int currNode = time[0];
+            int neighbour = time[1];
+            int distance = time[2];
+            adj[currNode].add(List.of(neighbour, distance));
+        }
+
         return adj;
     }
 
-    public int networkDelayTime(int[][] times, int n, int k) 
-    {
-        // Create adjacency list
-        var adj = buildGraph(times, n);
-
-        // Create a time array
-        int[] time = new int[n + 1];
-        Arrays.fill(time, Integer.MAX_VALUE);
-        time[k] = 0;
-
-        // Visited array to optimize Dijkstra's
-        boolean[] visited = new boolean[n + 1];
-
-        // Min heap (priority queue) -> (time, node)
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+    public int networkDelayTime(int[][] times, int n, int k) {
+        var adj = buildGraph(n, times);
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k] = 0;
+        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         pq.offer(new int[]{0, k});
 
         while (!pq.isEmpty()) {
-            int[] p = pq.poll();
-            int currTime = p[0];
-            int node = p[1];
+            var curr = pq.poll();
+            int distance = curr[0];
+            int node = curr[1];
 
-            if (visited[node]) 
-            continue;
+            if (distance > dist[node]) continue;
 
-            // Mark the node as visited
-            visited[node] = true;
-
-            for (int[] neighbor : adj.get(node)) 
-            {
-                int edgeTime = neighbor[0];
-                int adjNode = neighbor[1];
-                if (time[adjNode] > edgeTime + currTime) 
-                {
-                    time[adjNode] = edgeTime + currTime;
-                    pq.offer(new int[]{time[adjNode], adjNode});
+            for(var adjNode : adj[node]) {
+                int nbr = adjNode.get(0);
+                int weight = adjNode.get(1);
+                if(distance + weight < dist[nbr]) {
+                    dist[nbr] = distance + weight;
+                    pq.offer(new int[]{dist[nbr], nbr});
                 }
             }
         }
-
-        int maxTime = 0;
-        // Check if all nodes are reachable
-        for (int i = 1; i <= n; i++) 
-        {
-            if (time[i] == Integer.MAX_VALUE) 
+        
+        int minTime = 0;
+        for(int i = 1; i <= n; i++) {
+            if(dist[i] == Integer.MAX_VALUE)
             return -1;
-            maxTime = Math.max(maxTime, time[i]);
+            minTime = Math.max(minTime, dist[i]);
         }
-        return maxTime;
+        return minTime;
     }
 }
