707a48ca4e813771ff78d6032dad603ff2ef2357
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-13 00:17:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (32.47%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -16,29 +16,30 @@
 class Solution {
     private int levelX, levelY;
     private TreeNode parentX, parentY;
-    void preorder(TreeNode root, TreeNode parent, int level, int x, int y) {
-        if (root == null) {
-            return;
-        }
+    void preorder(TreeNode root, TreeNode parent, int level, int x, int y)
+    {
+        if(root == null)
+        return;
 
-        if(root.val == x) {
+        if(root.val == x) 
+        {
             levelX = level;
             parentX = parent;
         }
 
-        if(root.val == y) {
+        if(root.val == y) 
+        {
             levelY = level;
             parentY = parent;
         }
+        
         preorder(root.left, root, level + 1, x, y);    
         preorder(root.right, root, level + 1, x, y);   
     }
 
-    public boolean isCousins(TreeNode root, int x, int y) {
+    public boolean isCousins(TreeNode root, int x, int y) 
+    {
         preorder(root, null, 0, x, y);
-        if(levelX == levelY && parentX != parentY) {
-            return true;
-        }
-        return false;
+        return levelX == levelY && parentX != parentY;
     }
 }
