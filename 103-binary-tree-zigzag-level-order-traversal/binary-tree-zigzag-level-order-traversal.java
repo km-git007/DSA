10da408dd26ba58c4732ebb2b671703fd5f12f1f
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-12 23:29:10 +0530

    Time: 1 ms (63.83%) | Memory: 41.8 MB (98.60%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,48 +14,29 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
-    {
-        List<List<Integer>> result = new ArrayList<>();
-        // return empty list if the tree is empty
-        if(root == null)
-        return result; 
-        
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>>  result = new ArrayList<>();
+        if (root == null) return result;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        queue.add(root);
         
-        int level = 0;
-        // Process each level of the tree
-        while (!queue.isEmpty()) 
-        {
-            // number of nodes at current level
-            int levelSize = queue.size();  
-            List<Integer> list = new ArrayList<>();    
-            // Traverse nodes at the current level
-            for (int i = 0; i < levelSize; i++) 
-            {
-                TreeNode current = queue.poll();
-                list.add(current.val);
-                
-                // Add children to the queue for the next level
-                if(current.left != null)
-                queue.offer(current.left);
-                
-                if(current.right != null) 
-                queue.offer(current.right);
+        boolean flag = false;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelList = new ArrayList<>();
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
             }
-
-            // if the curr level is odd the reverse the list
-            if(level % 2 == 1)
-            Collections.reverse(list);
-
-            // Add the current level's list to the result
-            result.add(list);
-
-            // increase the level
-            level++;
+            if(flag) {
+                Collections.reverse(levelList);
+            }
+            flag = !flag;
+            result.add(new ArrayList<>(levelList));
         }
-        // return the result
         return result;
     }
 }
