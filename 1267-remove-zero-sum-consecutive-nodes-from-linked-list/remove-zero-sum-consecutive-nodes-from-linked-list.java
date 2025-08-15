7baa69a52e173721adf177bcfc22b60805dc17dd
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-15 22:37:42 +0530

    Time: 3 ms (49.32%) | Memory: 44.5 MB (32.53%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -9,36 +9,33 @@
  * }
  */
 class Solution {
-    public ListNode removeZeroSumSublists(ListNode head) 
-    {
-        ListNode dummy=new ListNode(-1);
-        dummy.next=head;
+    public ListNode removeZeroSumSublists(ListNode head) {
+                ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
 
-        HashMap<Integer,ListNode> map=new HashMap<>();
-        map.put(0, dummy);
-
-        ListNode curr=head;
-        int pSum=0;
-        while(curr!=null)
-        {
-            pSum += curr.val;
-            if(map.containsKey(pSum))
-            {
-                ListNode node = map.get(pSum).next;
-                int sum = pSum + node.val;
-                while(sum != pSum)
-                {
-                    node = node.next;
-                    map.remove(sum);
-                    sum += node.val;
+        Map<Integer, ListNode> map = new HashMap<>();
+        map.put(0, dummyHead);
+        
+        ListNode curr = head;
+        int prefixSum = 0;
+        while(curr != null){
+            prefixSum += curr.val;
+            if(map.containsKey(prefixSum)){
+                ListNode ptr = map.get(prefixSum).next;
+                int tempSum = prefixSum;
+                // remove the prefix sum stored in the map
+                while(ptr != curr){
+                    tempSum += ptr.val;
+                    map.remove(tempSum);
+                    ptr = ptr.next;
                 }
-                map.get(pSum).next = curr.next;
+                // make connection
+                map.get(prefixSum).next = curr.next;
+            }else {
+                map.put(prefixSum, curr);
             }
-            else
-            map.put(pSum, curr);
-
             curr = curr.next;
         }
-        return dummy.next;
+        return dummyHead.next;
     }
 }
