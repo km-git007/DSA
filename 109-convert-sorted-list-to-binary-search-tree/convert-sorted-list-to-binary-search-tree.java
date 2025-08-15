1891574b8fdf6fea23730e2c42834fa5ed60200b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-16 00:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (6.43%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -1,66 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private ListNode[] findMiddleNode(ListNode start) {
+    private ListNode findMiddleNode(ListNode start, ListNode end) {
         ListNode slow = start, fast = start;
-        ListNode prev = null;
-        while(fast != null && fast.next != null){
-            prev = slow;
+        while(fast != end && fast.next != end){
             slow = slow.next;
             fast = fast.next.next;
         }
-        return new ListNode[]{slow, prev};
+        return slow;
     }
 
-    private TreeNode buildBST(ListNode start) {
-        if(start == null) return null;
+    private TreeNode buildBST(ListNode start, ListNode end) {
+        if(start == end) return null;
 
-        var middleAndPrev = findMiddleNode(start);
-        ListNode middle = middleAndPrev[0];
-        ListNode prev = middleAndPrev[1];
+        // find the middle element
+        ListNode middle = findMiddleNode(start, end); 
 
         // create the new node
         TreeNode newNode = new TreeNode(middle.val);
-
-        if(start == middle){
-            return newNode;
-        }
-
-        newNode.right = buildBST(middle.next);
-        // very essential step
-        middle.next = null;
-        if(prev != null){
-            prev.next = null;
-        }
-        newNode.left = buildBST(start);
-
+        
+        newNode.left = buildBST(start, middle);
+        newNode.right = buildBST(middle.next, end);
         return newNode;
     }
-    
+
     public TreeNode sortedListToBST(ListNode head) {
-        return buildBST(head);
+        return buildBST(head, null);
     }
 }
