8fb3e86679c4d4479efd2129c59cd31027299c5c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-15 17:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (55.47%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,24 +14,28 @@
  * }
  */
 class Solution {
-
-    int index;
-    private TreeNode buildBST(int[] preorder, int ub)
-    {
-        if(index == preorder.length || preorder[index] > ub)
-        return null;
-
-        TreeNode node = new TreeNode(preorder[index++]);
-
-        node.left = buildBST(preorder,node.val);
-        node.right = buildBST(preorder,ub);
-
+    private TreeNode constructBST(int start, int end, int[] preorder) {
+        if(start > end) return null;
+        
+        TreeNode node = new TreeNode(preorder[start]);
+        int inorderSuccesorIndex = findInorderSuccessor(start + 1, preorder, node.val);
+        if(inorderSuccesorIndex != -1){
+            node.right = constructBST(inorderSuccesorIndex, end, preorder);
+            node.left = constructBST(start + 1, inorderSuccesorIndex - 1, preorder);
+        }else{
+            node.left = constructBST(start + 1, end, preorder);
+        }
         return node;
     }
 
-    public TreeNode bstFromPreorder(int[] preorder) 
-    {
-        index = 0;
-        return buildBST(preorder, Integer.MAX_VALUE);
+    private int findInorderSuccessor(int start, int[] preorder, int key) {
+        for(int i = start; i < preorder.length; i++){
+            if(preorder[i] > key) return i;
+        }
+        return -1;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return constructBST(0, preorder.length - 1, preorder);
     }
 }
