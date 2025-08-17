4313e72f3b2b393c8b1c7f732a9c056a4380adc8
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-17 12:08:05 +0530

    Time: 12 ms (85.27%) | Memory: 45.6 MB (65.08%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,21 +1,30 @@
 class Solution {
     public String frequencySort(String s) {
-                // Count frequencies
+        // Count frequencies
         Map<Character, Integer> freqMap = new HashMap<>();
         for(char ch : s.toCharArray()) {
             freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
         }
         
-        // Sort by frequency in descending order
-        List<Character> chars = new ArrayList<>(freqMap.keySet());
-        chars.sort((a, b) -> freqMap.get(b) - freqMap.get(a));
-        
-        // Build result
-        StringBuilder sb = new StringBuilder();
-        for(char ch : chars) {
+        // Bucket sort by frequency
+        List<Character>[] buckets = new List[s.length() + 1];
+        for(char ch : freqMap.keySet()) {
             int freq = freqMap.get(ch);
-            for(int i = 0; i < freq; i++) {
-                sb.append(ch);
+            if(buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(ch);
+        }
+        
+        // Build result from highest frequency to lowest
+        StringBuilder sb = new StringBuilder();
+        for(int freq = buckets.length - 1; freq >= 0; freq--) {
+            if(buckets[freq] != null) {
+                for(char ch : buckets[freq]) {
+                    for(int i = 0; i < freq; i++) {
+                        sb.append(ch);
+                    }
+                }
             }
         }
         
