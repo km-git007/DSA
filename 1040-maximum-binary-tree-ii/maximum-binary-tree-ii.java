7c12731dce4256307a35f4170ade3d02e03b35e8
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-25 17:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (91.63%) - LeetSync

--- 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.java
+++ 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.java
@@ -1,38 +1,15 @@
 class Solution {
-    private void inorderTraversal(TreeNode root, List<Integer> nums){
-        if(root == null)
-        return;
-
-        inorderTraversal(root.left, nums);
-        nums.add(root.val);
-        inorderTraversal(root.right, nums);
-    }
-
-    private TreeNode constructMaximumBinaryTree(int[] nums) {
-        Deque<TreeNode> stack = new LinkedList<>();
-        TreeNode root = new TreeNode(-1);
-        for (int num : nums) {
-            TreeNode node = new TreeNode(num);
-            if(node.val > root.val){
-                root = node;
-            }
-            while (!stack.isEmpty() && stack.peek().val < node.val) {
-                node.left = stack.pop();
-            }
-
-            if (!stack.isEmpty()) {
-                stack.peek().right = node;
-            }
-
-            stack.push(node);
-        }
-        return root;
-    }
-
     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
-        List<Integer> nums = new ArrayList<>();
-        inorderTraversal(root, nums);
-        nums.add(val);
-        return constructMaximumBinaryTree(nums.stream().mapToInt(Integer::intValue).toArray());
+        if(root == null)
+        return new TreeNode(val);
+
+        if(root.val < val){
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
     }
 }
