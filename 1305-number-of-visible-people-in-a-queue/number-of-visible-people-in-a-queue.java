c7c5124f1806ef6b91fb3265abca3e3cdbd8c886
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-25 13:59:04 +0530

    Time: 20 ms (96.27%) | Memory: 60.1 MB (41.17%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
@@ -1,22 +1,18 @@
 class Solution {
-    public int[] canSeePersonsCount(int[] heights) 
-    {
-        int[] canSeePersonsCount = new int[heights.length];
+    public int[] canSeePersonsCount(int[] heights) {
         Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = heights.length - 1; i >= 0; i--)
-        {
+        int[] numPeople = new int[heights.length];
+        for(int i = heights.length - 1; i >= 0; i--){
+            int count = 0;
             while(!stack.isEmpty() && stack.peek() < heights[i]){
                 stack.pop();
-                canSeePersonsCount[i]++;
+                count++;
             }
-            // if stack isn't empty then though the current minion is
-            // smaller than that at the top of the stack but, it can still see it.
-            if(!stack.isEmpty()){
-                canSeePersonsCount[i]++;
-            }
-            
+
+            numPeople[i] = stack.isEmpty() ? count : count + 1;
+
             stack.push(heights[i]);
         }
-        return canSeePersonsCount;
+        return numPeople;
     }
 }
