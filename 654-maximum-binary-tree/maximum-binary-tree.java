d62aba09baa46602cbb8a818e31ab8b435aad8d7
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-25 14:37:34 +0530

    Time: 9 ms (14.66%) | Memory: 45.1 MB (49.46%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.java
+++ 654-maximum-binary-tree/maximum-binary-tree.java
@@ -1,43 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    private int findMaxIndex(int start, int end, int[] nums){
-        int maxIndex = start;
-        for(int i = start + 1; i <= end; i++){
-            if(nums[i] > nums[maxIndex]){
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-    
-    private TreeNode solve(int start, int end, int[] nums){
-        if(start > end){
-            return null;
-        }
-        
-        int maxIndex = findMaxIndex(start, end, nums);
-        TreeNode root = new TreeNode(nums[maxIndex]);
-        root.left = solve(start, maxIndex - 1, nums);
-        root.right = solve(maxIndex + 1, end, nums);
-        
-        return root;
-    }
-    
+class Solution {    
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return solve(0, nums.length - 1, nums);
+        Deque<TreeNode> stack = new LinkedList<>();
+        for (int num : nums) {
+            TreeNode node = new TreeNode(num);
+            while (!stack.isEmpty() && stack.peek().val < node.val) {
+                node.left = stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                stack.peek().right = node;
+            }
+
+            stack.push(node);
+        }
+        
+        while(stack.size() > 1){
+            stack.pop();
+        }
+        
+        return stack.pop();
     }
 }
