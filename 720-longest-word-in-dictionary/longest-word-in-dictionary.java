65bbe8e4f8a67cf97395cd4ce0f7e24e5807aa08
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-26 13:19:17 +0530

    Time: 39 ms (7.67%) | Memory: 45.2 MB (52.72%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,66 +1,22 @@
 class Solution {
-
-    private class Trie
-    {
-        boolean isEnd;
-        Trie[] children;
-        Trie()
-        {
-            isEnd = false;
-            children = new Trie[26];
-        }
-    }
-    
-    private Trie root;
-
-    private void insert(String word) 
-    {
-        Trie curr=root;
-        for(char c : word.toCharArray()) 
-        {
-            int index=c-'a';
-            if(curr.children[index]==null) 
-            curr.children[index] = new Trie();
-
-            curr=curr.children[index];
-        }
-        curr.isEnd=true;
-    }
-
-    public Solution()
-    {
-        root = new Trie();
-    }
-
-    private boolean canBuild(String word)
-    {
-        Trie curr = root;
-        for(char c : word.toCharArray())
-        {
-            int index = c - 'a';
-            if(curr.children[index] == null || !curr.children[index].isEnd)
-            return false;
-
-            curr = curr.children[index];
-        }
-        return true;
-    }
-
-    public String longestWord(String[] words) 
-    {
-        for(String word : words)
-        insert(word);
-
-        String result = "";
-        for(String word : words)
-        {
-            if(canBuild(word))
-            {
-                if(word.length() > result.length() || 
-                (word.length() == result.length() && word.compareTo(result) < 0))
-                result = word;
+    public String longestWord(String[] words) {
+        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
+        int longestLen = 0;
+        String longestWord = "";
+        for (String word : words) {
+            int currLen = 0;
+            for(int i = 0; i < word.length(); i++){
+                if(!wordSet.contains(word.substring(0, i + 1))){
+                    break;
+                }
+                currLen++;
+            }
+            String currWord = word.substring(0, currLen);
+            if(currLen > longestLen || (currLen == longestLen && currWord.compareTo(longestWord) <= 0)){
+                longestLen = currLen;
+                longestWord = currWord;
             }
         }
-        return result;
+        return longestWord;
     }
 }
