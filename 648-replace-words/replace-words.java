91b62e8b33b7741a43edcd1b70479ef5749a7aa3
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-26 14:33:47 +0530

    Time: 696 ms (15.04%) | Memory: 55.4 MB (63.86%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,59 +1,24 @@
-class TrieNode {
-    TrieNode[] children;
-    boolean isEnd;
-    TrieNode() {
-        children = new TrieNode[26];
-    }
-}
-
 class Solution {
- private TrieNode root;
-    private void insert(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
-                node.children[c - 'a'] = new TrieNode();
-            }
-            node = node.children[c - 'a'];
-        }
-        node.isEnd = true;
-    }
-
-    private boolean search(String word) {
-        TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
-                return false;
-            }
-            node = node.children[c - 'a'];
-        }
-        return node.isEnd;
-    }
-
-    Solution(){
-        root = new TrieNode();
-    }
-
     public String replaceWords(List<String> dictionary, String sentence)
     {
+        Set<String> wordSet = new HashSet<>(dictionary);
+        StringBuilder sb = new StringBuilder();
         String[] words = sentence.split(" ");
-        for(String word : dictionary){
-            insert(word);
-        }
-        
-        String[] res = new String[words.length];
-        for(int i = 0; i < words.length; i++){
-            for(int j = 0; j < words[i].length(); j++){
-                String prefix = words[i].substring(0, j + 1);
-                if(search(prefix)){
-                    res[i] = prefix;
+        for(String word : words){
+            boolean match = false;
+            for(int i = 0; i < word.length(); i++){
+                String prefix = word.substring(0, i + 1);
+                if(wordSet.contains(prefix)){
+                    sb.append(prefix).append(" ");
+                    match = true;
                     break;
                 }
             }
-            if(res[i] == null){
-                res[i] = words[i];
+            if(!match){
+                sb.append(word).append(" ");
             }
         }
-        return String.join(" ", res);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
