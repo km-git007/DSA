768d8eb19cd84ec0156ec8de1eb8a8c726f53094
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-26 13:39:02 +0530

    Time: 6 ms (87.03%) | Memory: 45 MB (72.14%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.java
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -1,22 +1,64 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+    TrieNode() {
+        children = new TrieNode[26];
+    }
+}
+
 class Solution {
-    public String longestWord(String[] words) {
-        Set<String> wordSet = new HashSet<>(Arrays.asList(words));
-        int longestLen = 0;
-        String longestWord = "";
-        for (String word : words) {
-            int currLen = 0;
-            for(int i = 0; i < word.length(); i++){
-                if(!wordSet.contains(word.substring(0, i + 1))){
-                    break;
-                }
-                currLen++;
+    private TrieNode root;
+    private void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
             }
-            String currWord = word.substring(0, currLen);
-            if(currLen > longestLen || (currLen == longestLen && currWord.compareTo(longestWord) <= 0)){
-                longestLen = currLen;
-                longestWord = currWord;
+            node = node.children[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    private boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isEnd;
+    }
+    
+    Solution(){
+        root = new TrieNode();
+        sb = new StringBuilder();
+        maxLen = 0;
+        longestWord = "";
+    }
+
+    private StringBuilder sb;
+    private int maxLen;
+    private String longestWord;
+    private void dfs(TrieNode root){
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            if(root.children[ch - 'a'] != null && root.children[ch - 'a'].isEnd){
+                sb.append(ch);
+                dfs(root.children[ch - 'a']);
+                if(sb.length() > maxLen){
+                    maxLen = sb.length();
+                    longestWord = sb.toString();
+                }
+                sb.deleteCharAt(sb.length() - 1);
             }
         }
+    }
+
+    public String longestWord(String[] words) {
+        for(String word : words){
+            insert(word);
+        }
+        dfs(root);
         return longestWord;
     }
 }
