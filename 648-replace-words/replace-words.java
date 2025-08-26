d66a347ce29e71d018d33b51d11b202c73534dd5
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-26 14:42:09 +0530

    Time: 11 ms (78.66%) | Memory: 55.2 MB (85.93%) - LeetSync

--- 648-replace-words/replace-words.java
+++ 648-replace-words/replace-words.java
@@ -1,24 +1,57 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+    TrieNode() {
+        children = new TrieNode[26];
+    }
+}
+
 class Solution {
-    public String replaceWords(List<String> dictionary, String sentence)
-    {
-        Set<String> wordSet = new HashSet<>(dictionary);
-        StringBuilder sb = new StringBuilder();
-        String[] words = sentence.split(" ");
-        for(String word : words){
-            boolean match = false;
-            for(int i = 0; i < word.length(); i++){
-                String prefix = word.substring(0, i + 1);
-                if(wordSet.contains(prefix)){
-                    sb.append(prefix).append(" ");
-                    match = true;
-                    break;
-                }
+ private TrieNode root;
+    private void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
             }
-            if(!match){
-                sb.append(word).append(" ");
+            node = node.children[c - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    private String search(String word) {
+        TrieNode node = root;
+        StringBuilder sb = new StringBuilder();
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return word;
+            }
+            sb.append(c);
+            node = node.children[c - 'a'];
+            if(node.isEnd){
+                return sb.toString();
             }
         }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
+        // control would never reach here
+        return word;
+    }
+
+    Solution(){
+        root = new TrieNode();
+    }
+
+    public String replaceWords(List<String> dictionary, String sentence)
+    {
+        String[] words = sentence.split(" ");
+        for(String word : dictionary){
+            insert(word);
+        }
+        
+        String[] res = new String[words.length];
+        int index = 0;
+        for(String word : words){
+            res[index++] = search(word);
+        }
+        return String.join(" ", res);
     }
 }
