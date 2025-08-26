85e4e842a3ff31419ef47d5a2c9f4f33107b8b3c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-26 13:05:20 +0530

    Time: 81 ms (43.53%) | Memory: 68.2 MB (69.79%) - LeetSync

--- 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.java
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.java
@@ -1,65 +1,50 @@
-class TreeNode{
-    int val;
-    TreeNode right, left;
-    TreeNode(int val) {
-        this.val = val;
-    }
-}
-
 class Solution {
-    private TreeNode buildTree(int[] parents){
-        Map<Integer, TreeNode> map = new HashMap<>();
-        TreeNode root = new TreeNode(0);
-        map.put(0, root);
-        for(int i = 1; i < parents.length; i++){
-            map.putIfAbsent(parents[i], new TreeNode(parents[i]));
-            TreeNode parent = map.get(parents[i]);
-            
-            map.putIfAbsent(i, new TreeNode(i));
-            TreeNode child = map.get(i);
-            if(parent.left == null){
-                parent.left = child;
-            }else{
-                parent.right = child;
-            }
+private int treeSize, count;
+    private long maxScore;
+    private int sizeOfTree(int node, List<Integer> [] children){
+
+        long score = 1;
+        // including the current node
+        int size = 1;
+
+        for(int child : children[node]){
+            int childSize = sizeOfTree(child, children);
+            score *= childSize;
+            size += childSize;
         }
-        return root;
-    }
-    
-    private int sizeOfTree(TreeNode root){
-        if(root == null) return 0;
-        return 1 + sizeOfTree(root.left) + sizeOfTree(root.right);
-    }
-    
-    private long maxScore = 0; 
-    private int treeSize, count = 0;
-    private int dfs(TreeNode root){
-        if(root == null) 
-        return 0;
 
-        int leftScore = dfs(root.left);
-        int rightScore = dfs(root.right);
-        int rest = treeSize - leftScore - rightScore - 1;
+        int rest = treeSize - size;
+        if(rest > 0){
+            score *= rest;
+        }
 
-        long scoreOfNode = 1;
-        if (leftScore > 0) scoreOfNode *= leftScore;
-        if (rightScore > 0) scoreOfNode *= rightScore;
-        if (rest > 0) scoreOfNode *= rest;
-
-        if(scoreOfNode > maxScore){
-            maxScore = scoreOfNode;
+        if(score > maxScore){
+            maxScore = score;
             count = 1;
-        }else if(scoreOfNode == maxScore){
+        }else if(score == maxScore){
             count++;
         }
 
-        return 1 + leftScore + rightScore;
+        return size;
+    }
+
+    private void buildTree(int[] parents, List<Integer> [] children){
+        for(int i = 0; i < parents.length; i++){
+            children[i] = new ArrayList<>();
+        }
+
+        for(int i = 1; i < parents.length; i++){
+            children[parents[i]].add(i);
+        }
+        return;
     }
 
     public int countHighestScoreNodes(int[] parents) {
-        TreeNode root = buildTree(parents);
-        treeSize = sizeOfTree(root);
-        dfs(root);
+        treeSize = parents.length;
+        maxScore = count = 0;
+        List<Integer> [] children = new ArrayList[parents.length];
+        buildTree(parents, children);
+        sizeOfTree(0, children);
         return count;
     }
 }
