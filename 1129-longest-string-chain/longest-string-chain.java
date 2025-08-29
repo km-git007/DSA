4a1eb63552140f4cfe22674d2d8b1a02fad6c075
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-08-29 17:12:08 +0530

    Time: 42 ms (74.29%) | Memory: 45.1 MB (33.06%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,35 +1,21 @@
-class Solution {
-    private Set<String> wordSet;
-    private Map<String, Integer> dp;
-    private int longestChain(String word){
-        if(word.isEmpty()) 
-        return 0;
-        
-        if(dp.containsKey(word)) 
-        return dp.get(word);
-        
-        int maxLen = 1;
-        for(int i = 0; i < word.length(); i++){
-            String newWord = word.substring(0, i) + word.substring(i + 1);
-            if(wordSet.contains(newWord)){
-                maxLen = Math.max(maxLen, 1 + longestChain(newWord));
-            }
-        }
-        dp.put(word, maxLen);
-        return maxLen;
-    }
-    
-    public int longestStrChain(String[] words) {
-        dp = new HashMap<>();
-
-        // add all the words in the set
-        wordSet = new HashSet<>();
-        wordSet.addAll(Arrays.asList(words));
-        
-        int longestChainLength = 0;
+class Solution {    
+    public int longestStrChain(String[] words) 
+    {
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        int maxChainLength = 1;    
+        Map<String, Integer> map = new HashMap<>();
         for(String word : words){
-            longestChainLength = Math.max(longestChain(word), longestChainLength);
+            map.put(word, 1);
+            int currentChainLength = 1;
+            for(int i = 0; i < word.length(); i++){
+                String previousWord = word.substring(0, i) + word.substring(i + 1);
+                if(map.containsKey(previousWord)){
+                    currentChainLength = Math.max(currentChainLength, map.get(previousWord) + 1);
+                }
+            }
+            map.put(word, currentChainLength);
+            maxChainLength = Math.max(maxChainLength, currentChainLength);
         }
-        return longestChainLength;
+        return maxChainLength;
     }
 }
