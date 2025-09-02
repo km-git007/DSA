cc9a63c128b0a4035f573649f2004313ba80c3e8
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-02 20:21:56 +0530

    Time: 8 ms (42.13%) | Memory: 44.5 MB (48.29%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,23 +1,18 @@
 class Solution {
-    private Set<String> wordSet;
-    private Map<String, Boolean> dp;
-    private boolean check(String word) {
-        if(wordSet.contains(word)) return true;
-        if(dp.containsKey(word)) return dp.get(word);
-        for(int i = 0; i < word.length(); i++){
-            String prefix = word.substring(0, i + 1);
-            String suffix = word.substring(i + 1);
-            if(wordSet.contains(prefix) && check(suffix)){
-                dp.put(word, true);
-                return true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && words.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        dp.put(word, false);
-        return false;
-    }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        wordSet = new HashSet<>(wordDict);
-        dp = new HashMap<>();
-        return check(s);
+        return dp[n];
     }
 }
