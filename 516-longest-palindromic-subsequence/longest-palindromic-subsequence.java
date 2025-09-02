01c5b4a533a23fa01a09ab9c4d562c4df08978c2
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-03 02:04:28 +0530

    Time: 24 ms (93.15%) | Memory: 56.2 MB (5.90%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) 
-    {
-        int n = s.length();
-        int[][] dp = new int[n][n];
-
-        // Base case: single-character substrings are palindromes of length 1
-        for(int i = 0; i < n; i++) 
-        dp[i][i] = 1;
-
-        // Build the dp table bottom-up
-        for (int len = 2; len <= n; len++) {  // Length of the substring
-            for (int start = 0; start <= n - len; start++) {
-                int end = start + len - 1;
-
-                if (s.charAt(start) == s.charAt(end)) {
-                    dp[start][end] = 2 + dp[start + 1][end - 1];
-                } else {
-                    dp[start][end] = Math.max(dp[start + 1][end], dp[start][end - 1]);
-                }
-            }
+    private int[][] dp;
+    private int solve(int start, int end, String s){
+        if(start > end){
+            return 0;
         }
 
-        // The result is stored in dp[0][n-1] (whole string)
-        return dp[0][n - 1];
+        if(start == end){
+            return 1;
+        }
+        
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
+        if(s.charAt(start) == s.charAt(end)){
+            return dp[start][end] = 2 + solve(start + 1, end - 1, s);
+        }
+
+        return dp[start][end] = Math.max(solve(start + 1, end, s), solve(start, end - 1, s));
+    }
+    
+    public int longestPalindromeSubseq(String s) {
+        dp = new int[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(0, s.length() - 1, s);
     }
 }
