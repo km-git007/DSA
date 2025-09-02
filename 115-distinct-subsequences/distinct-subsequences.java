968c6e970ef7d2cfeadead09665506236e8300ec
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-02 22:47:19 +0530

    Time: 20 ms (39.69%) | Memory: 51.4 MB (41.88%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,19 +1,36 @@
+
 class Solution {
-    public int numDistinct(String s, String t){
-        int[] prev = new int[t.length() + 1];
-        prev[0] = 1;
-        int[] curr = new int[t.length() + 1];
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = 1; j <= t.length(); j++){
-                if(s.charAt(i - 1) == t.charAt(j - 1)){
-                    curr[j] = prev[j - 1] + prev[j];
-                }
-                else {
-                    curr[j] = prev[j];
-                }
-            }
-            System.arraycopy(curr, 1, prev, 1, t.length());
+    private int[][] dp;
+    private int countDistinctSubsequences(int n, int m, String s, String t){
+        if(m == 0){
+            return 1;
         }
-        return prev[t.length()];
+
+        if(n == 0){
+            return 0;
+        }
+        
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+
+        int count = 0;
+        // if the (m - 1)th char doesn't match (n - 1)th char
+        count += countDistinctSubsequences(n - 1, m, s, t);
+
+        // if chars match
+        if(s.charAt(n - 1) == t.charAt(m - 1)){
+            count += countDistinctSubsequences(n - 1, m - 1, s, t);
+        }
+
+        return dp[n][m] = count;
+    }
+
+    public int numDistinct(String s, String t) {
+        dp = new int[s.length() + 1][t.length() + 1];
+        for(int i = 0; i <= s.length(); i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return countDistinctSubsequences(s.length(), t.length(), s, t);
     }
 }
