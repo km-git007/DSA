92037e8b3f25dac1e31df6a3417ff0fd327525d9
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-03 18:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (87.85%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,24 +1,30 @@
 class Solution {
-    public int numDecodings(String s) 
-    {
-        int n = s.length();
-        // empty string can be decoded in one way
-        int plusOne = 1;
-        int plusTwo = -1, curr;
-        for(int i = n - 1; i >= 0; i--)
-        {
-            if(s.charAt(i) == '0')
-            curr = 0;
-            else
-            {
-                curr = plusOne;
 
-                if(i + 1 < n && Integer.parseInt(s.substring(i, i + 2)) <= 26)
-                curr += plusTwo;
-            }
-            plusTwo = plusOne;
-            plusOne = curr;
+    private int[] dp;
+    private int decode(int index, String s){
+        if(index >= s.length()){
+            return 1;
         }
-        return plusOne;
+
+        // leading zero found, so return '0'. this is invalid grouping
+        if(s.charAt(index) == '0'){
+            return 0;
+        }
+        
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int ways = decode(index + 1, s);
+        if(index + 1 < s.length() && (s.charAt(index) == '1' || (s.charAt(index) == '2' && s.charAt(index + 1) <= '6'))){
+            ways += decode(index + 2, s);
+        }
+
+        return dp[index] = ways;
+    }
+
+    public int numDecodings(String s) {
+        dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return decode(0, s);
     }
 }
