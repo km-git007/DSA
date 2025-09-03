ae51f7ac75eb74d73215de59e4c86485fa0cec9c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-03 18:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (94.57%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,30 +1,20 @@
 class Solution {
-
-    private int[] dp;
-    private int decode(int index, String s){
-        if(index >= s.length()){
-            return 1;
-        }
-
-        // leading zero found, so return '0'. this is invalid grouping
-        if(s.charAt(index) == '0'){
-            return 0;
-        }
-        
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        int ways = decode(index + 1, s);
-        if(index + 1 < s.length() && (s.charAt(index) == '1' || (s.charAt(index) == '2' && s.charAt(index + 1) <= '6'))){
-            ways += decode(index + 2, s);
-        }
-
-        return dp[index] = ways;
-    }
-
     public int numDecodings(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return decode(0, s);
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                int ways = 0;
+                ways += dp[i + 1];
+                if(i + 1 < n && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) <= '6'))){
+                    ways += dp[i + 2];
+                }
+                dp[i] = ways;
+            }
+        }
+        return dp[0];
     }
 }
