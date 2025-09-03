4093de7cf506a91be5f7c45875de12d66c3aaf6b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-03 19:49:51 +0530

    Time: 21 ms (69.54%) | Memory: 45.7 MB (58.05%) - LeetSync

--- 2348-count-number-of-texts/count-number-of-texts.java
+++ 2348-count-number-of-texts/count-number-of-texts.java
@@ -1,36 +1,22 @@
 class Solution {
-    private Set<Character> set;
-    private final int MOD = 1_000_000_007;
-    private int[] dp;
-    private int count(int index, String s){
-        if(index >= s.length()){
-            return 1;
-        }
-        
-        if(dp[index] != -1){
-            return dp[index];
-        }
-
-        char ch = s.charAt(index);
-        int loop = set.contains(ch) ? 4 : 3;
-        int ways = 0;
-
-        for(int k = index; k - index < loop && k < s.length(); k++){
-            if(s.charAt(k) != ch){
-                break;
-            }
-            ways = (ways + count(k + 1, s)) % MOD;
-        }
-        return dp[index] = ways;
-    }
-
     public int countTexts(String pressedKeys) {
-        set = new HashSet<>();
-        set.add('7');
-        set.add('9');
+        Set<Character> set = new HashSet<>();
+        set.add('7'); set.add('9');
 
-        dp = new int[pressedKeys.length()];
-        Arrays.fill(dp, -1);
-        return count(0, pressedKeys);
+        final int MOD = 1_000_000_007;
+        int n = pressedKeys.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--){
+            char ch = pressedKeys.charAt(i);
+            int loop = set.contains(ch) ? 4 : 3;
+            for(int k = i; k - i < loop && k < n; k++){
+                if(pressedKeys.charAt(k) != ch){
+                    break;
+                }
+                dp[i] = (dp[i] + dp[k + 1]) % MOD;
+            }
+        }
+        return dp[0];
     }
 }
