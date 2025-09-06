b72090fdce3b74b7f5ada99bc02cf15eb4a68e1c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-07 01:29:18 +0530

    Time: 2 ms (48.88%) | Memory: 48.4 MB (9.02%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,66 @@
-class Solution {
-    private int n;
-    private void solve(int row,boolean[] vis,int[][] isConnected)
-    {
-        for(int col=0;col<n;col++)
-        {
-            if(isConnected[row][col]==1 && !vis[col])
-            {
-                vis[col]=true;
-                solve(col,vis,isConnected);
-            }
+class DSU{
+    int[] parent, size;
+    DSU(int n){
+        size = new int[n];
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        Arrays.fill(size, 1);
+    }
+
+    public int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int x, int y){
+        int parentX = find(x);
+        int parentY = find(y);
+
+        if(parentX == parentY){
+            return;
+        }
+        
+        if(size[parentX] <= size[parentY]){
+            parent[parentX] = parentY;
+            size[parentY] += size[parentX];
+        }
+        else{
+            parent[parentY] = parentX;
+            size[parentX] += size[parentY];
         }
     }
 
+    public int[] getParent(){
+        return parent;
+    }
+}
+
+class Solution {
     public int findCircleNum(int[][] isConnected) 
     {
-        n=isConnected.length;
-        boolean[] vis=new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++)
+        int n = isConnected.length;
+        DSU dsu = new DSU(n);
+        for(int i = 0; i < n; i++)
         {
-            if(!vis[i])
+            for(int j = 0; j < n; j++)
             {
-                vis[i]=true;
-                solve(i,vis,isConnected);
-                count++;
+                if(isConnected[i][j] == 0)
+                continue;
+
+                dsu.union(i, j);
             }
+        }
+
+        int count = 0;
+        int[] parent = dsu.getParent();
+        for(int i = 0; i < n; i++)
+        {
+            if(parent[i] == i)
+            count++;
         }
         return count;
     }
