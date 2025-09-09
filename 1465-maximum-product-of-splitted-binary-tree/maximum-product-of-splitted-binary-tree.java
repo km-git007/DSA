41783fdbf89aa2fb8722923cbdd4e8b102a39096
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-10 01:39:22 +0530

    Time: 6 ms (72.35%) | Memory: 57.3 MB (66.92%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -1,41 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     private long maxProduct;
-    private final int MOD = 1000000007;
-    private long totalSum;
-    private long totalSum(TreeNode root){
-        if(root == null) return 0;
-        return root.val + totalSum(root.left) + totalSum(root.right);
+    private final long MOD = 1_000_000_007;
+
+    private long sum(TreeNode root) {
+        if (root == null) return 0;
+        return root.val + sum(root.left) + sum(root.right);
     }
 
-    private long findTreeSum(TreeNode root){
-        if(root == null) return 0;
+    private long subtreeSum(TreeNode root, long totalSum) {
+        if (root == null) return 0;
 
-        long leftSum = findTreeSum(root.left);
-        long rightSum = findTreeSum(root.right);
-        long subTreeSum = leftSum + rightSum + root.val;
-        maxProduct = Math.max(maxProduct, (subTreeSum * (totalSum - subTreeSum)));
-        return subTreeSum;
+        long left = subtreeSum(root.left, totalSum);
+        long right = subtreeSum(root.right, totalSum);
+        long current = left + right + root.val;
+
+        long product = current * (totalSum - current);
+        maxProduct = Math.max(maxProduct, product);
+
+        return current;
     }
 
     public int maxProduct(TreeNode root) {
         maxProduct = 0;
-        totalSum = totalSum(root);
-        findTreeSum(root);
+        long totalSum = sum(root);
+        subtreeSum(root, totalSum);
         return (int)(maxProduct % MOD);
     }
 }
