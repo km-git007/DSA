ddf61527ae57664b8f1e90b621b34ba30d37f711
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-10 19:13:05 +0530

    Time: 13 ms (68.05%) | Memory: 42.7 MB (62.22%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,59 +8,57 @@
  * }
  */
 class Solution {
-    public Solution()
-    {
-        adjList = new HashMap<>();
-        visited = new HashSet<>();
-        ans = new ArrayList<>();
-    }
-
-    private void buildAdjList(TreeNode root,TreeNode parent)
-    {
-        if(root == null)
-        return;
-
-        if(!adjList.containsKey(root.val))
-        adjList.put(root.val,new HashSet<>());
-
-        HashSet<Integer> set=adjList.get(root.val);
-
-        if(parent!=null)
-        set.add(parent.val);
-
-        if(root.left != null) 
-        set.add(root.left.val);
-
-        if(root.right != null) 
-        set.add(root.right.val);
-
-        buildAdjList(root.left,root);
-        buildAdjList(root.right,root);
-    }
-    
-    private int K;
-    private HashSet<Integer> visited;
-    private HashMap<Integer,HashSet<Integer>> adjList;
-    List<Integer> ans;
-    private void dfs(int node,int steps)
-    {
-        visited.add(node);
-        HashSet<Integer> neighbours=adjList.get(node);
-        for(Integer neighbour : neighbours) 
-        {
-            if(!visited.contains(neighbour))
-            dfs(neighbour,steps+1);
+    private void dfs(TreeNode root, Map<TreeNode, TreeNode> parentMap){
+        if(root == null){
+            return;
         }
 
-        if(steps==K)
-        ans.add(node);
+        if(root.left != null){
+            parentMap.put(root.left, root);
+        }
+        if(root.right != null){
+            parentMap.put(root.right, root);
+        }
+
+        dfs(root.left, parentMap);
+        dfs(root.right, parentMap);
     }
 
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) 
-    {
-        K=k;
-        buildAdjList(root,null);
-        dfs(target.val,0);
-        return ans;
+    private List<Integer> bfs(TreeNode target, int k, Map<TreeNode, TreeNode> parentMap){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(target);
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+        while(!queue.isEmpty() && k > 0){
+            int  size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if(node.left != null && !visited.contains(node.left)){
+                    queue.add(node.left);
+                    visited.add(node.left);
+                }
+                if(node.right != null && !visited.contains(node.right)){
+                    queue.add(node.right);
+                    visited.add(node.right);
+                }
+                if(parentMap.containsKey(node) && !visited.contains(parentMap.get(node))){
+                    queue.add(parentMap.get(node));
+                    visited.add(parentMap.get(node));
+                }
+            }
+            k--;
+        }
+        List<Integer> res = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            res.add(node.val);
+        }
+        return res;
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        dfs(root, parentMap);
+        return bfs(target, k, parentMap);
     }
 }
