ce40ac24790bdf0c0f44d658ea7505020f4154b3
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-12 01:35:03 +0530

    Time: 14 ms (42.04%) | Memory: 55.1 MB (58.17%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,30 +1,17 @@
 class Solution {
-
-    public int numberOfSubarrays(int[] nums, int k) 
-    {
-        return solve(nums, k) - solve(nums, k - 1);
-    }
-
-    // return number Of Subarrays Having Less Than Equal to K odd Numbers
-    public int solve(int[] nums, int k) 
-    {
-        int count = 0, i = 0;
-        int oddCount = 0;
-        for(int j = 0; j < nums.length; j++)
-        {
-            if(nums[j] % 2 == 1)
-            oddCount++;
-
-            while(i <= j && oddCount > k)
-            {
-                if(nums[i] % 2 == 1)
-                oddCount--;
-
+    private int atLeastKNiceSubarrays(int[] nums, int k) {
+        int count = 0, oddCount = 0, i = 0;
+        for(int j = 0; j < nums.length; j++){
+            oddCount += nums[j] % 2 == 0 ? 0 : 1;
+            while(oddCount >= k){
+                count += nums.length - j;
+                oddCount -= nums[i] % 2 == 0 ? 0 : 1;
                 i++;
             }
-
-            count += j - i + 1;
         }
         return count;
+    }
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atLeastKNiceSubarrays(nums, k) - atLeastKNiceSubarrays(nums, k + 1);
     }
 }
