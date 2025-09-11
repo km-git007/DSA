7d4b992e0d6c2c4bfc1a594e523544d0e5cadb16
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-12 01:24:42 +0530

    Time: 8 ms (51.80%) | Memory: 85.4 MB (97.04%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,28 +1,23 @@
 class Solution {
-
-    private boolean isVowel(char c) 
-    {
-        return "aeiou".indexOf(c) != -1;
+    private boolean isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 
-    public int[] vowelStrings(String[] words, int[][] queries) 
-    {
-        int n = words.length;
-        int[] pSum = new int[n + 1];
-        pSum[0] = 0;
-
-        for(int i = 0; i < n; i++)
-        {
-            boolean isVowelString = isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1));
-            pSum[i + 1] = pSum[i] + (isVowelString ? 1 : 0);
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int[] vowelStringsCount = new  int[words.length + 1];
+        for (int i = 0; i < words.length; i++) {
+            char startingChar = words[i].charAt(0);
+            char endingChar = words[i].charAt(words[i].length() - 1);
+            boolean isVowelString = isVowel(startingChar) && isVowel(endingChar);
+            vowelStringsCount[i + 1] += vowelStringsCount[i] + (isVowelString ? 1 : 0);
         }
 
         int[] res = new int[queries.length];
         int index = 0;
-        for(int[] query : queries)
-        {
-            int start = query[0] + 1, end = query[1] + 1;
-            res[index++] = pSum[end] - pSum[start - 1];
+        for(int[] query : queries){
+            int start = query[0] + 1;
+            int end = query[1] + 1;
+            res[index++] = vowelStringsCount[end]- vowelStringsCount[start - 1];
         }
         return res;
     }
