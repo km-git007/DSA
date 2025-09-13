c0b14b5476b70f0f468dcef37257b16242ce0ff0
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-13 23:31:10 +0530

    Time: 3 ms (72.15%) | Memory: 43.8 MB (5.68%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,33 +1,31 @@
 class Solution {
-    private List<Integer> list=new ArrayList<>();
-    private List<List<Integer>> ans=new ArrayList<>();
-    private void solve(int[] cand,int target,int index)
-    {
-        if(target==0)
-        {
-            ans.add(new ArrayList(list));
+    private List<List<Integer>> res;
+    private List<Integer> temp;
+    private void solve(int[] nums, int target, int index){
+        if(target == 0){
+            res.add(new ArrayList<>(temp));
             return;
         }
 
-        for(int i=index;i<cand.length;i++)
-        {
-            if(i!=index && cand[i]==cand[i-1])
+        for(int i = index; i < nums.length; i++){
+            if(nums[i] > target)
+            break;
+
+            if(i > index && nums[i] == nums[i - 1])
             continue;
 
-            if(target>=cand[i])
-            {
-                list.add(cand[i]);
-                solve(cand,target-cand[i],i+1);
-                list.remove(list.size()-1);
-            }
+            temp.add(nums[i]);
+            solve(nums, target - nums[i], i + 1);
+            temp.removeLast();
         }
     }
-    
-    public List<List<Integer>> combinationSum2(int[] cand, int target) 
-    {
-        Arrays.sort(cand);
 
-        solve(cand,target,0);
-        return ans;
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        res = new ArrayList<>();
+        temp = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        solve(nums, target, 0);
+        return res;
     }
 }
