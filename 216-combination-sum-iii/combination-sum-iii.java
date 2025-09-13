09d2eb305810cb555ce2807950374c3bc8889cd5
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-13 23:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (78.85%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,32 +1,27 @@
 class Solution {
     private List<List<Integer>> res;
     private List<Integer> temp;
-    private void solve(int startNum, int target, int k)
-    {
-        if(k == 0 && target == 0)
-        {
+    private void solve(int num, int target, int k){
+        if(target == 0 && k == 0){
             res.add(new ArrayList<>(temp));
             return;
         }
 
-        for(int num = startNum; num < 10; num++)
-        {
-            if(num > target)
-            break;
+        if(num > 9){
+            return;
+        }
 
-            // add
+        // skip
+        solve(num + 1, target, k);
+
+        if(num <= target && k > 0){
             temp.add(num);
-
-            // explore
             solve(num + 1, target - num, k - 1);
-
-            // backTrack
-            temp.remove(temp.size() - 1);
+            temp.removeLast();
         }
     }
 
-    public List<List<Integer>> combinationSum3(int k, int n) 
-    {
+    public List<List<Integer>> combinationSum3(int k, int n) {
         res = new ArrayList<>();
         temp = new ArrayList<>();
         solve(1, n, k);
