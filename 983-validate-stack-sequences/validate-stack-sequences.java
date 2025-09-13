878ad2a3bca6875f66351a9ef0889c79b8206c8f
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-14 01:56:29 +0530

    Time: 1 ms (94.97%) | Memory: 44.8 MB (13.62%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.java
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,13 +1,10 @@
 class Solution {
-    public boolean validateStackSequences(int[] pushed, int[] popped) 
-    {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
         Deque<Integer> stack = new ArrayDeque<>();
         int index = 0;
-        for(int num : pushed)
-        {
+        for (int num : pushed) {
             stack.push(num);
-            while(!stack.isEmpty() && popped[index] == stack.peek())
-            {
+            while (!stack.isEmpty() && stack.peek() == popped[index]) {
                 stack.pop();
                 index++;
             }
