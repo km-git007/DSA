b674870f8aa0a908de968e51cea5fad840879753
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-16 21:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (15.19%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,31 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int findInorderSuccessor(TreeNode root){
-        while(root.left != null){
+    
+    private TreeNode inorderSuccessor(TreeNode root) {
+        while(root.left != null) {
             root = root.left;
         }
-        return root.val;
+        return root;
     }
     
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return null;
+        if (root == null) {
+            return null;
+        }
         
-        if(root.val == key){
-            if(root.left == null){
+        if(root.val == key) {
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+            
+            else if(root.left == null) {
                 return root.right;
             }
-            else if(root.right == null){
+            
+            else if(root.right == null) {
                 return root.left;
             }
             
-            int inorderSuccessor = findInorderSuccessor(root.right);
-            root.val = inorderSuccessor;
-            root.right = deleteNode(root.right, inorderSuccessor);
+            TreeNode successor = inorderSuccessor(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
         }
-        else if(root.val > key){
-            root.left = deleteNode(root.left, key);
-        }else {
+        
+        else if(root.val < key) {
             root.right = deleteNode(root.right, key);
         }
+        else {
+            root.left = deleteNode(root.left, key);
+        }
+        
         return root;
     }
 }
