73cac6b8b68cf7760648844689844b5ad4262c83
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-16 21:46:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (63.67%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,25 +1,23 @@
 class Solution {
-    // int[] dp;
-    // private int solve(int n){
-    //     if(n == 0 || n == 1) return 1;
-
-    //     if(dp[n] != -1) return dp[n];
-        
-    //     int uniqueBSTs = 0;
-    //     for(int i = 1; i <= n; i++){
-    //         uniqueBSTs += solve(i - 1) * solve(n - i);
-    //     }
-    //     return dp[n] = uniqueBSTs;
-    // }
-
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        for(int node = 1; node <= n; node++){
-            for(int root = 1; root <= node; root++){
-                dp[node] += dp[root - 1] * dp[node - root];
-            }
+    private Integer[] dp;
+    private int uniqueBSTs(int n) {
+        if(n == 0){
+            return 1;
         }
-        return dp[n];
+        
+        if(dp[n] != null){
+            return dp[n];
+        }
+        
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            count += uniqueBSTs(i - 1) * uniqueBSTs(n - i);
+        }
+        return dp[n] = count;
+    }
+    
+    public int numTrees(int n) {
+        dp = new Integer[n + 1];
+        return uniqueBSTs(n);
     }
 }
