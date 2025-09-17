cc4a23cc308c6d1ebc5b6fbf9dea9db23ccb30df
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-17 16:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (43.23%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -1,18 +1,20 @@
 class Solution {
-    private int[] solve(TreeNode root){
-        if(root == null) return new int[2];
+    private int[] loot(TreeNode root) {
+        if(root == null) {
+            return new int[]{0, 0};
+        }
         
-        int[] leftLoot = solve(root.left);
-        int[] rightLoot = solve(root.right);
+        int[] leftLoot = loot(root.left);
+        int[] rightLoot = loot(root.right);
         
-        // loot the root 
         int rob = root.val + leftLoot[1] + rightLoot[1];
         int notRob = Math.max(leftLoot[0], leftLoot[1]) + Math.max(rightLoot[0], rightLoot[1]);
-        return  new int[]{rob, notRob};
-    }
 
+        return new int[]{rob, notRob};
+    }
+    
     public int rob(TreeNode root) {
-        int[] res = solve(root);
+        int[] res = loot(root);
         return Math.max(res[0], res[1]);
     }
 }
