01c49f395761f9364c33a705fd2a4888b692fd3b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-19 00:44:21 +0530

    Time: 27 ms (35.81%) | Memory: 101.6 MB (48.99%) - LeetSync

--- 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.java
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.java
@@ -1,28 +1,19 @@
 class DataStream {
-    private Deque<Integer> deque;
-    int counter, value, k;
+    private int value, k;
+    private int streak;
+
     public DataStream(int value, int k) {
-        deque = new ArrayDeque<>();
-        counter = 0;
         this.value = value;
         this.k = k;
+        this.streak = 0;
     }
 
     public boolean consec(int num) {
-        if(deque.size() == k) {
-            counter -= deque.pollFirst() == value ? 1 : 0;
+        if (num == value) {
+            streak++;
+        } else {
+            streak = 0;
         }
-        
-        // add to the deque
-        deque.addLast(num);
-        counter += num == value ? 1 : 0;
-        
-        return counter == k;
+        return streak >= k;
     }
 }
-
-/**
- * Your DataStream object will be instantiated and called as such:
- * DataStream obj = new DataStream(value, k);
- * boolean param_1 = obj.consec(num);
- */
