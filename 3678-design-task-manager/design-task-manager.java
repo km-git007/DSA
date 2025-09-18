b0a27660809a5baf322d1d4b7f94ada2b217771c
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-18 15:06:35 +0530

    Time: 280 ms (91.72%) | Memory: 177.2 MB (23.67%) - LeetSync

--- 3678-design-task-manager/design-task-manager.java
+++ 3678-design-task-manager/design-task-manager.java
@@ -1,9 +1,11 @@
 class Task implements Comparable<Task> {
     int userId, taskId, priority;
+    boolean isRemoved;
     public Task(int userId, int taskId, int priority) {
         this.userId = userId;
         this.taskId = taskId;
         this.priority = priority;
+        this.isRemoved = false;
     }
 
     @Override
@@ -17,11 +19,11 @@ class Task implements Comparable<Task> {
 
 class TaskManager {
 
-    private TreeSet<Task> taskSet;
+    private Queue<Task> taskQueue;
     private Map<Integer, Task> idToTaskMap;
     public TaskManager(List<List<Integer>> tasks) {
         idToTaskMap = new HashMap<>();
-        taskSet = new TreeSet<>();
+        taskQueue = new PriorityQueue<>();
         for(List<Integer> task : tasks) {
             add(task.get(0), task.get(1), task.get(2));
         }
@@ -29,34 +31,34 @@ class TaskManager {
 
     public void add(int userId, int taskId, int priority) {
         Task t = new Task(userId, taskId, priority);
-        taskSet.add(t);
+        taskQueue.add(t);
         idToTaskMap.put(taskId, t);
     }
 
     public void edit(int taskId, int newPriority) {
         Task oldTask = idToTaskMap.get(taskId);
-        
+
         // remove the oldTask
         rmv(taskId);
-        
+
         // add new task
         add(oldTask.userId, taskId, newPriority);
     }
 
     public void rmv(int taskId) {
         Task oldTask = idToTaskMap.get(taskId);
-        //remove the old task from treeSet and Map
-        taskSet.remove(oldTask);
+        oldTask.isRemoved = true;
+        //remove the old task from Map
         idToTaskMap.remove(taskId);
     }
 
     public int execTop() {
-        if(taskSet.isEmpty()) {
-            return -1;
-        }
-        
-        Task topPriorityTask = taskSet.first();
-        rmv(topPriorityTask.taskId);
-        return topPriorityTask.userId;
+       while(!taskQueue.isEmpty()) {
+           Task t = taskQueue.poll();
+           if(!t.isRemoved) {
+               return t.userId;
+           }
+       }
+       return -1;
     }
 }
