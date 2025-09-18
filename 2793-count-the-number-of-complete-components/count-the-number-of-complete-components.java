60182a4edcf320bc43d505f46baa152eade14159
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-18 18:14:52 +0530

    Time: 10 ms (73.74%) | Memory: 45.4 MB (33.50%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -1,30 +1,43 @@
+import java.util.*;
+
 class Solution {
     private int n;
     private List<Integer>[] adj;
-    private void buildGraph(int[][] edges){
-        for(int i = 0; i < n; i++){
+
+    private void buildGraph(int[][] edges) {
+        for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
         }
-
-        for(int[] edge : edges){
+        for (int[] edge : edges) {
             adj[edge[0]].add(edge[1]);
             adj[edge[1]].add(edge[0]);
         }
     }
 
-    private int[] dfs(int node, boolean[] vis){
-        vis[node] = true;
+    private boolean bfs(int start, boolean[] vis){
+        // BFS for this component
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        vis[start] = true;
 
-        int edges = adj[node].size();
-        int count = 1;
-        for(int nbr : adj[node]){
-            if(!vis[nbr]){
-                int[] res = dfs(nbr, vis);
-                edges += res[0];
-                count += res[1];
+        int count = 0;       // number of nodes
+        int totalEdges = 0;  // sum of degrees
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            count++;
+            totalEdges += adj[node].size(); // add degree of this node
+
+            for (int nbr : adj[node]) {
+                if (!vis[nbr]) {
+                    vis[nbr] = true;
+                    queue.add(nbr);
+                }
             }
         }
-        return new int[]{edges, count};
+
+        // totalEdges is double-counted
+        return totalEdges == count * (count - 1);
     }
 
     public int countCompleteComponents(int N, int[][] edges) {
@@ -34,15 +47,9 @@ class Solution {
 
         boolean[] vis = new boolean[n];
         int completeComponents = 0;
-        for(int node = 0; node < n; node++){
-            if(vis[node]){
-                continue;
-            }
-            int[] res = dfs(node, vis);
-            int totalEdges = res[0], count = res[1];
-            if(totalEdges == count * (count - 1)){
-                completeComponents++;
-            }
+        for (int start = 0; start < n; start++) {
+            if (vis[start]) continue;
+            completeComponents += bfs(start, vis) ? 1 : 0;
         }
         return completeComponents;
     }
