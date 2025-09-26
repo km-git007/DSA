c2e44469a35741bf3bde88068ef477ef0bc80fbe
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-26 18:30:34 +0530

    Time: 1 ms (44.05%) | Memory: 45.1 MB (50.89%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,31 @@
 class Solution {
-    private int newColor, color, n, m;
-    private final int [][] directions = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-    private void dfs(int row,int col, int[][] image)
-    {
-        if( row < 0 || row >=n || col <0 || col>=m || image[row][col]!=color )
-        return;
+    private int n, m, targetColor;
+    private final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private void fillColor(int row, int col, int color, int[][] image, boolean[][] vis){
+        if(row < 0 || col < 0 || row >= n || col >= m || vis[row][col] || image[row][col] != color){
+            return;
+        }
 
-        image[row][col]=newColor;
+        image[row][col] = targetColor;
+        vis[row][col] = true;
 
-        for(int[] direction : directions)
-        dfs(row+direction[0],col+direction[1],image);
-        
+        for(int[] dir : directions) {
+            int nextRow = row + dir[0];
+            int nextCol = col + dir[1];
+            fillColor(nextRow, nextCol, color, image, vis);
+        }
+
     }
 
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
-    {
-        if (image[sr][sc] == newColor)     
-        return image;
-        
-        // color which will get applied.
-        this.newColor = newColor;
-        // original color of the grid.
-        this.color = image[sr][sc];
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        targetColor = color;
+        if(image[sr][sc] == color) {
+            return image;
+        }
+
         n = image.length;
         m = image[0].length;
-
-        dfs(sr, sc, image);
-
+        fillColor(sr, sc, image[sr][sc], image, new boolean[n][m]);
         return image;
     }
 }
