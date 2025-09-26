19c67cf10f7caaa4f4da374a77fbd715cc479475
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-26 18:31:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (84.97%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,18 +1,17 @@
 class Solution {
     private int n, m, targetColor;
     private final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    private void fillColor(int row, int col, int color, int[][] image, boolean[][] vis){
-        if(row < 0 || col < 0 || row >= n || col >= m || vis[row][col] || image[row][col] != color){
+    private void fillColor(int row, int col, int color, int[][] image){
+        if(row < 0 || col < 0 || row >= n || col >= m || image[row][col] != color){
             return;
         }
 
         image[row][col] = targetColor;
-        vis[row][col] = true;
 
         for(int[] dir : directions) {
             int nextRow = row + dir[0];
             int nextCol = col + dir[1];
-            fillColor(nextRow, nextCol, color, image, vis);
+            fillColor(nextRow, nextCol, color, image);
         }
 
     }
@@ -25,7 +24,7 @@ class Solution {
 
         n = image.length;
         m = image[0].length;
-        fillColor(sr, sc, image[sr][sc], image, new boolean[n][m]);
+        fillColor(sr, sc, image[sr][sc], image);
         return image;
     }
 }
