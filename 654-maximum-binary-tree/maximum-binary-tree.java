9c96691c341d42179178715e1ad97af48fd689f3
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-09-27 18:06:14 +0530

    Time: 11 ms (8.83%) | Memory: 44.8 MB (92.15%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.java
+++ 654-maximum-binary-tree/maximum-binary-tree.java
@@ -1,23 +1,36 @@
-class Solution {    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        Deque<TreeNode> stack = new LinkedList<>();
         TreeNode root = new TreeNode(-1);
+        Deque<TreeNode> stack = new ArrayDeque<>();
         for (int num : nums) {
             TreeNode node = new TreeNode(num);
-            if(node.val > root.val){
+            if(root.val < node.val) {
                 root = node;
             }
-            while (!stack.isEmpty() && stack.peek().val < node.val) {
+            while(!stack.isEmpty() && stack.peek().val < num) {
                 node.left = stack.pop();
             }
 
-            if (!stack.isEmpty()) {
+            if(!stack.isEmpty() && stack.peek().val > num) {
                 stack.peek().right = node;
             }
-
             stack.push(node);
         }
-        
         return root;
     }
 }
