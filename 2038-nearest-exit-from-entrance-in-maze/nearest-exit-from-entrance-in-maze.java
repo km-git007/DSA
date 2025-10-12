13033a38019d9a2237a8c98fef13a169624cf67d
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-10-12 17:54:58 +0530

    Time: 4 ms (97.74%) | Memory: 46.3 MB (12.65%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -2,30 +2,26 @@ class Solution {
 
     int n,m;
     // Directions for moving up, down, left, and right
-    private static final int[][] directions = {{0,-1}, {0,1}, {-1,0}, {1,0}};
-    private int bfs(Queue<int[]> queue,char[][] maze, int[] entrance)
+    private static final int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    private int bfs(Queue<int[]> queue, char[][] maze, int[] entrance)
     {
         int steps = 0;
-        while(!queue.isEmpty())
-        {
+        while(!queue.isEmpty()){
             int levelSize = queue.size();
-            for(int i = 0; i < levelSize; i++)
-            {
-                int[] coordinates = queue.poll();
-                int x = coordinates[0];
-                int y = coordinates[1];
-                
-                if((x==0 || x == n-1 || y==0 || y == m-1) && (x != entrance[0] || y != entrance[1]))
-                return steps;
-                
-                for(int[] dir : directions)
-                {
-                    int newX = x + dir[0];
-                    int newY = y + dir[1];
-                    if(newX >= 0 && newX<n && newY >= 0 && newY<m && maze[newX][newY]=='.')
-                    {
-                        maze[newX][newY] = '+';
-                        queue.add(new int[]{newX, newY});
+            for(int i = 0; i < levelSize; i++){
+                int[] curr = queue.poll();
+                int row = curr[0], col = curr[1];
+
+                if((row == 0 || row == n - 1 || col == 0 || col == m - 1) && (row != entrance[0] || col != entrance[1])){
+                    return steps;
+                }
+
+                for(var dir : directions){
+                    int x = row + dir[0];
+                    int y = col + dir[1];
+                    if(x >= 0 && x < n && y >= 0 && y < m && maze[x][y] != '+'){
+                        queue.offer(new int[]{x, y});
+                        maze[x][y] = '+';
                     }
                 }
             }
@@ -36,12 +32,13 @@ class Solution {
 
     public int nearestExit(char[][] maze, int[] entrance)
     {
-        n=maze.length;
-        m=maze[0].length;
+        n = maze.length;
+        m = maze[0].length;
 
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{entrance[0], entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+';  // Recommended to mark entrance as visited
+        //mark entrance as visited
+        maze[entrance[0]][entrance[1]] = '+';  
 
         return bfs(queue,maze,entrance);
     }
