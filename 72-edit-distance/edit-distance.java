dfc8aed1718fc283aca39abe85ac48e63fa7fec6
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-19 03:43:32 +0530

    Time: 3 ms (96.12%) | Memory: 47.2 MB (20.08%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,31 +1,40 @@
 class Solution {
-        int n, m;
-    int[][] dp;
+    private Integer[][] dp;
     private int solve(int i, int j, String word1, String word2){
-        // add the remaining
-        if(i >= n) return m - j;
-        // delete the remaining
-        if(j >= m) return n - i;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1.charAt(i) == word2.charAt(j)) {
-            return solve(i+1, j+1, word1, word2);
+        if(i == word1.length() && j == word2.length()){
+            return 0;
         }
+
+        // Need to insert remaining chars from word2
+        if(i == word1.length()){
+            return word2.length() - j;  
+        }
+
+        // Need to delete remaining chars from word1
+        if(j == word2.length()){
+            return word1.length() - i; 
+        }
+
+        if(dp[i][j] != null){
+            return dp[i][j];
+        }
+
+        if(word1.charAt(i) == word2.charAt(j)){
+            return dp[i][j] = solve(i + 1, j + 1, word1, word2);
+        }
+
         int insert = solve(i, j + 1, word1, word2);
         int replace = solve(i + 1, j + 1, word1, word2);
         int delete = solve(i + 1, j, word1, word2);
-
-        return dp[i][j] = 1 + Math.min(insert, Math.min(replace, delete));
+        return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
     }
 
     public int minDistance(String word1, String word2) {
-        n = word1.length();
-        m = word2.length();
-        dp = new int[n + 1][m + 1];
-        for(int i = 0; i <= n; i++){
-            Arrays.fill(dp[i], -1);
+        if(word2.length() > word1.length()){
+            return minDistance(word2, word1);
         }
+
+        dp = new Integer[word1.length() + 1][word2.length() + 1];
         return solve(0, 0, word1, word2);
     }
 }
