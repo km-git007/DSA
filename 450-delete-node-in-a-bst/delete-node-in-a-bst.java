f5a37a00d0c94a65db6d9387c9c6ce26bfe8e027
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-22 01:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (41.97%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -14,7 +14,6 @@
  * }
  */
 class Solution {
-    
     private TreeNode inorderSuccessor(TreeNode root) {
         while(root.left != null) {
             root = root.left;
@@ -27,29 +26,23 @@ class Solution {
             return null;
         }
         
-        if(root.val == key) {
-            if(root.left == null && root.right == null) {
+        if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }else if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }else{
+            if(root.left == null && root.right == null){
                 return null;
             }
             
-            else if(root.left == null) {
-                return root.right;
-            }
-            
-            else if(root.right == null) {
+            if(root.right == null){
                 return root.left;
             }
             
             TreeNode successor = inorderSuccessor(root.right);
             root.val = successor.val;
-            root.right = deleteNode(root.right, successor.val);
-        }
-        
-        else if(root.val < key) {
-            root.right = deleteNode(root.right, key);
-        }
-        else {
-            root.left = deleteNode(root.left, key);
+            root.right = deleteNode(root.right, root.val);
+
         }
         
         return root;
