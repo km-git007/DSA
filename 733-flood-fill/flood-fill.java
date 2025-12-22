b382300a0046e1c75b59ff8837c2daf1c5eef411
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-22 23:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (16.83%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,29 +1,30 @@
 class Solution {
     private int n, m, targetColor;
     private final int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    private void fillColor(int row, int col, int color, int[][] image){
-        if(row < 0 || col < 0 || row >= n || col >= m || image[row][col] != color){
+    private void fillColor(int row, int col, int color, int[][] image) {
+        if(row < 0 || col < 0 || row >= n || col >= m || image[row][col] != color) {
             return;
         }
-
+        
+        // fill color
         image[row][col] = targetColor;
-
+        
         for(int[] dir : directions) {
-            int nextRow = row + dir[0];
-            int nextCol = col + dir[1];
-            fillColor(nextRow, nextCol, color, image);
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            fillColor(newRow, newCol, color, image);
         }
-
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        targetColor = color;
         if(image[sr][sc] == color) {
             return image;
         }
 
         n = image.length;
         m = image[0].length;
+        targetColor = color;
+        
         fillColor(sr, sc, image[sr][sc], image);
         return image;
     }
