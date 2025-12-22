f2f7f81cfd0477940cc20ddde0b9667261131a35
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-23 01:00:49 +0530

    Time: 87 ms (75.52%) | Memory: 48.6 MB (71.02%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -1,47 +1,50 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        Set<String> deadendsSet = new HashSet<>(Arrays.asList(deadends));
-        if(deadendsSet.contains("0000")){
+        Set<String> deadSet = new HashSet<>(Arrays.asList(deadends));
+        if(deadSet.contains("0000")) {
             return -1;
         }
 
+        int turnCount = 0;
         Queue<String> queue = new LinkedList<>();
-        queue.offer("0000");
-
-        int minimumTurns = 0;
-        while(!queue.isEmpty()){
+        queue.add("0000");
+        while(!queue.isEmpty()) {
             int levelSize = queue.size();
-            for(int i = 0; i < levelSize; i++){
+            for(int i = 0; i < levelSize; i++) {
                 String curr = queue.poll();
-                if(curr.equals(target)){
-                    return minimumTurns;
+                if(curr.equals(target)) {
+                    return turnCount;
                 }
 
-                StringBuilder sb = new StringBuilder(curr);
-                for(int j = 0; j < 4; j++){
-                    char ch = sb.charAt(j);
+                StringBuilder currCombination = new StringBuilder(curr);
+                for(int index = 0; index < 4; index++) {
+                    char original = currCombination.charAt(index);
 
-                    char prevChar = (char) (((ch - '0' + 9) % 10) + '0');
-                    sb.setCharAt(j, prevChar);
-                    String prev = sb.toString();
-                    if(!deadendsSet.contains(prev)){
-                        queue.offer(prev);
-                        deadendsSet.add(prev);
+                    char nextChar = (char) ((original - '0' + 1) % 10 + '0');
+                    currCombination.setCharAt(index, nextChar);
+                    String nextCombination = currCombination.toString();
+
+                    if(!deadSet.contains(nextCombination)) {
+                        queue.add(nextCombination);
+                        deadSet.add(nextCombination);
                     }
 
-                    char nextChar = (char) (((ch - '0' + 1) % 10) + '0');
-                    sb.setCharAt(j, nextChar);
-                    String next = sb.toString();
-                    if(!deadendsSet.contains(next)){
-                        queue.offer(next);
-                        deadendsSet.add(next);
+                    char prevChar = (char) ((original - '0' + 9) % 10 + '0');
+                    currCombination.setCharAt(index, prevChar);
+                    nextCombination  = currCombination.toString();
+
+                    if(!deadSet.contains(nextCombination)) {
+                        queue.add(nextCombination);
+                        deadSet.add(nextCombination);
                     }
+
                     // undo
-                    sb.setCharAt(j, ch);
+                    currCombination.setCharAt(index, original);
                 }
             }
-            minimumTurns++;
+            turnCount++;
         }
+
         return -1;
     }
 }
