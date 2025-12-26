0c6e80c8959c4d9802b5455e02fe40df4be6577b
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-27 02:27:51 +0530

    Time: 50 ms (81.33%) | Memory: 108 MB (74.22%) - LeetSync

--- 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.java
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.java
@@ -1,50 +1,44 @@
 class Solution {
-private int treeSize, count;
-    private long maxScore;
-    private int sizeOfTree(int node, List<Integer> [] children){
+    private long maxScore = 0;
+    private int count = 0;
+    private List<Integer>[] tree;
 
+    private int dfs(int node, int n) {
         long score = 1;
-        // including the current node
-        int size = 1;
+        int size = 0;
 
-        for(int child : children[node]){
-            int childSize = sizeOfTree(child, children);
-            score *= childSize;
+        for (int child : tree[node]) {
+            int childSize = dfs(child, n);
             size += childSize;
+            score *= childSize;
         }
 
-        int rest = treeSize - size;
-        if(rest > 0){
-            score *= rest;
-        }
+        int remaining = n - size - 1;
+        if (remaining > 0) score *= remaining;
 
-        if(score > maxScore){
+        if (score > maxScore) {
             maxScore = score;
             count = 1;
-        }else if(score == maxScore){
+        } else if (score == maxScore) {
             count++;
         }
 
-        return size;
-    }
-
-    private void buildTree(int[] parents, List<Integer> [] children){
-        for(int i = 0; i < parents.length; i++){
-            children[i] = new ArrayList<>();
-        }
-
-        for(int i = 1; i < parents.length; i++){
-            children[parents[i]].add(i);
-        }
-        return;
+        return size + 1;
     }
 
     public int countHighestScoreNodes(int[] parents) {
-        treeSize = parents.length;
-        maxScore = count = 0;
-        List<Integer> [] children = new ArrayList[parents.length];
-        buildTree(parents, children);
-        sizeOfTree(0, children);
+        int n = parents.length;
+        tree = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < n; i++) {
+            tree[parents[i]].add(i);
+        }
+
+        dfs(0, n);
         return count;
     }
 }
