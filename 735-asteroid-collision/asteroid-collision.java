9f38bc27bfceabc01f0c91d20b61261a990a5404
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-12-27 20:15:25 +0530

    Time: 2 ms (97.08%) | Memory: 46.8 MB (63.95%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,31 +1,25 @@
 class Solution {
-        public int[] asteroidCollision(int[] asteroids)
-        {
+    public int[] asteroidCollision(int[] asteroids) {
         Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = 0; i < asteroids.length; i++)
-        {
-            if(asteroids[i] < 0){
-                while(!stack.isEmpty() && stack.peek() > 0 && Math.abs(stack.peek()) < Math.abs(asteroids[i])){
+        for(int asteroid : asteroids) {
+            if(stack.isEmpty() || asteroid > 0 || (asteroid < 0 && stack.peek() < 0)) {
+                stack.push(asteroid);
+            }else{
+                while(!stack.isEmpty() && stack.peek() > 0 && Math.abs(asteroid) > stack.peek()) {
                     stack.pop();
                 }
-                
-                if(stack.isEmpty() || stack.peek() < 0){
-                    stack.push(asteroids[i]);
-                    continue;
-                }
-                // if two of them are of the same magnitude
-                if(Math.abs(stack.peek()) == Math.abs(asteroids[i])){
+                if(stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroid);
+                } else if (stack.peek() == Math.abs(asteroid)) {
                     stack.pop();
                 }
-            }
-            else{
-                stack.push(asteroids[i]);
             }
         }
         
         int[] result = new int[stack.size()];
-        for(int i = result.length - 1; i >= 0; i--){
-            result[i] = stack.pop();
+        int index = stack.size() - 1;
+        while(!stack.isEmpty()){
+            result[index--] = stack.pop();
         }
         return result;
     }
