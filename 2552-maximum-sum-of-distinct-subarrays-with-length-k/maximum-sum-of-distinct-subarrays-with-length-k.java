518ec20ab332071d530147b1cf4fae4161cfbef1
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2026-01-16 01:47:00 +0530

    Time: 56 ms (34.96%) | Memory: 100 MB (13.14%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,28 +1,20 @@
 class Solution {
-    public long maximumSubarraySum(int[] nums, int k) 
-    {
-        int i = 0;
+    public long maximumSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
         long sum = 0, maxSum = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int j = 0; j < nums.length; j++)
-        {
+        int i = 0;
+        for(int j = 0; j < nums.length; j++) {
             sum += nums[j];
             map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
-            if(j - i + 1 == k)
-            {
-               if(map.size() == k)
-               maxSum = Math.max(sum, maxSum);
-
-               sum -= nums[i];
-
-               // remove 1 occurance the ith number from the map
-               map.put(nums[i], map.get(nums[i]) - 1);
-
-                // if no more occurance of the its number is left than remove it from the map
-                if(map.get(nums[i]) == 0)
-                map.remove(nums[i]);
-
+            if(j - i + 1 == k) {
+                if(map.size() == k) {
+                    maxSum = Math.max(maxSum, sum);
+                }
+                sum -= nums[i];
+                map.put(nums[i], map.getOrDefault(nums[i], 0) - 1);
+                if(map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
                 i++;
             }
         }
