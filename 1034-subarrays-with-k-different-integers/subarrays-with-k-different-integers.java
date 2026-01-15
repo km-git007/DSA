150a846041f15d50be92f5001748d468375c0955
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2026-01-16 02:13:57 +0530

    Time: 48 ms (73.87%) | Memory: 50 MB (66.10%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,34 +1,24 @@
 class Solution {
 
-    public int subarraysWithatMostKDistinct(int[] nums, int k)
-    {
-        if(k==0)
-        return 0;
-
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int count=0,i=0;
-        for(int j=0;j<nums.length;j++)
-        {
-            // if(map.containsKey(nums[j]))
-            // map.put(nums[j],map.get(nums[j])+1);
-            // else
-            // map.put(nums[j],1);
-            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-            
-            while(map.size()>k)
-            {
-                map.put(nums[i],map.get(nums[i])-1);
-                if(map.get(nums[i])==0)
-                map.remove(nums[i]);
+    private int numSubarrayWithAtLeastKDistinct(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length, i = 0, count = 0;
+        for(int j = 0; j < nums.length; j++) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+            while (map.size() >= k) {
+                count += n - j;
+                map.put(nums[i], map.getOrDefault(nums[i], 0) - 1);
+                if(map.get(nums[i]) == 0) {
+                    map.remove(nums[i]);
+                }
                 i++;
             }
-            count+=j-i+1;
         }
         return count;
-    } 
+    }
 
-    public int subarraysWithKDistinct(int[] nums, int k) 
+    public int subarraysWithKDistinct(int[] nums, int k)
     {
-        return subarraysWithatMostKDistinct(nums,k)-subarraysWithatMostKDistinct(nums,k-1);
+        return numSubarrayWithAtLeastKDistinct(nums, k) - numSubarrayWithAtLeastKDistinct(nums, k + 1);
     }
 }
