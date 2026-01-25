13a6a920f032b29b65aa8d47b9c009287236c119
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2026-01-25 20:21:16 +0530

    Time: 105 ms (19.36%) | Memory: 135.9 MB (29.80%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int timeToInformAll(int id, Map<Integer, List<Integer>> heirarchyMap, int[] informTime){
+        if(!heirarchyMap.containsKey(id)){
+            return 0;
+        }
+
+        int maxTime = 0;
+        List<Integer> subordinates = heirarchyMap.get(id);
+        for(int i = 0; i < subordinates.size(); i++){
+            maxTime = Math.max(maxTime, informTime[id] + timeToInformAll(subordinates.get(i), heirarchyMap, informTime));
+        }
+        return maxTime;
+    }
+
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> heirarchyMap = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            heirarchyMap.putIfAbsent(manager[i], new ArrayList<>());
+            heirarchyMap.get(manager[i]).add(i);
+        }
+        return timeToInformAll(headID, heirarchyMap, informTime);
+    }
+}
