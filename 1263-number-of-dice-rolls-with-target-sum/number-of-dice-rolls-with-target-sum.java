b68847ac19910f9953bdf55f89a15d34ef0d4e44
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2026-01-28 02:58:28 +0530

    Time: 10 ms (69.93%) | Memory: 44 MB (27.95%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,32 +1,31 @@
 class Solution {
+    private final int MOD = 1_000_000_007;
+    private int[][] dp;
+    private int recur(int n, int k, int target){
+        if(n == 0 && target == 0){
+            return 1;
+        }
 
-    private int MOD=1000000007;
-    int[][] dp=new int[31][1001];
-    private int solve(int n,int k,int target)
-    {
-        if(n == 0 && target == 0)
-        return 1;
-
-        if(n==0)
-        return 0;
-
-        if(dp[n][target]!=-1)
-        return dp[n][target];
+        if(n < 0 || target < 0){
+            return 0;
+        }
+        
+        if(dp[n][target] != -1){
+            return dp[n][target];
+        }
 
         int ways = 0;
-        for(int i = 1; i <= k; i++)
-        {
-            if(i <= target)
-            ways = (ways % MOD) + (solve(n-1, k, target - i) % MOD);
+        for(int i = 1; i <= k; i++){
+            ways = ways % MOD + recur(n - 1, k, target - i) % MOD;
         }
         return dp[n][target] = ways % MOD;
     }
 
-    public int numRollsToTarget(int n, int k, int target) 
-    {
-        for(int i=0;i<31;i++)
-        Arrays.fill(dp[i],-1);
-
-        return solve(n,k,target);
+    public int numRollsToTarget(int n, int k, int target) {
+        dp = new int[n + 1][target + 1];
+        for(int i = 0; i <= n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return recur(n, k, target);
     }
 }
