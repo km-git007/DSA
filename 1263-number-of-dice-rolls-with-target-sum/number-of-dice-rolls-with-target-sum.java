818b097179732172712ca103d7a99bb38345b062
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2026-01-28 17:50:26 +0530

    Time: 12 ms (47.52%) | Memory: 45.8 MB (11.56%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,7 +1,7 @@
 class Solution {
-    private final int MOD = 1_000_000_007;
-    private int[][] dp;
-    private int recur(int n, int k, int target){
+    private final long MOD = 1_000_000_007;
+    private long[][] dp;
+    private long recur(int n, int k, int target){
         if(n == 0 && target == 0){
             return 1;
         }
@@ -14,18 +14,18 @@ class Solution {
             return dp[n][target];
         }
 
-        int ways = 0;
+        long ways = 0;
         for(int i = 1; i <= k; i++){
-            ways = ways % MOD + recur(n - 1, k, target - i) % MOD;
+            ways += recur(n - 1, k, target - i) % MOD;
         }
         return dp[n][target] = ways % MOD;
     }
 
     public int numRollsToTarget(int n, int k, int target) {
-        dp = new int[n + 1][target + 1];
+        dp = new long[n + 1][target + 1];
         for(int i = 0; i <= n; i++){
             Arrays.fill(dp[i], -1);
         }
-        return recur(n, k, target);
+        return (int)recur(n, k, target);
     }
 }
